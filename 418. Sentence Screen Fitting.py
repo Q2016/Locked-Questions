86e8a2b60a0ebc4366fefbe0ea5c3eabee3879d9
Q2016 <k.sohraby@gmail.com>
2021-12-10 09:14:57 -0800

    Update 418. Sentence Screen Fitting.py

--- 418. Sentence Screen Fitting.py
+++ 418. Sentence Screen Fitting.py
@@ -36,3 +36,43 @@ apple
 pie-I
 had--
 The character '-' signifies an empty space on the screen.
+
+
+My solution:
+def wordsTyping(sentence, rows, cols):
+    
+    block=" ".join(sentence)
+    
+    #print(block)
+    l=len(block)
+    screen=rows*cols
+    
+    "----- ----- ----- -----"
+    
+    cursor=0
+    repeated=0
+    for i in range(rows):
+        if l<cols:
+            cursor+=cols-l+1# +1 for space
+        elif l>cols and block[cols-1]==" ":
+            cursor+=l-cols+1
+        elif l>cols and block[cols-1]!=" ":
+            n=1
+            while block[cols-1-n]!=" ":
+                n+=1
+            cursor+=l-cols+n+1
+        # for the else: no need to change the cursor
+        if cursor>screen:
+            return repeated 
+        repeated+=1
+            
+
+rows, cols, sentence = 2, 8, ["hello", "world"]
+rows, cols, sentence = 3, 6, ["a", "bcd", "e"]
+rows, cols, sentence = 4, 5, ["I", "had", "apple", "pie"]
+
+wordsTyping(sentence, rows, cols)
+
+
+Others:
+https://www.youtube.com/watch?v=1ChX4Cpz0bU
