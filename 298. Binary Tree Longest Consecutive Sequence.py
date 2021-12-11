e91fc87d6c032110b71ccd7bbb9f53fc9f38e27f
Q2016 <k.sohraby@gmail.com>
2021-12-10 21:35:53 -0800

    Update 298. Binary Tree Longest Consecutive Sequence.py

--- 298. Binary Tree Longest Consecutive Sequence.py
+++ 298. Binary Tree Longest Consecutive Sequence.py
@@ -21,3 +21,67 @@ Longest consecutive sequence path is 3-4-5, so return 3.
   / 
  1
 Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+
+My solution:
+   
+class Solution:
+    # your task is to complete this function
+    # function should print the top view of the binary tree
+    # Note: You aren't required to print a new line after every test case
+    
+    sequence=[]
+    def longestConsecutive(self, root):
+        # Code here
+        
+        if root.left==None and root.right==None:
+            return sequence 
+        value_pre=root.data
+        if root.right==value_pre+1:
+            self.sequence.append(value_pre)
+            return longestConsecutive(root.right)
+        if root.left==value_pre+1:
+            self.sequence.append(value_pre)
+            return longestConsecutive(root.left)
+         
+https://www.geeksforgeeks.org/longest-consecutive-sequence-binary-tree/         
+class newNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+ 
+# Utility method to return length of
+# longest consecutive sequence of tree
+def longestConsecutiveUtil(root, curLength,
+                           expected, res):
+    if (root == None):
+        return
+ 
+    # if root data has one more than its
+    # parent then increase current length
+    if (root.data == expected):
+        curLength += 1
+    else:
+        curLength = 1
+ 
+    # update the maximum by current length
+    res[0] = max(res[0], curLength)
+ 
+    # recursively call left and right subtree
+    # with expected value 1 more than root data
+    longestConsecutiveUtil(root.left, curLength,
+                           root.data + 1, res)
+    longestConsecutiveUtil(root.right, curLength,
+                           root.data + 1, res)
+ 
+# method returns length of longest consecutive
+# sequence rooted at node root
+def longestConsecutive(root):
+    if (root == None):
+        return 0
+ 
+    res = [0]
+ 
+    # call utility method with current length 0
+    longestConsecutiveUtil(root, 0, root.data, res)
+ 
+    return res[0]
