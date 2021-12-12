058acfd124cff750ab68de8e927ad4d1e5af0751
Q2016 <k.sohraby@gmail.com>
2021-12-11 20:36:11 -0800

    Update 281. Zigzag Iterator.py

--- 281. Zigzag Iterator.py
+++ 281. Zigzag Iterator.py
@@ -5,3 +5,40 @@ By calling next repeatedly until hasNext returns false, the order of elements re
 Follow up: What if you are given k 1d vectors? How well can your code be extended to such cases?
   
   
+  
+  
+  
+  
+
+  
+  
+  
+  https://github.com/ShiqinHuo/LeetCode-Python/blob/master/Python/zigzag-iterator.py
+  class ZigzagIterator(object):
+
+    def __init__(self, v1, v2):
+        """
+        Initialize your q structure here.
+        :type v1: List[int]
+        :type v2: List[int]
+        """
+        self.q = collections.deque([(len(v), iter(v)) for v in (v1, v2) if v])
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        len, iter = self.q.popleft()
+        if len > 1:
+            self.q.append((len-1, iter))
+        return next(iter)
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return bool(self.q)
+
+# Your ZigzagIterator object will be instantiated and called as such:
+# i, v = ZigzagIterator(v1, v2), []
+# while i.hasNext(): v.append(i.next())
