7091111f53187c047b5ea815302a96c9d235b2ec
Q2016 <k.sohraby@gmail.com>
2021-12-17 14:40:26 -0800

    Update 280. Wiggle Sort.py

--- 280. Wiggle Sort.py
+++ 280. Wiggle Sort.py
@@ -29,3 +29,48 @@ class Solution(object):
             if ((i % 2) and nums[i - 1] > nums[i]) or (not (i % 2) and nums[i - 1] < nums[i]):
                 # Swap unordered elements.
                 nums[i - 1], nums[i] = nums[i], nums[i - 1]
+
+                
+                
+First try to solve it without in-place:
+
+sort the array in increasing order.
+create a result array of the same size.
+keep 2 pointers, one from the beginning, one from the middle(notice odd/even of array).
+put beginning first, then the middle pointer, into the result array.
+Solve it in-place.
+
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        if(nums.length<2)  return;
+
+        Arrays.sort(nums);
+        int i=2;
+        for( ; i< nums.length-1 ; ) {
+            int x = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = x;
+            i +=2;
+        }       
+    }
+}
+Is it really necessarily to sort the array initially ?
+--NO--
+noticing that the property of wiggle array is:
+
+  if i%2 == 1, nums[i] >= nums[i-1];
+  if i%2 == 0, nums[i] <= nums[i-1];
+if the property is not observed, simple swap the property-break number. the previous relation at i-2 will still stand.
+
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        for(int i=1; i< nums.length;i++){
+            if((i&1) == 1 && nums[i] < nums[i-1] 
+                || (i&1) == 0 && nums[i] > nums[i-1]){
+                int t = nums[i];
+                nums[i] = nums[i-1];
+                nums[i-1] = t;
+            }
+        }
+    }
+}                
