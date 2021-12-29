859d06c89696027449a99d987401e4ee6cdea631
Q2016 <k.sohraby@gmail.com>
2021-12-29 07:42:38 -0800

    Create 31. Next Permutation.py

---
+++ 31. Next Permutation.py
@@ -0,0 +1,32 @@
+Maybe in general for the lexigraphical permutation:
+https://aakash01.github.io/programming/2014/03/11/permutations-of-a-given-string-in-lexicographic-order
+
+
+
+https://leetcode.com/problems/next-permutation/
+
+
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
