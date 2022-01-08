eed8c216d655de6d4ad5a398cff5fdceecb04256
Q2016 <k.sohraby@gmail.com>
2022-01-07 18:33:31 -0800

    Create 334. Increasing Triplet Subsequence.py

---
+++ 334. Increasing Triplet Subsequence.py
@@ -0,0 +1,19 @@
+
+https://github.com/ShiqinHuo/LeetCode-Python/blob/master/Python/increasing-triplet-subsequence.py
+
+
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        min_num, a, b = float("inf"), float("inf"), float("inf")
+        for c in nums:
+            if min_num >= c:
+                min_num = c
+            elif b >= c:
+                a, b = min_num, c
+            else:  # a < b < c
+                return True
+        return False
