e33c67ae3ff8787255ef2197fba74ce64c64ed7e
Q2016 <k.sohraby@gmail.com>
2022-01-09 19:24:16 -0800

    Create 48. Rotate Image.py

---
+++ 48. Rotate Image.py
@@ -0,0 +1,26 @@
+
+https://leetcode.com/problems/rotate-image/
+
+
+Solution
+Approach 1: Rotate Groups of Four Cells
+Intuition
+
+Observe how the cells move in groups when we rotate the image.
+
+The corners all move
+
+We can iterate over each group of four cells and rotate them.
+
+Implementation
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
