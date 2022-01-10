d83a9225e2c489de5995861fd323def54e150e0d
Q2016 <k.sohraby@gmail.com>
2022-01-10 14:30:51 -0800

    Create 134. Gas Station.py

---
+++ 134. Gas Station.py
@@ -0,0 +1,23 @@
+
+https://leetcode.com/problems/gas-station/discuss/42565/My-AC-is-O(1)-space-O(n)-running-time-solution.-Does-anybody-have-posted-this-solution
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+
+       int start = gas.size()-1;
+       int end = 0;
+       int sum = gas[start] - cost[start];
+       while (start > end) {
+          if (sum >= 0) {
+             sum += gas[end] - cost[end];
+             ++end;
+          }
+          else {
+             --start;
+             sum += gas[start] - cost[start];
+          }
+       }
+       return sum >= 0 ? start : -1;
+    }
+};
