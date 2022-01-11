76fc282d6fd9cb03d2ddc8170a85318681749575
Q2016 <k.sohraby@gmail.com>
2022-01-10 21:19:20 -0800

    Create 695. Max Area of Island.py

---
+++ 695. Max Area of Island.py
@@ -0,0 +1,31 @@
+
+//The solution is to search each island. Remember marking grid[r][c] = 2 as visited.
+
+//DFS
+
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), ans = 0;
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) 
+                if (grid[i][j] == 1) ans = max(ans, dfs(grid, i, j));
+        return ans;
+    }
+private:
+    int dfs(vector<vector<int>>& grid, int row, int col) {
+        int m = grid.size(), n = grid[0].size(), area = 1;
+        grid[row][col] = 2;
+        vector<int> dir({-1,0,1,0,-1});
+        for (int i = 0; i < 4; i++) {
+            int r = row+dir[i], c = col+dir[i+1];
+            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1) 
+                area += dfs(grid, r, c);
+        }
+        return area;
+    }
+};
+
+
+/*The idea is to count the area of each island using dfs. During the dfs, we set the value of each point in the island to 0. The time complexity is O(mn).
+*/
