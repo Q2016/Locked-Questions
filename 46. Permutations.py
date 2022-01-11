28c7441ba20559228ccf1483c87e5f7c1d1e42c0
Q2016 <k.sohraby@gmail.com>
2022-01-10 18:59:36 -0800

    Create 46. Permutations.py

---
+++ 46. Permutations.py
@@ -0,0 +1,15 @@
+List of backtracking questions:
+https://leetcode.com/problems/permutations/discuss/18284/Backtrack-Summary%3A-General-Solution-for-10-Questions!!!!!!!!-Python-(Combination-Sum-Subsets-Permutation-Palindrome)
+
+    def permute(self, nums):
+        def backtrack(start, end):
+            if start == end:
+                ans.append(nums[:])
+            for i in range(start, end):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start+1, end)
+                nums[start], nums[i] = nums[i], nums[start]
+                
+        ans = []
+        backtrack(0, len(nums))
+        return ans
