d63f8a05fd70070ed2f7283e4429451cff7d664e
Q2016 <k.sohraby@gmail.com>
2022-01-13 09:41:30 -0800

    Create 143. Reorder List.py

---
+++ 143. Reorder List.py
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/reorder-list/discuss/1640597/PythonJavaC%2B%2B-2-Easy-Solutions-oror-Visualized-Explanation-oror-Beginner-Friendly
+
+
+step 1: find the middle pointer of the linked list and split the linked list into two halves using slow and fast pointers
+								head = [1, 2, 3]
+head = [1, 2, 3, 4, 5]    =>
+								mid  = [4, 5]
+step 2: reverse the second half
+head = [1, 2, 3]
+mid  = [4, 5]             =>    mid = [5, 4]
+step 3: interleaving merge the first half and the reversed second half
+head = [1, 2, 3]
+					      =>    head = [1, 5, 2, 4, 3]
+mid = [5, 4]
+Time  Complexity: O(N)
+Space Complexity: O(1)
+  
+  
+      def reorderList(self, head):
+        if head.next:
+            # step 1: find the middle pointer of the linked list
+            slow, fast = head, head
+            while fast.next and fast.next.next:
+                slow, fast = slow.next, fast.next.next
+            mid, slow.next = slow.next, None
+
+            # step 2: reverse the last half linked list
+            p, q, mid.next = mid, mid.next, None
+            while q:
+                p, q, p.next = q, q.next, p
+            mid = p
+
+            # step 3: merge first half and reversed last half
+            p, q = head, mid
+            while q:
+                pp, qq = p, q  # backup p and q
+                p, q = p.next, q.next
+                pp.next, qq.next = qq, p
