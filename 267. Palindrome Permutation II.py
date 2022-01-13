93b1353c6272b32b74bf0569e522b526cf6acb2e
Q2016 <k.sohraby@gmail.com>
2022-01-13 13:47:07 -0800

    Update 267. Palindrome Permutation II.py

--- 267. Palindrome Permutation II.py
+++ 267. Palindrome Permutation II.py
@@ -56,3 +56,50 @@ class Solution2(object):
         chars = ''.join(k * (v / 2) for k, v in cnt.iteritems())
         return [''.join(half_palindrome + mid + half_palindrome[::-1]) \
                 for half_palindrome in set(itertools.permutations(chars))] if len(mid) < 2 else []
+    
+    
+    
+    
+    
+    
+or:
+    
+    
+    class Solution(object):
+    def generatePalindromes(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        m = defaultdict(int)
+        for c in s:
+            m[c] += 1
+
+        odd = None
+        for k, v in m.items():
+            if v % 2 == 1:
+                if odd is not None:
+                    return []
+                odd = k
+
+        cur = ""
+        if odd:
+            m[odd] -= 1
+            cur = odd
+
+        ret = []
+        # actually only need to build half
+        self.grow(s, m, None, cur, ret)
+        return ret
+
+    def grow(self, s, count_map, pi, cur, ret):
+        if len(cur) == len(s):
+            ret.append(cur)
+            return
+
+        for k in count_map.keys():
+            if k != pi and count_map[k] > 0:
+                for i in xrange(1, count_map[k]/2+1):  # jump the parent
+                    count_map[k] -= i*2
+                    self.grow(s, count_map, k, k*i+cur+k*i, ret)
+                    count_map[k] += i*2
