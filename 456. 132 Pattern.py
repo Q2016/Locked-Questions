c9b562b6813cb3f7c70a6da7f60af1788c184ef1
Q2016 <k.sohraby@gmail.com>
2022-01-14 07:29:38 -0800

    Create 456. 132 Pattern.py

---
+++ 456. 132 Pattern.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        ak = float("-inf")
+        st = []
+        for i in reversed(xrange(len(nums))):
+            if nums[i] < ak:
+                return True
+            else:
+                while st and nums[i] > st[-1]:
+                    ak = st.pop()
+            st.append(nums[i])
+        return False
