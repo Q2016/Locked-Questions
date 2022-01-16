3610ced61c929af97638d967ba6a26cee5f9ebcb
Q2016 <k.sohraby@gmail.com>
2022-01-16 09:43:20 -0800

    Create 179. Largest Number.py

---
+++ 179. Largest Number.py
@@ -0,0 +1,25 @@
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+        
+class Solution:
+    def largestNumber(self, nums):
+        largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        return '0' if largest_num[0] == '0' else largest_num
+      
+      
+      
+Complexity Analysis
+
+Time complexity : O(nlgn)
+
+Although we are doing extra work in our comparator, it is only by a constant factor. 
+Therefore, the overall runtime is dominated by the complexity of sort, which is O(nlgn) in Python and Java.
+
+Space complexity : O(n)
+
+Here, we allocate O(n) additional space to store the copy of nums. Although we could do
+that work in place (if we decide that it is okay to modify nums), we must allocate O(n) space for t
+he final return string. Therefore, the overall memory footprint is linear in the length of nums.
+
+      
