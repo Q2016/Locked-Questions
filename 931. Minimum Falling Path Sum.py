3f731b5fcb366b78c5c083224e6df0b5f2a8cbc9
Q2016 <k.sohraby@gmail.com>
2022-01-16 08:51:22 -0800

    Create 931. Minimum Falling Path Sum.py

---
+++ 931. Minimum Falling Path Sum.py
@@ -0,0 +1,37 @@
+https://leetcode.com/problems/minimum-falling-path-sum/discuss/381898/Python-Not-shortest-but-easiest-to-understand-with-explanation
+
+#this problem will use DP 
+#by taking the minimum value from itself plus one of the 3 values right above it
+
+#EX: 
+# 1  2  3   
+# 4  5  6  
+# 7  8  9 
+
+# new value for number at A[1][1] will be  min(5 + 1, 5 + 2, 5 + 3)
+# therefore it will be 5 + 1 = 6, and 6 will then replace the value at A[1][1]
+
+#new value for number at A[1][0] will be  min(4 + 1, 4 + 2) = 5
+#it will only have two values to compare since there is no upper left value
+
+#new value for number at A[1][2] will be  min(6 + 2, 6 + 3) = 8
+#it will only have two values to compare since there is no upper right value
+
+def minFallingPathSum(self, A: List[List[int]]) -> int:
+    for i in range(1,len(A)):
+        for j in range(len(A[0])):
+		
+            #edge cases are first column and last column which only have two paths from above
+            if j == 0:
+                A[i][j]  = min((A[i][j] + A[i - 1][j]), (A[i][j] + A[i - 1][j + 1]) )
+				
+            elif (j == len(A[0]) - 1):
+                A[i][j]  = min((A[i][j] + A[i - 1][j]), (A[i][j] + A[i - 1][j - 1]) )
+				
+            #every other column will have three paths coming from above
+            else:
+                A[i][j] = min(A[i][j] + A[i - 1][j],A[i][j] + A[i - 1][j + 1], A[i][j] + A[i - 1][j - 1])
+        
+	# Now that minimum falling sums for each value at the bottom row have been computer
+	# We can just take the min of the bottow row to get the smallest overall path sum 
+    return min(A[len(A) - 1])
