27e5cc4462286ff34560e86c600bb9d84ee90245
Q2016 <k.sohraby@gmail.com>
2022-01-16 18:03:22 -0800

    Create 1123. Lowest Common Ancestor of Deepest Leaves.py

---
+++ 1123. Lowest Common Ancestor of Deepest Leaves.py
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if(!root)
+            return NULL;
+        int l = countDepth(root->left);
+        int r = countDepth(root->right);
+        if(l == r)
+            return root;
+        else if(l < r)
+            return lcaDeepestLeaves(root->right);
+        else
+            return lcaDeepestLeaves(root->left);
+    }
+    
+    int countDepth(TreeNode* root){
+        if(!root)
+            return 0;
+        if(!depth.count(root))
+            depth[root] = 1 + max(countDepth(root->left), countDepth(root->right));
+        return depth[root];
+		
+	private:
+		unordered_map<TreeNode*, int> depth;
+    }
+};
