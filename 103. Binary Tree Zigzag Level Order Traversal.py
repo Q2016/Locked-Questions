44ad2cc777cf70a9ff876fcc17963b9d7fdb43fb
Q2016 <k.sohraby@gmail.com>
2022-01-20 18:17:35 -0800

    Update 103. Binary Tree Zigzag Level Order Traversal.py

--- 103. Binary Tree Zigzag Level Order Traversal.py
+++ 103. Binary Tree Zigzag Level Order Traversal.py
@@ -1,3 +1,22 @@
+Question:
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. 
+(i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+Example 1:
+    3
+  /   \
+ 9    20
+     /   \
+   15     7
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]    
+
+Solution: BFS
+    
+    
 https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/749036/Python-Clean-BFS-solution-explained
 
 
