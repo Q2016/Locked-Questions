c68f1ddd20cf633e740f4b01a378d81178202e1e
Q2016 <k.sohraby@gmail.com>
2022-01-20 19:11:53 -0800

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.py

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,47 +1,39 @@
-class Solution {
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
-    }
-};
+Question:
+    
+Construct Binary Tree from Preorder and Inorder Traversal    
 
-var buildTree = function(preorder, inorder) {
-    p = i = 0
-    build = function(stop) {
-        if (inorder[i] != stop) {
-            var root = new TreeNode(preorder[p++])
-            root.left = build(root.val)
-            i++
-            root.right = build(stop)
+Solution: Recursion
+    
+    
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/solution/    
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+
+        def array_to_tree(left, right):
+            nonlocal preorder_index
+            # if there are no elements to construct the tree
+            if left > right: return None
+
+            # select the preorder_index element as the root and increment it
+            root_value = preorder[preorder_index]
+            root = TreeNode(root_value)
+
+
+            preorder_index += 1
+
+            # build left and right subtree
+            # excluding inorder_index_map[root_value] element because it's the root
+            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
+            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
+
             return root
-        }
-        return null
-    }
-    return build()
-};
 
-Explanation/Discussion:
+        preorder_index = 0
 
-Consider this input:
+        # build a hashmap to store value -> its index relations
+        inorder_index_map = {}
+        for index, value in enumerate(inorder):
+            inorder_index_map[value] = index
 
-preorder: [1, 2, 4, 5, 3, 6]
-inorder: [4, 2, 5, 1, 6, 3]
-The obvious way to build the tree is:
-
-Use the first element of preorder, the 1, as root.
-Search it in inorder.
-Split inorder by it, here into [4, 2, 5] and [6, 3].
-Split the rest of preorder into two parts as large as the inorder parts, here into [2, 4, 5] and [3, 6].
-Use preorder = [2, 4, 5] and inorder = [4, 2, 5] to add the left subtree.
-Use preorder =[3, 6]andinorder = [6, 3] to add the right subtree.
-But consider the worst case for this: A tree that's not balanced but is just a straight line to the left. Then inorder is the reverse of preorder, and already the cost of step 2, searching in inorder, is O(n^2) overall. Also, depending on how you "split" the arrays, you're looking at O(n^2) runtime and possibly O(n^2) space for that as well.
-
-You can bring the runtime for searching down to O(n) by building a map from value to index before you start the main work, and I've seen several solutions do that. But that is O(n) additional space, and also the splitting problems remain. To fix those, you can use pointers into preorder and inorder instead of splitting them. And when you're doing that, you don't need the value-to-index map, either.
-
-Consider the example again. Instead of finding the 1 in inorder, splitting the arrays into parts and recursing on them, just recurse on the full remaining arrays and stop when you come across the 1 in inorder. That's what my above solution does. Each recursive call gets told where to stop, and it tells its subcalls where to stop. It gives its own root value as stopper to its left subcall and its parent`s stopper as stopper to its right subcall.
-
-Language details:
-
-Small trick in my Javascript solution: The top-most call doesn't explicitly get a stopper value, so its stop is undefined. Which is good, because that's also what inorder[i] is when we have consumed all values, i.e., when i is inorder.length.
-
-About the Python solution: I'm not sure there's a good way to have those p and i pointers that I use in my Javascript solution, so instead I opted for popping elements from preorder and inorder. Since popping from the front with pop(0) is expensive O(n), I reverse them before I start so I can use the cheap O(1) popping from the back.
+        return array_to_tree(0, len(preorder) - 1)
