99ebd194c31d24f00a30d3c1406987af9554a56e
Q2016 <k.sohraby@gmail.com>
2022-01-22 08:40:09 -0800

    Update 1091. Shortest Path in Binary Matrix.py

--- 1091. Shortest Path in Binary Matrix.py
+++ 1091. Shortest Path in Binary Matrix.py
@@ -1,3 +1,14 @@
+Question:
+Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. 
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected. 
+The length of a clear path is the number of visited cells of this path.	
+
+
+Solution: Optimized BFS
+
+
 Solution from:
 https://leetcode.com/problems/shortest-path-in-binary-matrix/discuss/313347/A*-search-in-Python
 
