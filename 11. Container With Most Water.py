777733d7a6cab34933d2e8e4ca4059ef97d27736
Q2016 <k.sohraby@gmail.com>
2022-01-22 08:57:22 -0800

    Update 11. Container With Most Water.py

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -1,4 +1,9 @@
+Question:
+There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.    
 
+Solution: Two pointers & Greedy
 
 Idea / Proof:
 
