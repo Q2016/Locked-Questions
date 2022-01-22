c2707b9d680dc87362728193f832006a798d4750
Q2016 <k.sohraby@gmail.com>
2022-01-21 18:02:14 -0800

    Update 1066. Campus Bikes II.py

--- 1066. Campus Bikes II.py
+++ 1066. Campus Bikes II.py
@@ -1,9 +1,9 @@
-On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. Each worker and bike is a 2D coordinate on this grid.
-
-We assign one unique bike to each worker so that the sum of the Manhattan distances between each worker and their assigned bike is minimized.
-
+Question:
+On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. 
+Each worker and bike is a 2D coordinate on this grid.
+We assign one unique bike to each worker so that the sum of the Manhattan distances 
+between each worker and their assigned bike is minimized.
 The Manhattan distance between two points p1 and p2 is Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
-
 Return the minimum possible sum of Manhattan distances between each worker and their assigned bike.
 
 
@@ -29,7 +29,8 @@ Output:
 4
 Explanation: 
 
-We first assign bike 0 to worker 0, then assign bike 1 to worker 1 or worker 2, bike 2 to worker 2 or worker 1. Both assignments lead to sum of the Manhattan distances as 4.
+We first assign bike 0 to worker 0, then assign bike 1 to worker 1 or worker 2, bike 2 to worker 2 or worker 1. 
+Both assignments lead to sum of the Manhattan distances as 4.
 
 
 
