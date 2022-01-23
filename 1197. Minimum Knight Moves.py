c8f70741a91a224653d211a1ed8a863f00ee90c0
Q2016 <k.sohraby@gmail.com>
2022-01-23 13:00:46 -0800

    Update 1197. Minimum Knight Moves.py

--- 1197. Minimum Knight Moves.py
+++ 1197. Minimum Knight Moves.py
@@ -1,14 +1,13 @@
-https://goodtecher.com/leetcode-1197-minimum-knight-moves/
-
+Question:
 In an infinite chess board with coordinates from -infinity to +infinity, you have a knight at square [0, 0].
-
 A knight has 8 possible moves it can make, as illustrated below. Each move is two squares in a 
 cardinal direction, then one square in an orthogonal direction.
-<image>
 Return the minimum number of steps needed to move the knight to the square [x, y]. 
 
 
 
+Solution: BFS?
+https://goodtecher.com/leetcode-1197-minimum-knight-moves/
 
 A Knight movement can be summarised as below in terms of coordinate increments considering a current position as x, y:
 1. (x-1, y+2) ==> (-1, +2)
@@ -26,16 +25,11 @@ Also since the initial position is at (0, 0) we can restrict the moves in the po
 quadrant. We can do so if we take the absolute value of the target coordinate. We can do so since we 
 only need to find the minimum number of steps. Also, we keep track of visiting positions.
 
-Code Implementation:
 
   
   def minKnightMoves(x, y):
     x , y = abs(x), abs(y)
-    possible_moves = [
-        (1, 2), (2, 1), (-1, 2), 
-        (-2, 1), (-1, -2), (-2, -1), 
-        (1, -2), (2, -1)
-        ]
+    possible_moves = [(1, 2), (2, 1), (-1, 2),(-2, 1), (-1, -2), (-2, -1),(1, -2), (2, -1)]
     
     que = collections.deque([[0, 0, 0]])
     visited = set()
@@ -53,5 +47,5 @@ Code Implementation:
                 
 Complexity Analysis:
 Time complexity: O(max(x²,y²)). This is due to the fact that every place the Knight covers a radius of 2x or 2y. 
-  The number of cells inside this circle is the max of x² or y².
+The number of cells inside this circle is the max of x² or y².
 Constant space: O(max(x²,y²)) at any level we store all nodes that can be reached from the previous level.
