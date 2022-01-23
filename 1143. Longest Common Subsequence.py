e7b11606c2698199031b2a7fd55172a3cb3a5817
Q2016 <k.sohraby@gmail.com>
2022-01-23 10:46:01 -0800

    Update 1143. Longest Common Subsequence.py

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -1,4 +1,23 @@
-class Solution {//https://en.m.wikipedia.org/wiki/Longest_common_subsequence_problem
+Question:
+Given two strings text1 and text2, return the length of their longest common subsequence. 
+A subsequence of a string is a new string generated from the original string with some 
+characters (can be none) deleted without changing the relative order of the remaining characters.
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.    
+    
+    
+    
+Solution: DP
+
+//https://en.m.wikipedia.org/wiki/Longest_common_subsequence_problem    
+
+class Solution {
 public:
     int longestCommonSubsequence(string a, string b) {
     vector<vector<short>> m(a.size() + 1, vector<short>(b.size() + 1));
