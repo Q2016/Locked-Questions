0792a15bc98859d46d5784f5703bbd5146598a8b
Q2016 <k.sohraby@gmail.com>
2022-01-23 10:43:36 -0800

    Update 1143. Longest Common Subsequence.py

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -5,8 +5,11 @@ public:
   
     for (auto i = 1; i <= a.size(); ++i)
         for (auto j = 1; j <= b.size(); ++j)
-            if (a[i - 1] == b[j - 1]) m[i][j] = m[i - 1][j - 1] + 1;
-            else m[i][j] = max(m[i - 1][j], m[i][j - 1]);
-  return m[a.size()][b.size()];
+            if (a[i - 1] == b[j - 1]) 
+                m[i][j] = m[i - 1][j - 1] + 1;
+            else 
+                m[i][j] = max(m[i - 1][j], m[i][j - 1]);
+  
+    return m[a.size()][b.size()];
     }
 };
