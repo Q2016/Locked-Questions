52dfcd8f0755b0f471382df31cc885cd9a724d63
Q2016 <k.sohraby@gmail.com>
2022-01-23 12:03:41 -0800

    Update 1167. Minimum Cost to Connect Sticks.py

--- 1167. Minimum Cost to Connect Sticks.py
+++ 1167. Minimum Cost to Connect Sticks.py
@@ -1,24 +1,19 @@
-Explain
-https://www.youtube.com/watch?v=3dqR2nYElyw
-
-
+Question:
 You have some sticks with positive integer lengths.
-
 You can connect any two sticks of lengths X and Y into one stick by paying a cost of X + Y.  
 You perform this action until there is one stick remaining.
-
 Return the minimum cost of connecting all the given sticks into one stick in this way.
 
 Example 1:
-
 Input: sticks = [2,4,3]
-Output: 14
-Example 2:
-
-Input: sticks = [1,8,3,5]
-Output: 30
+Output: 14 2+3=5 ,5+3=8  5+8???
 
 
+
+Solution:  Heapify  
+    
+Explain
+https://www.youtube.com/watch?v=3dqR2nYElyw
 
 Intuition:
 This is a direct application of the Huffman Coding algorithm. In this problem, each stick's 
