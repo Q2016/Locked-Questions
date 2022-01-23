4c0daa67d82427f30083f2b3b813c2ff43d76f8d
Q2016 <k.sohraby@gmail.com>
2022-01-23 11:55:54 -0800

    Update 1161. Maximum Level Sum of a Binary Tree.py

--- 1161. Maximum Level Sum of a Binary Tree.py
+++ 1161. Maximum Level Sum of a Binary Tree.py
@@ -1,29 +1,26 @@
-standard queue level order traversal.
+Question:
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal    
 
-tmp_sum records sum of current level, global_sum records largest level sum so far.
+Solution: BFS
 
-If tmp_sum > global_sum, it means sum of current level is large, assign current level to res.
-
-class Solution {
-public:
-    int maxLevelSum(TreeNode* root) {
-        if (root == NULL) return 0;
-        int res = 0, level = 0, global_sum = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int count = q.size(), tmp_sum = 0;
-            while (count--) {
-                TreeNode* tmp = q.front();
-                q.pop();
-                tmp_sum += tmp->val;
-                if (tmp->left) q.push(tmp->left);
-                if (tmp->right) q.push(tmp->right);
-            }
-            level++;
-            if (tmp_sum > global_sum) res = level;
-            global_sum = max(tmp_sum, global_sum);
-        }
-        return res;
-    }
-};
+Use BFS to find the sum of each level, then locate the level with largest sum.    
+    
+    
+    def maxLevelSum(self, root: TreeNode) -> int:
+        max, level, maxLevel = -float('inf'), 0, 0
+        q = collections.deque()
+        q.append(root)
+        while q:
+            level += 1
+            sum = 0
+            for _ in range(len(q)):
+                node = q.popleft()
+                sum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if max < sum:
+                max, maxLevel = sum, level        
+        return maxLevel
