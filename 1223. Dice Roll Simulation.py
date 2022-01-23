4be34464a60547db7eac0823da6393f09b71fded
Q2016 <k.sohraby@gmail.com>
2022-01-23 14:30:50 -0800

    Update 1223. Dice Roll Simulation.py

--- 1223. Dice Roll Simulation.py
+++ 1223. Dice Roll Simulation.py
@@ -1,27 +1,48 @@
+Question:
+A die simulator generates a random number from 1 to 6 for each roll. You introduced a constraint 
+to the generator such that it cannot roll the number i more than rollMax[i] consecutive times.
+Given an array of integers rollMax and an integer n, return the number of distinct sequences 
+that can be obtained with exact n rolls. 
 
-Solution:
+Example 1:
+Input: n = 2, rollMax = [1,1,2,2,2,3]
+Output: 34
+Explanation: There will be 2 rolls of die, if there are no constraints on the die, 
+there are 6 * 6 = 36 possible combinations. In this case, looking at rollMax array, 
+the numbers 1 and 2 appear at most once consecutively, therefore sequences (1,1) and (2,2) 
+cannot occur, so the final answer is 36-2 = 34.    
+
+
+
+Solution: Difficult??
+
 https://leetcode.com/problems/dice-roll-simulation/discuss/404840/Short-Python-DP-with-detailed-image-explanation  
 
 
 Here I illustrate the idea with the example: Input: n = 3, rollMax = [1,1,1,2,2,3]. Output: 181.
 
-Also the definition of DP array:
+Definition of DP array:
 
-for j = 0, 1, ..., faces - 1
-dp[i][j] means how many combinations it could be that at i-th rolling and the last face is j
-for j = faces
-dp[i][j] means how many combinations it could be that at i-th rolling in total
+-for j = 0, 1, ..., faces - 1
+    dp[i][j] means how many combinations it could be that at i-th rolling and the last face is j
+-for j = faces
+    dp[i][j] means how many combinations it could be that at i-th rolling in total
 Basically on every [i][j], we are trying to climb up in the column [j], and how many steps we could climb up is based on rollMax[j].
 
-image
 
-Another example: let's say we roll dice i = 5 times, and the last face is j = 5, then we have this sequence xxxx5 where x could ranges from 0 to 5 arbitrarily if we are not restricted. Then let's calculate how many combinations we could form xxxx5 with constraint rollMax[j] = 3. They could be categorized as follow:
+
+Another example: let's say we roll dice i = 5 times, and the last face is j = 5, then we have this sequence 
+xxxx5 where x could ranges from 0 to 5 arbitrarily if we are not restricted. Then let's calculate how many 
+combinations we could form xxxx5 with constraint rollMax[j] = 3. They could be categorized as follow:
 
 step1: xxxy5, where x in [0, 5] and y in [0, 4], this means from the end 5, we have consecutive one 5
 step2: xxy55, this means from the end 5, we have consecutive two 5
 step3: xy555, this means from the end 5, we have consecutive three 5
 We could not climb the column upper because we are restricted to only be able to climb up rollMax[j] = 3 steps.
-Then the question is how to calculate the combinations of ...xy. Actually if you take a deeper look, what ...xy means is essentially: give me all the combinations as long as the last face is not 5. We don't care what x should be here because it is not restricted and could choose any value (it could be even same as y, or same as 5). As long as y is not equal to 5, we are good to go. And that is the definition dp[i][j]!
+Then the question is how to calculate the combinations of ...xy. Actually if you take a deeper look, what ...xy 
+means is essentially: give me all the combinations as long as the last face is not 5. We don't care what x should 
+be here because it is not restricted and could choose any value (it could be even same as y, or same as 5). 
+As long as y is not equal to 5, we are good to go. And that is the definition dp[i][j]!
 
 class Solution:
     def dieSimulator(self, n: int, rollMax: List[int]) -> int:
@@ -50,4 +71,4 @@ class Solution:
             # update total sum of this row
             dp[i][faces] = sum(dp[i])
         
-        return dp[n][faces] % 1000000007
+        return dp[n][faces]
