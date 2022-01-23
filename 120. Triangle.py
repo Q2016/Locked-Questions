343271e25d07153d9172b8d487f0ad82bca2c12f
Q2016 <k.sohraby@gmail.com>
2022-01-23 13:23:07 -0800

    Update 120. Triangle.py

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -1,10 +1,29 @@
+Question:
+Given a triangle array, return the minimum path sum from top to bottom.
+For each step, you may move to an adjacent number of the row below. More formally, 
+if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+Example 1:
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   _2
+  _3 4
+ 6 _5 7
+4 _1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).    
+
 # Note:
 # Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
 #
 
+
+
+Solution: DP
+
+
 class Solution:
-    # @param triangle, a list of lists of integers
-    # @return an integer
+
     def minimumTotal(self, triangle):
         if not triangle:
             return 0
