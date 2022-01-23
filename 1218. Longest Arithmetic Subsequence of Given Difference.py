6a5231ef33dffdb210b3aa17352c1949015a01c7
Q2016 <k.sohraby@gmail.com>
2022-01-23 13:42:59 -0800

    Update 1218. Longest Arithmetic Subsequence of Given Difference.py

--- 1218. Longest Arithmetic Subsequence of Given Difference.py
+++ 1218. Longest Arithmetic Subsequence of Given Difference.py
@@ -1,3 +1,18 @@
+Question:
+Given an integer array arr and an integer difference, return the length of the longest 
+subsequence in arr which is an arithmetic sequence such that the difference between adjacent 
+elements in the subsequence equals difference.
+A subsequence is a sequence that can be derived from arr by deleting some or no elements without 
+changing the order of the remaining elements.
+
+Example 1:
+Input: arr = [1,2,3,4], difference = 1
+Output: 4
+Explanation: The longest arithmetic subsequence is [1,2,3,4].        
+
+        
+Solution:  DP
+        
 def longestSubsequence(self, arr: List[int], diff: int) -> int:
         res = {}
         for num in arr:
