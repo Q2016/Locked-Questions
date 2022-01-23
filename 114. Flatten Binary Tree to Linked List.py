76b35d5a93ceb6be6fbcfe3f4ae1d7b9299a7cf9
Q2016 <k.sohraby@gmail.com>
2022-01-23 10:37:24 -0800

    Update 114. Flatten Binary Tree to Linked List.py

--- 114. Flatten Binary Tree to Linked List.py
+++ 114. Flatten Binary Tree to Linked List.py
@@ -1,3 +1,16 @@
+Question:
+Given the root of a binary tree, flatten the tree into a "linked list".    
+
+                    1          =>   1->2->3->4->5->6
+                  /   \
+                2       5
+              /   \       \
+            3       4       6
+
+Solution: Preorder? Postorder?
+
+
+
 private TreeNode prev = null;
 
 public void flatten(TreeNode root) {
