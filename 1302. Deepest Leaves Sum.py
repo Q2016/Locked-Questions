d813877a75efefeac4351ff72f6dcbd8045589c4
Q2016 <k.sohraby@gmail.com>
2022-01-24 07:10:49 -0800

    Update 1302. Deepest Leaves Sum.py

--- 1302. Deepest Leaves Sum.py
+++ 1302. Deepest Leaves Sum.py
@@ -1,7 +1,6 @@
 Question:
 Given the root of a binary tree, return the sum of values of its deepest leaves.
  
-
 Example 1:
 Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
 Output: 15    
@@ -14,13 +13,10 @@ Output: 15
          /                     \ 
         7                       8
     
+    
 Solution: BFS    
 
     def deepestLeavesSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
         q = [] 
         res =  []
         t = []
