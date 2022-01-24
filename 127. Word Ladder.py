9f5c2c5e18bc39f33cdb7bc4dd8adf8aef37217d
Q2016 <k.sohraby@gmail.com>
2022-01-23 20:55:18 -0800

    Update and rename 127. Word Ladder to 127. Word Ladder.py

---
+++ 127. Word Ladder.py
@@ -0,0 +1,42 @@
+Question:
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a 
+sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the 
+shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+Example 1:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+    
+    
+
+Solution: BFS
+
+https://github.com/ShiqinHuo/LeetCode-Python/blob/master/Python/word-ladder.py
+
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+
+        distance, cur, visited, lookup = 0, [beginWord], set([beginWord]), set(wordList)
+
+        while cur:
+            next_queue = []
+
+            for word in cur:
+                if word == endWord:
+                    return distance + 1
+                for i in xrange(len(word)):
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        candidate = word[:i] + j + word[i + 1:]
+                        if candidate not in visited and candidate in lookup:
+                            next_queue.append(candidate)
+                            visited.add(candidate)
+            distance += 1
+            cur = next_queue
+
+        return 0
