7b17944ae0c7404fe0d68357a78dbe6f52ebb9f7
Q2016 <k.sohraby@gmail.com>
2022-01-24 06:08:46 -0800

    Update 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py

--- 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py
+++ 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py
@@ -1,13 +1,24 @@
+Question:
+Given a m x n matrix mat and an integer threshold, return the maximum side-length of a square with a sum less 
+than or equal to threshold or return 0 if there is no such square.
+
+Example 1:
+Input: mat = [[|1,1|,3,2,4,3,2],
+              [|1,1|,3,2,4,3,2],
+              [1,1,3,2,4,3,2]], threshold = 4
+Output: 2
+Explanation: The maximum side length of square with sum less than 4 is 2 as shown.
+    
+    
+    
+Solution:   DP + Prefix sum + Binary search
+
 Use dynamic programming to store the prefix sum. Since value in matrix is positive, 
 so we can use binary search to find the appropriate square length k.
 
 class Solution(object):
     def maxSideLength(self, mat, threshold):
-        """
-        :type mat: List[List[int]]
-        :type threshold: int
-        :rtype: int
-        """
+
         if not mat:
             return 0
         max_square = 0
