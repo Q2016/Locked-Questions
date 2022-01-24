a8a80f962e6b1410ebae05251bdae4420dc3951b
Q2016 <k.sohraby@gmail.com>
2022-01-24 07:10:27 -0800

    Update 1302. Deepest Leaves Sum.py

--- 1302. Deepest Leaves Sum.py
+++ 1302. Deepest Leaves Sum.py
@@ -1,17 +1,42 @@
-class Solution {
-public:
-    int deepestLeavesSum(TreeNode* root) {
-        int res = 0, i;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (q.size()) {
-            for (i = q.size() - 1, res = 0; i >= 0; --i) {
-                TreeNode* node = q.front(); q.pop();
-                res += node->val;
-                if (node->right) q.push(node->right);
-                if (node->left)  q.push(node->left);
-            }
-        }
-        return res;
-    }
-};
+Question:
+Given the root of a binary tree, return the sum of values of its deepest leaves.
+ 
+
+Example 1:
+Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15    
+
+                    1
+                 /     \
+               2         3  
+            /    \         \   
+          4       5          6
+         /                     \ 
+        7                       8
+    
+Solution: BFS    
+
+    def deepestLeavesSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        q = [] 
+        res =  []
+        t = []
+        if root is None:
+            return res
+        q.append(root)
+        while q:
+            t=[]  #empty list for each level
+            level = len(q)
+            while level != 0:
+                temp = q.pop(0)
+                level -= 1 
+                t.append(temp.val)
+                if temp.left:
+                    q.append(temp.left) ###storing elemets of the tree level wise
+                if temp.right:    
+                    q.append(temp.right) ###storing elemets of the tree level wise
+            res.append(t) ###final level stored in t
+        return sum(t)
