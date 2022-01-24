0b4126118d3b669a030cb91594e5a8cbf2d9d999
Q2016 <k.sohraby@gmail.com>
2022-01-23 16:39:24 -0800

    Update 1254. Number of Closed Islands.py

--- 1254. Number of Closed Islands.py
+++ 1254. Number of Closed Islands.py
@@ -1,7 +1,41 @@
+Question:
+Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 
+4-directionally connected group of 0s and a closed island is an island totally 
+(all left, top, right, bottom) surrounded by 1s.
+Return the number of closed islands.
+
+Example 1:
+Input: grid = [[1,1,1,1,1,1,1,0],
+	       [1,0,0,0,0,1,1,0],
+	       [1,0,1,0,1,1,1,0],
+	       [1,0,0,0,0,1,0,1],
+	       [1,1,1,1,1,1,1,0]]
+Output: 2
+Explanation: 
+Islands in gray are closed because they are completely surrounded by water (group of 1s).	
 
 
-class Solution {
- public:   
+Solution: DFS
+   
+Intuition
+The idea is simple. We want to perform a DFS on the matrix to find the connected component of 0s. 
+Call a connnected component valid if none of its element lies on the border of the matrix. 
+It is clear that we need to find all the valid connected component.
+We consider a cell with value 1 as a blocked cell and a cell with value 0 an empty cell. 
+We also need to maintain a visited matrix, but we can just modify the input matrix, 
+setting each 0 to 1 after we have visited it.
+Now, all that remains is to start a DFS from the first unvisited 0. This DFS call would cover 
+all the elements of this connected component (As per the property of DFS). Now, we just need 
+to keep track whether any element in this connected component touches the boundary of the matrix or not. 
+If it does, this connected component becomes invalid. If none of the elements touch the boundary, 
+it means that it is surrounded by1s on all the sides. Hence, we increment the counter for answer.
+To check whether any element in the connected componenet is a terminal node, we can just maintain a 
+boolean variable called terminal and set it to true whenver we see a terminal node during the DFS call. 
+After the DFS, we only increment the counter if terminal is set to false.
+How do we look out for terminal nodes? Note that if we hit a terminal node, we are also going to 
+hit an out of bound neighbour. Hence, if we see an out of bounds index, we update the terminal to True
+
+
     int closedIsland(vector<vector<int>>& g) {
 	//count the number
         int count = 0; 
@@ -30,19 +64,8 @@ class Solution {
          int right = dfs(g,row,col+1);
          return up & down & left & right;//any of the side meet the edge,is not a island;
     }
-};
 
 
 
-/*
-Intuition
-The idea is simple. We want to perform a DFS on the matrix to find the connected component of 0s. Call a connnected component valid if none of its element lies on the border of the matrix. It is clear that we need to find all the valid connected component.
 
-We consider a cell with value 1 as a blocked cell and a cell with value 0 an empty cell. We also need to maintain a visited matrix, but we can just modify the input matrix, setting each 0 to 1 after we have visited it.
 
-Now, all that remains is to start a DFS from the first unvisited 0. This DFS call would cover all the elements of this connected component (As per the property of DFS). Now, we just need to keep track whether any element in this connected component touches the boundary of the matrix or not. If it does, this connected component becomes invalid. If none of the elements touch the boundary, it means that it is surrounded by1s on all the sides. Hence, we increment the counter for answer.
-
-To check whether any element in the connected componenet is a terminal node, we can just maintain a boolean variable called terminal and set it to true whenver we see a terminal node during the DFS call. After the DFS, we only increment the counter if terminal is set to false.
-
-How do we look out for terminal nodes? Note that if we hit a terminal node, we are also going to hit an out of bound neighbour. Hence, if we see an out of bounds index, we update the terminal to True
-*/
