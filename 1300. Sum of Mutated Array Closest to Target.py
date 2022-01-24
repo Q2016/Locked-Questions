fce8cba5c487e8447f1c0b6c679af3852568ae29
Q2016 <k.sohraby@gmail.com>
2022-01-24 06:56:07 -0800

    Update 1300. Sum of Mutated Array Closest to Target.py

--- 1300. Sum of Mutated Array Closest to Target.py
+++ 1300. Sum of Mutated Array Closest to Target.py
@@ -1,31 +1,40 @@
-class Solution {
-public:
-    int findBestValue(vector<int>& arr, int target) {
-        int sum = 0;
-        int temp = -10000000;
-        for (int a : arr) {
-            sum += a;
-            temp = max(temp, a);
-        }
-        if (sum == target) return temp;//max
-        int min = 0; 
-        int res = 1;
-        int diff = 10000000;
-        // The answer would lie between 0 and maximum value in the array.
-        while (min <= temp) {
-            int mid = min + (temp - min) / 2;
-            sum = getMutatedSum(arr, mid); 
-            if (sum > target) {
-                temp = mid - 1;
-            } else {
-                min = mid + 1;
-            }
-            // If current difference is less than diff || current 
-            //difference==diff but mid < res.(choose the smaller one.)
-            if (abs(sum - target) < diff || (abs(sum - target) == diff && mid < res)) { 
-                res = mid;
-                diff = abs(sum - target);
-            }
-        }
-        return res;
-    }
+Question:
+Given an integer array arr and a target value target, return the integer value such that 
+when we change all the integers larger than value in the given array to be equal to value, 
+the sum of the array gets as close as possible (in absolute difference) to target.
+In case of a tie, return the minimum such integer.
+    
+Example 1:
+Input: arr = [4,9,3], target = 10
+Output: 3
+Explanation: When using 3 arr converts to [3, 3, 3] which sums 9 and that's the optimal answer.
+
+    
+Solution: Binary Search
+
+class Solution(object):
+    def getRes(self,arr,t):
+        nums = [t if num >= t else num for num in arr]
+        return sum(nums)
+    
+    def findBestValue(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 1
+        h = max(arr)
+        
+        while l <= h:
+            mid = (h-l)//2 + l
+            curr = self.getRes(arr,mid)
+            if curr == target:
+                return mid
+            elif curr < target:
+                l = mid+1
+            else:
+                h = mid-1
+        if abs(self.getRes(arr,l) - target) < abs(self.getRes(arr,h) - target):
+            return l
+        return h
