6a0de5e38424a73985b236488f129b444a1ab268
Q2016 <k.sohraby@gmail.com>
2022-01-25 08:25:36 -0800

    Update 1424. Diagonal Traverse II.py

--- 1424. Diagonal Traverse II.py
+++ 1424. Diagonal Traverse II.py
@@ -1,10 +1,21 @@
-https://leetcode.com/problems/diagonal-traverse-ii/discuss/597980/Python-Two-simple-solutions-(dictionary-or-sort)-O(n)
+Question:
+Given a 2D integer array nums, return all elements of nums in diagonal order as shown in the below images.
 
+Example 1:
+Input: nums = [[1,2,3],
+	       [4,5,6],
+	       [7,8,9]]
+Output: [1,4,2,7,5,3,8,6,9]	
+	
+	
+Solution: Dictionary	
+
+https://leetcode.com/problems/diagonal-traverse-ii/discuss/597980/Python-Two-simple-solutions-(dictionary-or-sort)-O(n)
 
 The first solution is to use dictionary. Complexity O(n)
 Record the (r+c) as the key, then reverse list for each (r+c) and append it to the result.
 
-class Solution:
+
 	def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
 		D = collections.defaultdict(list)
 		R = len(nums)
