d7c3646aac7f00657406602b5270489b83ad4ab4
Q2016 <k.sohraby@gmail.com>
2022-01-25 07:49:28 -0800

    Update 143. Reorder List.py

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -1,17 +1,25 @@
+Question:
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.	
+
+
+Solution:---
+
 https://leetcode.com/problems/reorder-list/discuss/1640597/PythonJavaC%2B%2B-2-Easy-Solutions-oror-Visualized-Explanation-oror-Beginner-Friendly
 
-
 step 1: find the middle pointer of the linked list and split the linked list into two halves using slow and fast pointers
-								head = [1, 2, 3]
-head = [1, 2, 3, 4, 5]    =>
-								mid  = [4, 5]
+head = [1, 2, 3, 4, 5]    => head = [1, 2, 3]   mid  = [4, 5]		
 step 2: reverse the second half
 head = [1, 2, 3]
 mid  = [4, 5]             =>    mid = [5, 4]
 step 3: interleaving merge the first half and the reversed second half
 head = [1, 2, 3]
-					      =>    head = [1, 5, 2, 4, 3]
+		   =>    head = [1, 5, 2, 4, 3]
 mid = [5, 4]
+
 Time  Complexity: O(N)
 Space Complexity: O(1)
   
