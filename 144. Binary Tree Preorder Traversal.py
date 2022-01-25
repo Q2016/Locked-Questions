2a6a87b53ce216deff3b3f70bc952cb98a47d61b
Q2016 <k.sohraby@gmail.com>
2022-01-25 06:40:44 -0800

    Update 144. Binary Tree Preorder Traversal.py

--- 144. Binary Tree Preorder Traversal.py
+++ 144. Binary Tree Preorder Traversal.py
@@ -1,19 +1,27 @@
-Recursive solution
+Question:
+Given the root of a binary tree, return the preorder traversal of its nodes' values.    
 
-class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> nodes;
-        preorder(root, nodes);
-        return nodes;
-    }
-private:
-    void preorder(TreeNode* root, vector<int>& nodes) {
-        if (!root) {
-            return;
-        }
-        nodes.push_back(root -> val);
-        preorder(root -> left, nodes);
-        preorder(root -> right, nodes);
-    }
-};
+Solution: Recursive or iterative
+
+# recursively
+def preorderTraversal1(self, root):
+    res = []
+    self.dfs(root, res)
+    return res
+    
+def dfs(self, root, res):
+    if root:
+        res.append(root.val)
+        self.dfs(root.left, res)
+        self.dfs(root.right, res)
+
+# iteratively
+def preorderTraversal(self, root):
+    stack, res = [root], []
+    while stack:
+        node = stack.pop()
+        if node:
+            res.append(node.val)
+            stack.append(node.right)
+            stack.append(node.left)
+    return res
