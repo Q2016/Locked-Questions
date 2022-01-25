40edde459b4af3a02f476965d2d2e31dfe096a5c
Q2016 <k.sohraby@gmail.com>
2022-01-25 06:25:30 -0800

    Update 1447. Simplified Fractions.py

--- 1447. Simplified Fractions.py
+++ 1447. Simplified Fractions.py
@@ -1,4 +1,15 @@
+Question:
+Given an integer n, return a list of all simplified fractions between 0 and 1 (exclusive) such that 
+the denominator is less-than-or-equal-to n. The fractions can be in any order.
 
+Example 1:
+Input: n = 4
+Output: ["1/2","1/3","1/4","2/3","3/4"]
+Explanation: "2/4" is not a simplified fraction because it can be simplified to "1/2".    
+
+
+Solution: gcd
+    
 https://leetcode.com/problems/simplified-fractions/discuss/635260/Python3-Easy-GCD
 
 
@@ -16,6 +27,10 @@ class Solution:
                 if gcd(i, j) == 1:
                     res.append(str(j) + '/' + str(i))
         return res
+
+or:    
+    
+    
 Using Python math library
 class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
