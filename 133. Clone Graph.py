c0f2f939a1071b7bd0e3989d4d2b55d61205f12c
Q2016 <k.sohraby@gmail.com>
2022-01-25 13:40:21 -0800

    Update 133. Clone Graph.py

--- 133. Clone Graph.py
+++ 133. Clone Graph.py
@@ -1,72 +1,65 @@
-class Solution {
-public:
+Question:
+Given a reference of a node in a connected undirected graph. Return a deep copy (clone) of the graph.
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+For simplicity, each node's value is the same as the node's index (1-indexed). 
+ 
+Example 1:
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+
+Solution: DFS or BFS
     
-    unordered_map<Node*, Node*> copies;
+DFS iteratively    
+    def cloneGraph1(self, node):
+        if not node:
+            return node
+        m = {node: Node(node.val)}
+        stack = [node]
+        while stack:
+            n = stack.pop()
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    stack.append(neigh)
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+        return m[node]
+        
+        
+BFS        
+    def cloneGraph2(self, node): 
+        if not node:
+            return node
+        m = {node: Node(node.val)}
+        deque = collections.deque([node])
+        while deque:
+            n = deque.popleft()
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    deque.append(neigh)
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+        return m[node]
+        
+        
+        
+DFS recursively
 
-    Node* cloneGraph(Node* node) {
-        if (!node) {
-            return NULL;
-        }
-        if (copies.find(node) == copies.end()) {
-            copies[node] = new Node(node -> val, {});
-            for (Node* neighbor : node -> neighbors) {
-                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
-            }
-        }
-        return copies[node];
-    }
-
-};
-
-/*To clone a graph, you will need to traverse it. Both BFS and DFS are for this purpose. But that is not all you need. To clone a graph, you need to have a copy of each node and you need to avoid copying the same node for multiple times. So you still need a mapping from an original node to its copy.
-
-BFS
-
-class Solution {
-public:
-    Node* cloneGraph(Node* node) {
-        if (!node) {
-            return NULL;
-        }
-        Node* copy = new Node(node -> val, {});
-        copies[node] = copy;
-        queue<Node*> todo;
-        todo.push(node);
-        while (!todo.empty()) {
-            Node* cur = todo.front();
-            todo.pop();
-            for (Node* neighbor : cur -> neighbors) {
-                if (copies.find(neighbor) == copies.end()) {
-                    copies[neighbor] = new Node(neighbor -> val, {});
-                    todo.push(neighbor);
-                }
-                copies[cur] -> neighbors.push_back(copies[neighbor]);
-            }
-        }
-        return copy;
-    }
-private:
-    unordered_map<Node*, Node*> copies;
-};
-DFS
-
-The following succinct DFS code is taken from this post.
-
-class Solution {
-public:
-    Node* cloneGraph(Node* node) {
-        if (!node) {
-            return NULL;
-        }
-        if (copies.find(node) == copies.end()) {
-            copies[node] = new Node(node -> val, {});
-            for (Node* neighbor : node -> neighbors) {
-                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
-            }
-        }
-        return copies[node];
-    }
-private:
-    unordered_map<Node*, Node*> copies;
-};
-*/
+    def cloneGraph(self, node): 
+        if not node:
+            return node
+        m = {node: Node(node.val)}
+        self.dfs(node, m)
+        return m[node]
+    
+    def dfs(self, node, m):
+        for neigh in node.neighbors:
+            if neigh not in m:
+                m[neigh] = Node(neigh.val)
+                self.dfs(neigh, m)
+            m[node].neighbors.append(m[neigh])    
