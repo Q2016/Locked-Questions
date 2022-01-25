04df1587d80139f2fcbf1445b55f61aa2d125d42
Q2016 <k.sohraby@gmail.com>
2022-01-25 08:57:31 -0800

    Update 1423. Maximum Points You Can Obtain from Cards.py

--- 1423. Maximum Points You Can Obtain from Cards.py
+++ 1423. Maximum Points You Can Obtain from Cards.py
@@ -1,35 +1,38 @@
-class Solution {
-public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int sum = 0;
-        int n = cardPoints.size();
+Question:
+There are several cards arranged in a row, and each card has an associated number of points. 
+The points are given in the integer array cardPoints.
+In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+Your score is the sum of the points of the cards you have taken.
+Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+Example 1:
+Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+Output: 12
+Explanation: After the first step, your score will always be 1. However, choosing the rightmost 
+card first will maximize your total score. The optimal strategy is to take the three cards on the right, 
+giving a final score of 1 + 6 + 5 = 12.    
+    
+    
+Solution: Sliding window
+
+
+Problem Translation: Find the smallest subarray sum of length len(cardPoints) - k
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        size = len(cardPoints) - k
+        minSubArraySum = float('inf')
+        j = curr = 0
         
-        vector<int> cummulativeSumFromFront(n+1, 0);
-        vector<int> cummulativeSumFromBehind(n+1, 0);
-        
-        sum = 0;
-        for (int i=0; i<n; i++) {
-            sum += cardPoints[i];
-            cummulativeSumFromFront[i+1] = sum;
-        }
-        sum = 0;
-        for (int i=n-1; i>=0; i--) {
-            sum += cardPoints[i];
-            cummulativeSumFromBehind[i] = sum;
-        }
-        
-        // Reversing is optional. I reversed it so that it would be easy
-        // to access sum of last (k-i) elements by just indexing at [k-i]
-        // Otherwise, I would have had to index it at [n-k+i] which would
-        // have made it difficult to read.
-        reverse(cummulativeSumFromBehind.begin(), cummulativeSumFromBehind.end());
-        
-        int answer = 0;
-        for(int i=0; i<=k; i++) {      
-            answer = max(answer, 
-                           cummulativeSumFromFront[i] // Sum of first 'i' cards.
-                         + cummulativeSumFromBehind[k-i]); // Sum of last 'k-i' cards.
-        }
+        for i, v in enumerate(cardPoints):
+            curr += v
+            if i - j + 1 > size:
+                curr -= cardPoints[j]
+                j += 1
+            if i - j + 1 == size:    
+                minSubArraySum = min(minSubArraySum, curr)
+				
+        return sum(cardPoints) - minSubArraySum
         return answer;
     }
 };
