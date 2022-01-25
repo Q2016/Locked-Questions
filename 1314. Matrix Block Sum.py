0494d2ebb049c2d89f5e6bcd67b3d794a153781d
Q2016 <k.sohraby@gmail.com>
2022-01-25 05:29:56 -0800

    Update 1314. Matrix Block Sum.py

--- 1314. Matrix Block Sum.py
+++ 1314. Matrix Block Sum.py
@@ -1,34 +1,63 @@
-class Solution {// not my solution --> prefix matrix
-public:
-int sizeX, sizeY;
-    int extractSum(int i, int j, const vector<vector<int> >& sum) {
-        if (i < 0 || j < 0) return 0;
-        if (i >= sizeX) i = sizeX - 1;
-        if (j >= sizeY) j = sizeY - 1;
-        return sum[i][j];
-    }
-        
-    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int K) {
-        sizeX = mat.size();
-        sizeY = mat[0].size();
-        
-        vector<vector<int>> sum(sizeX, vector<int>(sizeY, 0));
-        // Calculate prefix matrix 
-        for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++) {
-    sum[i][j]=mat[i][j]+extractSum(i-1,j,sum)+extractSum(i,j-1,sum)-extractSum(i-1,j-1,sum);
-            }
-        }
-        
-        // Use prefix matrix to calculate our sum
-        vector<vector<int>> ans(sizeX, vector<int>(sizeY, 0));
-        for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++) {
-ans[i][j]=extractSum(i+K,j+K,sum)-extractSum(i+K,j-K-1,sum)-extractSum(i-K-1,j+K,sum)+ extractSum(i-K-1,j-K-1,sum);
-            }
-        }
-        
-        return ans;
-    }
+Question:
+Given a m x n matrix mat and an integer k, return a matrix answer where each answer[i][j] is the sum of all elements mat[r][c] for:
+i - k <= r <= i + k,
+j - k <= c <= j + k, and
+(r, c) is a valid position in the matrix.
+ 
+Example 1:
+Input: mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]], k = 1
+Output: [[12,21,16],
+         [27,45,33],
+         [24,39,28]]    
+    
+    
+    
+    
+    
+Solution: Prefix sum, simalar to LC 304
+    
+Note:
 
-};
+rangeSum[i + 1][j + 1] corresponds to cell (i, j);
+rangeSum[0][j] and rangeSum[i][0] are all dummy values, which are used for the convenience 
+of computation of DP state transmission formula.
+To calculate rangeSum, the ideas are as below:
+
++-----+-+-------+     +--------+-----+     +-----+---------+     +-----+--------+
+|     | |       |     |        |     |     |     |         |     |     |        |
+|     | |       |     |        |     |     |     |         |     |     |        |
++-----+-+       |     +--------+     |     |     |         |     +-----+        |
+|     | |       |  =  |              |  +  |     |         |  -  |              | + mat[i][j]
++-----+-+       |     |              |     +-----+         |     |              |
+|               |     |              |     |               |     |              |
+|               |     |              |     |               |     |              |
++---------------+     +--------------+     +---------------+     +--------------+
+
+rangeSum[i+1][j+1] =  rangeSum[i][j+1] + rangeSum[i+1][j]    -   rangeSum[i][j]   + mat[i][j]
+So, we use the same idea to find the specific block's sum. - credit to @haoel
+
++---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+|               |   |         |    |   |   |           |   |         |    |   |   |          |
+|   (r1,c1)     |   |         |    |   |   |           |   |         |    |   |   |          |
+|   +------+    |   |         |    |   |   |           |   +---------+    |   +---+          |
+|   |      |    | = |         |    | - |   |           | - |      (r1,c2) | + |   (r1,c1)    |
+|   |      |    |   |         |    |   |   |           |   |              |   |              |
+|   +------+    |   +---------+    |   +---+           |   |              |   |              |
+|        (r2,c2)|   |       (r2,c2)|   |   (r2,c1)     |   |              |   |              |
++---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+
+
+    def matrixBlockSum(self, mat: List[List[int]], K: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        rangeSum = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                rangeSum[i + 1][j + 1] = rangeSum[i + 1][j] + rangeSum[i][j + 1] - rangeSum[i][j] + mat[i][j]
+        ans = [[0] * n for _ in range(m)]        
+        for i in range(m):
+            for j in range(n):
+                r1, c1, r2, c2 = max(0, i - K), max(0, j - K), min(m, i + K + 1), min(n, j + K + 1)
+                ans[i][j] = rangeSum[r2][c2] - rangeSum[r1][c2] - rangeSum[r2][c1] + rangeSum[r1][c1]
+        return ans
