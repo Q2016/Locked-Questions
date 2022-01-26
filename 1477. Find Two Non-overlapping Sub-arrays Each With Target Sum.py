a14fb95463fe42c8644953f4bf62eab39415198d
Q2016 <k.sohraby@gmail.com>
2022-01-25 18:28:53 -0800

    Update 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py

--- 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py
+++ 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py
@@ -1,10 +1,22 @@
-Solution: One-pass, prefix-sum, O(n)
+Question:
+You are given an array of integers arr and an integer target. You have to find two non-overlapping sub-arrays 
+of arr each with a sum equal target. There can be multiple answers so you have to find an answer where the sum
+of the lengths of the two sub-arrays is minimum. Return the minimum sum of the lengths of the two required 
+sub-arrays, or return -1 if you cannot find such two sub-arrays.    
 
+Example 1:
+Input: arr = [3,2,2,4,3], target = 3
+Output: 2
+Explanation: Only two sub-arrays have sum = 3 ([3] and [3]). The sum of their lengths is 2
+
+
+    
+Solution: One-pass, prefix-sum, O(n)
 
 Keep track of the running prefix-sum and the length of the shortest sub-array that sums to the target up to that point (best_till in my solution).
 Each time we find another such sub-array, look up that length value at the index right before it starts.
 
-class Solution:
+
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
         prefix = {0: -1}
         best_till = [math.inf] * len(arr)
