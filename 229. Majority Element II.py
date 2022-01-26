6a6bc3ef32c8ab1eae0f6b4046782f1d0afd5911
Q2016 <k.sohraby@gmail.com>
2022-01-26 10:33:55 -0800

    Update 229. Majority Element II.py

--- 229. Majority Element II.py
+++ 229. Majority Element II.py
@@ -1,17 +1,23 @@
+Question:
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: [3]   
+
+    
+Solution:
+    
 For those who aren't familiar with Boyer-Moore Majority Vote algorithm,
 I found a great article (http://goo.gl/64Nams) that helps me to understand this fantastic algorithm!!
-Please check it out!
-
-The essential concepts is you keep a counter for the majority number X. If you find a number Y that is not X, the current counter should deduce 1. The reason is that if there is 5 X and 4 Y, there would be one (5-4) more X than Y. This could be explained as "4 X being paired out by 4 Y".
-
+The essential concepts is you keep a counter for the majority number X. If you find a number Y that is not X, 
+the current counter should deduce 1. The reason is that if there is 5 X and 4 Y, there would be one (5-4) more X than Y. 
+This could be explained as "4 X being paired out by 4 Y".
 And since the requirement is finding the majority for more than ceiling of [n/3], the answer would be less than or equal to two numbers.
 So we can modify the algorithm to maintain two counters for two majorities.
 
-Followings are my sample Python code:
+Solution: Boyer-Moore Majority Vote algorithm
 
-class Solution:
-# @param {integer[]} nums
-# @return {integer[]}
 def majorityElement(self, nums):
     if not nums:
         return []
@@ -27,5 +33,4 @@ def majorityElement(self, nums):
             candidate2, count2 = n, 1
         else:
             count1, count2 = count1 - 1, count2 - 1
-    return [n for n in (candidate1, candidate2)
-                    if nums.count(n) > len(nums) // 3]
+    return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums) // 3]
