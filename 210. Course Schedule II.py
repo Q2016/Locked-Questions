e48e77e3972f22274115338c370c9821921ae08c
Q2016 <k.sohraby@gmail.com>
2022-01-26 14:41:50 -0800

    Update 210. Course Schedule II.py

--- 210. Course Schedule II.py
+++ 210. Course Schedule II.py
@@ -1,45 +1,61 @@
-
-
-class Solution {
-public:
-    vector<int> result; int f=0;
+Question:
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must 
+take course bi first if you want to take course ai. Return the ordering of courses you should take 
+to finish all courses. If there are many valid answers, return any of them. 
+ 
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].    
     
-    void iscycle(vector<int> graph[], vector<int> &visited, int node){
-        
-        if(!visited[node]){
-            visited[node]=1;
-            //result.push_back(node); //we will push the visited nodes to the resulting array
+Solution: Topological Sorted Order + DFS
 
-            for(auto edge : graph[node]){
-                iscycle(graph,visited,edge);
-            
-            }
-        result.push_back(node);
-        visited[node]=2; // if function comes till here then we can mark the current node as 2 i.e, visited
-        
-        }
-        else if(visited[node]==1) f=1;  
-    }
-    
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {    
-        // create the graph
-        vector<int> graph[numCourses];
-        
-        for(auto edge : prerequisites){
-            graph[edge[0]].push_back(edge[1]);
-        }
-   
-        // visited
-        //vector<int> visited(numCourses,0);
-        vector<int> visited;
-        visited.resize(numCourses);
-        for (int i=0;i<numCourses;++i){
-            iscycle(graph, visited, i);
-        }
-        
-        if(f)
-            return vector<int>();
-        //result.erase(result.begin());
-        return result;    
-    }
-};
+class Solution:
+
+    WHITE = 1
+    GRAY = 2
+    BLACK = 3
+
+    def findOrder(self, numCourses, prerequisites):
+
+        # Create the adjacency list representation of the graph
+        adj_list = defaultdict(list)
+        # A pair [a, b] in the input represents edge from b --> a
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+
+        topological_sorted_order = []
+        is_possible = True
+
+        # By default all vertces are WHITE
+        color = {k: Solution.WHITE for k in range(numCourses)}
+        def dfs(node):
+            nonlocal is_possible
+
+            # Don't recurse further if we found a cycle already
+            if not is_possible:
+                return
+
+            # Start the recursion
+            color[node] = Solution.GRAY
+
+            # Traverse on neighboring vertices
+            if node in adj_list:
+                for neighbor in adj_list[node]:
+                    if color[neighbor] == Solution.WHITE:
+                        dfs(neighbor)
+                    elif color[neighbor] == Solution.GRAY:
+                         # An edge to a GRAY vertex represents a cycle
+                        is_possible = False
+
+            # Recursion ends. We mark it as black
+            color[node] = Solution.BLACK
+            topological_sorted_order.append(node)
+
+        for vertex in range(numCourses):
+            # If the node is unprocessed, then call dfs on it.
+            if color[vertex] == Solution.WHITE:
+                dfs(vertex)
+
+        return topological_sorted_order[::-1] if is_possible else []    
