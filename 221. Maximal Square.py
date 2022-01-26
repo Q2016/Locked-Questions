bdeb80988b9e69be8daf86a2013086094ee9edd5
Q2016 <k.sohraby@gmail.com>
2022-01-26 12:15:15 -0800

    Update 221. Maximal Square.py

--- 221. Maximal Square.py
+++ 221. Maximal Square.py
@@ -1,25 +1,29 @@
-https://leetcode.com/problems/maximal-square/
+Question:
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+Example 1:
+Input: matrix = [["1","0","1","0","0"],
+                 ["1","0","1","1","1"],
+                 ["1","1","1","1","1"],
+                 ["1","0","0","1","0"]]
+Output: 4    
 
 
-# Dp solution
+    
+solution: DP
 
-class Solution {
-public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        if(matrix.size()==0) return 0;
-        int maxSq=0;
-        int nRow=matrix.size();
-        int nCol=matrix[0].size();
-        vector<vector<int>> dp(nRow+1,vector<int>(nCol+1,0));
-        //dp[i][j] represents max square ending at position (i-1, j-1)
-        for(int i=1;i<=nRow;++i){
-            for(int j=1;j<=nCol;++j){
-                if(matrix[i-1][j-1]=='1'){
-                    dp[i][j]=min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]})+1;
-                    maxSq=max(maxSq,dp[i][j]);
-                }
-            }
-        }
-        return maxSq*maxSq;
-    }
-};
+
+    def maximalSquare(self, M):
+        def is_valid_sqaure(row, col, side):
+            return all(all(M[i][j] == '1' for j in range(col, col+side)) for i in range(row, row+side))
+        m, n = len(M), len(M[0])
+        for side_len in range(min(m,n), 0, -1):
+            for row in range(m - side_len + 1):
+                for col in range(n - side_len + 1):
+                    if is_valid_sqaure(row, col, side_len):
+                        return side_len**2
+        return 0
+    
+    
+Time Complexity : O(M*N*min(M,N)3)
+Space Complexity : O(1), only constant extra space is being used
