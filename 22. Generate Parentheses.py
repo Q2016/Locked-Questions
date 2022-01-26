30f7dc760658fb1929d78059bb88e6aae90faf91
Q2016 <k.sohraby@gmail.com>
2022-01-26 12:47:43 -0800

    Update 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -1,48 +1,31 @@
+Question:
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]        
 
 
-
-/*
-public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        backtrack(list, "", 0, 0, n);
-        return list;
-    }
-    
-    public void backtrack(List<String> list, String str, int open, int close, int max){
-        
-        if(str.length() == max*2){
-            list.add(str);
-            return;
-        }
-        
-        if(open < max)
-            backtrack(list, str+"(", open+1, close, max);
-        if(close < open)
-            backtrack(list, str+")", open, close+1, max);
-    }
-*/
+Solution:              
+Instead of adding '(' or ')' every time, let's only add them when we know it will remain a valid sequence. 
+We can do this by keeping track of the number of opening and closing brackets we have placed so far.
+We can start an opening bracket if we still have one (of n) left to place. And we can start a closing 
+bracket if it would not exceed the number of opening brackets.        
 
 
-
-
-class Solution {
-public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> list;
-        backtrack(list, "", 0, 0, n);
-        
-        return list;
-    }
-    
-    void backtrack(vector<string>& list, string str, int open, int close, int max){
-        if (str.size()==max*2){list.push_back(str); cout<<str<<endl;}
-        
-        if (open<max){
-            
-            backtrack(list, str+"(", open+1, close, max);
-        }
-        if (close<open){
-            backtrack(list, str+")", open, close+1, max);
-        }
-    }
-};
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(S = [], left = 0, right = 0):
+            if len(S) == 2 * n:
+                ans.append("".join(S))
+                return
+            if left < n:
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack()
+        return ans
