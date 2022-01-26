5b56fa452a93fac24d62b5fc5bcf6c748ba17f3f
Q2016 <k.sohraby@gmail.com>
2022-01-26 12:57:12 -0800

    Update 216. Combination Sum III.py

--- 216. Combination Sum III.py
+++ 216. Combination Sum III.py
@@ -1,21 +1,26 @@
-class Solution {
-public:
-vector<vector<int>> combinationSum3(int k, int n) {
-    vector<vector<int>> result;
-    vector<int> path;
-    backtrack(result, path, 1, k, n);
-    return result;
-}
+Question:
+Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+Only numbers 1 through 9 are used. Each number is used at most once. Return a list of all possible valid combinations. 
+ 
+Example 1:
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Explanation:
+1 + 2 + 4 = 7
+There are no other valid combinations.
 
-void backtrack(vector<vector<int>> &result, vector<int> &path, int start, int k, int target){
-    if(target==0&&k==0){
-        result.push_back(path);
-        return;
-    }
-    for(int i=start;i<=10-k&&i<=target;i++){
-        path.push_back(i);
-        backtrack(result,path,i+1,k-1,target-i);
-        path.pop_back();
-    }
-}
-};
+
+Solution: Backtracing
+    
+def combinationSum3(self, k, n):
+        ret = []
+        self.dfs(list(range(1, 10)), k, n, [], ret)
+        return ret
+    
+    def dfs(self, nums, k, n, path, ret):
+        if k < 0 or n < 0:
+            return 
+        if k == 0 and n == 0:
+            ret.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], k-1, n-nums[i], path+[nums[i]], ret)    
