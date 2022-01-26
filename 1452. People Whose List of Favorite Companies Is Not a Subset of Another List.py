743be0e70663a8eef93486c22f823d057b9aa0b9
Q2016 <k.sohraby@gmail.com>
2022-01-26 07:24:09 -0800

    Update 1452. People Whose List of Favorite Companies Is Not a Subset of Another List.py

--- 1452. People Whose List of Favorite Companies Is Not a Subset of Another List.py
+++ 1452. People Whose List of Favorite Companies Is Not a Subset of Another List.py
@@ -1,10 +1,25 @@
+Question:
+Given the array favoriteCompanies where favoriteCompanies[i] is the list of favorites companies 
+for the ith person (indexed from 0). Return the indices of people whose list of favorite companies 
+is not a subset of any other list of favorites companies. You must return the indices in increasing order.
+
+Example 1:
+Input: favoriteCompanies = [["leetcode","google","facebook"],["google","microsoft"],["google","facebook"],["google"],["amazon"]]
+Output: [0,1,4] 
+Explanation: 
+Person with index=2 has favoriteCompanies[2]=["google","facebook"] which is a subset of 
+favoriteCompanies[0]=["leetcode","google","facebook"] corresponding to the person with index 0. 
+Person with index=3 has favoriteCompanies[3]=["google"] which is a subset of 
+favoriteCompanies[0]=["leetcode","google","facebook"] and favoriteCompanies[1]=["google","microsoft"]. 
+Other lists of favorite companies are not a subset of another list, therefore, the answer is [0,1,4].    
+
+
+Solution:
+    
 https://leetcode.com/problems/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/discuss/636512/Python3-Easy-Solution
-
-
 
 Thanks to Python set library
 
-class Solution:
     def peopleIndexes(self, A: List[List[str]]) -> List[int]:
         d = {i: set(v) for i, v in enumerate(A)}
         res = []
