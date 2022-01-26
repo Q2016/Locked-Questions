1d95e37fd4926024604a00ae986cf2844339f9ec
Q2016 <k.sohraby@gmail.com>
2022-01-26 09:46:51 -0800

    Update 24. Swap Nodes in Pairs.py

--- 24. Swap Nodes in Pairs.py
+++ 24. Swap Nodes in Pairs.py
@@ -1,28 +1,20 @@
-# Time:  O(n)
-# Space: O(1)
-#
+Question:
 # Given a linked list, swap every two adjacent nodes and return its head.
-# 
-# For example,
-# Given 1->2->3->4, you should return the list as 2->1->4->3.
-# 
 # Your algorithm should use only constant space.
 # You may not modify the values in the list, only nodes itself can be changed.
-#
+
+Example:
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+
+Solution:
 
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-    
-    def __repr__(self):
-        if self:
-            return "{} -> {}".format(self.val, self.next)
 
-class Solution:
-    # @param a ListNode
-    # @return a ListNode
     def swapPairs(self, head):
         dummy = ListNode(0)
         dummy.next = head
@@ -34,3 +26,7 @@ class Solution:
             next_one.next = next_three
             current = next_one
         return dummy.next
+
+    
+# Time:  O(n)
+# Space: O(1)    
