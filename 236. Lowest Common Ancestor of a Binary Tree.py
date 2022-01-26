53d11ca7bfcffe31a7aa90ecee5a9a5655ca3477
Q2016 <k.sohraby@gmail.com>
2022-01-26 10:13:08 -0800

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 236. Lowest Common Ancestor of a Binary Tree.py
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,60 +1,42 @@
-Approach 1: Recursive Approach
-Intuition
+Question:
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between 
+two nodes p and q as the lowest node in T that has both p and q as descendants”
+                     3
+                   /   \
+                 5        1
+               /   \     /  \
+              6     2   0    8
+                   / \
+                  7   4
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.    
 
-The approach is pretty intuitive. Traverse the tree in a depth first manner. The moment you 
-encounter either of the nodes p or q, return some boolean flag. The flag helps to determine 
-if we found the required nodes in any of the paths. The least common ancestor would then be 
+    
+    
+ Solution:   Recursive Approach
+
+Traverse the tree in a DFS manner. The moment you encounter either of the nodes p or q, return some boolean flag. 
+The flag helps to determine if we found the required nodes in any of the paths. The least common ancestor would then be 
 the node for which both the subtree recursions return a True flag. It can also be the node 
 which itself is one of p or q and for which one of the subtree recursions returns a True flag.
 
-Let us look at the formal algorithm based on this idea.
-
-Algorithm
-
-Start traversing the tree from the root node.
-If the current node itself is one of p or q, we would mark a variable mid as True and continue 
-the search for the other node in the left and right branches.
-If either of the left or the right branch returns True, this means one of the two nodes was found below.
-If at any point in the traversal, any two of the three flags left, right or mid become True, this 
-means we have found the lowest common ancestor for the nodes p and q.
-Let us look at a sample tree and we search for the lowest common ancestor of two nodes 9 and 11 in the tree.
-
-
-
-
-
-
-class Solution:
-
-    def __init__(self):
-        # Variable to store LCA node.
-        self.ans = None
-
     def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+
         def recurse_tree(current_node):
 
             # If reached the end of a branch, return False.
             if not current_node:
                 return False
-            # Left Recursion
             left = recurse_tree(current_node.left)
-
-            # Right Recursion
             right = recurse_tree(current_node.right)
-
             # If the current node is one of p or q
             mid = current_node == p or current_node == q
-
             # If any two of the three flags left, right or mid become True.
             if mid + left + right >= 2:
                 self.ans = current_node
-
             # Return True if either of the three bool values is True.
             return mid or left or right
 
