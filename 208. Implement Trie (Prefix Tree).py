f155ecf6d9f84a65428b79b9d1718a4172583c86
Q2016 <k.sohraby@gmail.com>
2022-01-26 15:14:09 -0800

    Update 208. Implement Trie (Prefix Tree).py

--- 208. Implement Trie (Prefix Tree).py
+++ 208. Implement Trie (Prefix Tree).py
@@ -1,25 +1,34 @@
+Question:
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and 
+retrieve keys in a dataset of strings. There are various applications of this data structure, such as 
+autocomplete and spellchecker.
+
+Implement the Trie class:
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, 
+and false otherwise.
+ 
+Example 1:
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True        
+
+Solution: Trie
+
 https://leetcode.com/problems/implement-trie-prefix-tree/solution/
-
-
-
-There are several other data structures, like balanced trees and hash tables, which give 
-us the possibility to search for a word in a dataset of strings. Then why do we need trie? 
-Although hash table has O(1)O(1) time complexity for looking for a key, it is not efficient in the following operations :
-
-Finding all keys with a common prefix.
-Enumerating a dataset of strings in lexicographical order.
-Another reason why trie outperforms hash table, is that as hash table increases in size, 
-there are lots of hash collisions and the search time complexity could deteriorate to O(n)O(n), 
-where nn is the number of keys inserted. Trie could use less space compared to Hash Table when 
-storing many keys with the same prefix. In this case using trie has only O(m)O(m) time complexity, 
-where mm is the key length. Searching for a key in a balanced tree costs O(m \log n)O(mlogn) time complexity.
-
-Trie node structure
-Trie is a rooted tree. Its nodes have the following fields:
-
-Maximum of RR links to its children, where each link corresponds to one of RR character 
-values from dataset alphabet. In this article we assume that RR is 26, the number of lowercase latin letters.
-Boolean field which specifies whether the node corresponds to the end of the key, or is just a key prefix.
 
 class TrieNode:
         # Initialize your data structure here.
@@ -31,9 +40,7 @@ class TrieNode:
     
         def __init__(self):
             self.root = TrieNode()
-    
-        # @param {string} word
-        # @return {void}
+
         # Inserts a word into the trie.
         def insert(self, word):
             node=self.root
@@ -43,9 +50,7 @@ class TrieNode:
                 node=node.children[i]
             node.word=True
     
-        # @param {string} word
-        # @return {boolean}
-        # Returns if the word is in the trie.
+
         def search(self, word):
             node=self.root
             for i in word:
@@ -53,9 +58,7 @@ class TrieNode:
                     return False
                 node=node.children[i]
             return node.word
-    
-        # @param {string} prefix
-        # @return {boolean}
+
         # Returns if there is any word in the trie
         # that starts with the given prefix.
         def startsWith(self, prefix):
@@ -65,9 +68,4 @@ class TrieNode:
                     return False
                 node=node.children[i]
             return True
-            
-    
-    # Your Trie object will be instantiated and called as such:
-    # trie = Trie()
-    # trie.insert("somestring")
-    # trie.search("key")
+
