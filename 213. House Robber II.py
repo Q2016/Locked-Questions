0a9236878a877b8e38e5d6cd6e4b681afc813782
Q2016 <k.sohraby@gmail.com>
2022-01-26 14:21:33 -0800

    Update 213. House Robber II.py

--- 213. House Robber II.py
+++ 213. House Robber II.py
@@ -1,49 +1,36 @@
-// from Simple AC solution in Java in O(n) with explanation
+Question:
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of 
+money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor 
+of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically 
+contact the police if two adjacent houses were broken into on the same night. Given an integer array nums 
+representing the amount of money of each house, return the maximum amount of money you can rob tonight 
+without alerting the police.
 
-class Solution {
-public:
+Example 1:
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.    
+
+
     
-    int rob(vector<int>& nums, int lo, int hi) {
-        int include = 0, exclude = 0;
-        for (int j = lo; j <= hi; j++) {
-            int i = include, e = exclude;
-            include = e + nums[j];
-            exclude = max(e, i);
-        }
-    return max(include, exclude);
-    }
+Solution: DP
     
-    int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        return max(rob(nums, 0, nums.size() - 2), rob(nums, 1, nums.size() - 1));
-    }
-    
-    
-};
+Notice that the first house and the last house can not be both robbed, so we have rob(nums) = max(rob(nums[1:], nums[:-1]). 
+Since there are no circles in both nums[1:] and nums[:-1], we can simply apply the answers from House Rob. 
+https://leetcode.com/problems/house-robber/discuss/299056/Python-O(n)-time-O(1)-space-4-lines
 
-/*
-Since this question is a follow-up to House Robber, we can assume we already have a way to solve the simpler question, i.e. given a 1 row of house, we know how to rob them. So we already have such a helper function. We modify it a bit to rob a given range of houses.
+    def rob(self, nums):
+                                                                                                  
+        def simple_rob(nums):
+            rob, not_rob = 0, 0
+            for num in nums:
+                rob, not_rob = not_rob + num, max(rob, not_rob)
+            return max(rob, not_rob)
+        
+        if not nums:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+        else:
+            return max(simple_rob(nums[1:]), simple_rob(nums[:-1]))
 
-private int rob(int[] num, int lo, int hi) {
-    int include = 0, exclude = 0;
-    for (int j = lo; j <= hi; j++) {
-        int i = include, e = exclude;
-        include = e + num[j];
-        exclude = Math.max(e, i);
-    }
-    return Math.max(include, exclude);
-}
-Now the question is how to rob a circular row of houses. It is a bit complicated to solve like the simpler question. It is because in the simpler question whether to rob num[lo] is entirely our choice. But, it is now constrained by whether num[hi] is robbed.
-
-However, since we already have a nice solution to the simpler problem. We do not want to throw it away. Then, it becomes how can we reduce this problem to the simpler one. Actually, extending from the logic that if house i is not robbed, then you are free to choose whether to rob house i + 1, you can break the circle by assuming a house is not robbed.
-
-For example, 1 -> 2 -> 3 -> 1 becomes 2 -> 3 if 1 is not robbed.
-
-Since every house is either robbed or not robbed and at least half of the houses are not robbed, the solution is simply the larger of two cases with consecutive houses, i.e. house i not robbed, break the circle, solve it, or house i + 1 not robbed. Hence, the following solution. I chose i = n and i + 1 = 0 for simpler coding. But, you can choose whichever two consecutive ones.
-
-public int rob(int[] nums) {
-    if (nums.length == 1) return nums[0];
-    return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
-}
-
-*/
