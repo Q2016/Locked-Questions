1c81c3b405ad70dc0d61a77da690fdf899cd355c
Q2016 <k.sohraby@gmail.com>
2022-01-27 15:55:38 -0800

    Update 304. Range Sum Query 2D - Immutable.py

--- 304. Range Sum Query 2D - Immutable.py
+++ 304. Range Sum Query 2D - Immutable.py
@@ -1,11 +1,18 @@
-Prefix sum
+Question:
+Given a 2D matrix matrix, handle multiple queries of the following type: Calculate the sum of the elements of matrix inside 
+the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).    
+    
+    
+Solution: Prefix sum
 
-Constructor: Time & Space: O(m*n), where m is the number of rows, n is the number of columns in the grid
-sumRegion: Time & Space: O(1)
+Sum(ABCD)=Sum(OD)−Sum(OB)−Sum(OC)+Sum(OA)
 
-
-
-https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/572648/C%2B%2BJavaPython-Prefix-sum-with-Picture-explain-Clean-and-Concise
+O -----------
+|    |       |
+|--- A---B   |
+|    |   |   |
+|    C---D   |   
+ ------------
 
 class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
@@ -18,3 +25,10 @@ class NumMatrix:
     def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
         r1, c1, r2, c2 = r1 + 1, c1 + 1, r2 + 1, c2 + 1  # Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
         return self.sum[r2][c2] - self.sum[r2][c1 - 1] - self.sum[r1 - 1][c2] + self.sum[r1 - 1][c1 - 1]
+
+    
+    
+    
+    
+Constructor: Time & Space: O(m*n), where m is the number of rows, n is the number of columns in the grid
+sumRegion: Time & Space: O(1)    
