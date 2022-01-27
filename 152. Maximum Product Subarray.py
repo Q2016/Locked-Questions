9d569a14890e441fd43b4d8cbc9913965acf06d8
Q2016 <k.sohraby@gmail.com>
2022-01-27 08:14:58 -0800

    Update 152. Maximum Product Subarray.py

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -1,17 +1,19 @@
-# dynamic programing
-#from Unofficial solution intuitive explanations O(n) two different approaches
+Question:
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, 
+and return the product. A subarray is a contiguous subsequence of the array.
 
-
+Example 1:
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.    
+    
+    
+Solution: DP    
+    
 #https://www.youtube.com/watch?v=lXVy6YWFcRM
 
-class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
 
-        
         max_num=1
         min_num=1
         
