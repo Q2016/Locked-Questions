2d463761247e01d0acb21e57b51bfa93e7cfa5c6
Q2016 <k.sohraby@gmail.com>
2022-01-27 10:51:31 -0800

    Update 311. Sparse Matrix Multiplication.py

--- 311. Sparse Matrix Multiplication.py
+++ 311. Sparse Matrix Multiplication.py
@@ -1,13 +1,19 @@
-# Time:  O(m * n * l), A is m x n matrix, B is n x l matrix
-# Space: O(m * l)
+Question:
+Given two sparse matrices A and B, return the result of AB. You may assume that A's column number is equal to B's row number.
 
-class Solution(object):
+Example:
+A = [[ 1, 0, 0],        B = [[ 7, 0, 0 ],
+     [-1, 0, 3]]             [ 0, 0, 0 ],
+                             [ 0, 0, 1 ]]
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |    
+
+Solution:    
+
     def multiply(self, A, B):
-        """
-        :type A: List[List[int]]
-        :type B: List[List[int]]
-        :rtype: List[List[int]]
-        """
+
         m, n, l = len(A), len(A[0]), len(B[0])
         res = [[0 for _ in xrange(l)] for _ in xrange(m)]
         for i in xrange(m):
@@ -16,3 +22,8 @@ class Solution(object):
                     for j in xrange(l):
                         res[i][j] += A[i][k] * B[k][j]
         return res
+
+    
+    
+# Time:  O(m * n * l), A is m x n matrix, B is n x l matrix
+# Space: O(m * l)    
