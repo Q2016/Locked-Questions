e1679d77758967212509a86e8cfc7371e67b9b90
Q2016 <k.sohraby@gmail.com>
2022-01-26 19:13:33 -0800

    Update 179. Largest Number.py

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -1,3 +1,14 @@
+Question:
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+Since the result may be very large, so you need to return a string instead of an integer.
+
+Example 1:
+Input: nums = [10,2]
+Output: "210"    
+
+    
+Solution: ---    
+    
 class LargerNumKey(str):
     def __lt__(x, y):
         return x+y > y+x
