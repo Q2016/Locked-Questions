97843a4c2bf32f622015c336b85cd908eb3e788a
Q2016 <k.sohraby@gmail.com>
2022-01-27 13:58:39 -0800

    Update 309. Best Time to Buy and Sell Stock with Cooldown.py

--- 309. Best Time to Buy and Sell Stock with Cooldown.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -29,17 +29,17 @@ s0[0] = 0; // At the start, you don't have any stock if you just rest
 s1[0] = -prices[0]; // After buy, you should have -prices[0] profit. Be positive!
 s2[0] = INT_MIN; // Lower base case
 
-	def maxProfit([list]-> prices):
+    def maxProfit([list]-> prices):
 		
-        if (len(prices) <= 1): 
-            return 0;
+	if (len(prices) <= 1): 
+	    return 0;
 		s1[0] = -prices[0]
 		s0[0] = 0
 		s2[0] = INT_MIN
-		
-        for i in range(1, len(prices): 
+
+	for i in range(1, len(prices): 
 			s0[i] = max(s0[i - 1], s2[i - 1])
 			s1[i] = max(s1[i - 1], s0[i - 1] - prices[i])
 			s2[i] = s1[i - 1] + prices[i]
 		
-		return max(s0[len(prices) - 1], s2[len(prices) - 1])
+	return max(s0[len(prices) - 1], s2[len(prices) - 1])
