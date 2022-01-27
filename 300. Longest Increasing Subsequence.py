959fa6bece16522fa848dd0335ddb6ffddbb1298
Q2016 <k.sohraby@gmail.com>
2022-01-27 15:58:06 -0800

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -1,3 +1,17 @@
+Question:
+Given an integer array nums, return the length of the longest strictly increasing subsequence. A subsequence is a 
+sequence that can be derived from an array by deleting some or no elements without changing the order of the 
+remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.    
+    
+    
+
+Solution:
+
 tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
 For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
 
