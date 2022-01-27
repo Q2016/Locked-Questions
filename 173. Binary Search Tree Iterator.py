739adb0eddf6e85860b5fbf966d993d2a88ace9d
Q2016 <k.sohraby@gmail.com>
2022-01-27 06:28:22 -0800

    Update 173. Binary Search Tree Iterator.py

--- 173. Binary Search Tree Iterator.py
+++ 173. Binary Search Tree Iterator.py
@@ -1,37 +1,50 @@
+Question:
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the 
+constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
 
-So this can satisfy O(h) memory, hasNext() in O(1) time,
-But next() is O(h) time.
+Example 1:
+Input:["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output: [null, 3, 7, true, 9, true, 15, true, 20, false]
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
 
-I can't find a solution that can satisfy both next() in O(1) time, space in O(h)
+Solution:
+Use Stack to store directed left children from root. When next() be called, I just pop one element and process its right child as new root.
+The code is pretty straightforward. So this can satisfy O(h) memory, hasNext() in O(1) time, But next() is O(h) time.
+I can't find a solution that can satisfy both next() in O(1) time, space in O(h).    
 
-class BSTIterator {
-private:    
-    stack<TreeNode*> node_stack;
-public:
-    BSTIterator(TreeNode *root) {
-        while(root) 
-        {
-            node_stack.push(root);
-            root = root->left;
-        }
-    }
+class BSTIterator:
+    # @param root, a binary search tree's root node
+    def __init__(self, root):
+        self.stack = list()
+        self.pushAll(root)
 
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return !node_stack.empty();
-    }
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        return self.stack
 
-    /** @return the next smallest number */
-    int next() {
-        TreeNode *t = node_stack.top();
-        node_stack.pop();
-        int ret = t->val;
-        t = t->right;
-        while(t)
-        {
-            node_stack.push(t);
-            t = t->left;
-        }
-        return ret;
-    }
-};
+    # @return an integer, the next smallest number
+    def next(self):
+        tmpNode = self.stack.pop()
+        self.pushAll(tmpNode.right)
+        return tmpNode.val
+        
+    def pushAll(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
