e0e6ebc3998b36daf0ce19af7da9d85c9d5745f6
Q2016 <k.sohraby@gmail.com>
2022-01-26 16:15:44 -0800

    Update 199. Binary Tree Right Side View.py

--- 199. Binary Tree Right Side View.py
+++ 199. Binary Tree Right Side View.py
@@ -1,16 +1,34 @@
-class Solution {
-public:
-    void recursion(TreeNode *root, int level, vector<int> &res)
-    {
-        if(root==NULL) return ;
-        if(res.size()<level) res.push_back(root->val);
-        recursion(root->right, level+1, res);
-        recursion(root->left, level+1, res);
-    }
+Question:
+Given the root of a binary tree, imagine yourself standing on the right side of it, 
+return the values of the nodes you can see ordered from top to bottom.
+
+Example 1:
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]    
+
+                1
+             /     \
+           2         3
+            \         \
+             5         4
+
+Solution: BFS
     
-    vector<int> rightSideView(TreeNode *root) {
-        vector<int> res;
-        recursion(root, 1, res);
-        return res;
-    }
-};
+    def rightSideView(self, root):
+        deque = collections.deque()
+        if root:
+            deque.append(root)
+        res = []
+        while deque:
+            size, val = len(deque), 0
+            for _ in range(size):
+                node = deque.popleft()
+                val = node.val # store last value in each level
+                if node.left:
+                    deque.append(node.left)
+                if node.right:
+                    deque.append(node.right)
+            res.append(val)
+        return res    
+    
+            
