e80e217ddc9598993bd5023414a9eb879b63c622
Q2016 <k.sohraby@gmail.com>
2022-01-28 08:46:25 -0800

    Update 395. Longest Substring with At Least K Repeating Characters.py

--- 395. Longest Substring with At Least K Repeating Characters.py
+++ 395. Longest Substring with At Least K Repeating Characters.py
@@ -1,31 +1,31 @@
+Question:
+Given a string s and an integer k, return the length of the longest substring of s such that the 
+frequency of each character in this substring is greater than or equal to k.
+
+Example 1:
+Input: s = "aaabb", k = 3
+Output: 3
+Explanation: The longest substring is "aaa", as 'a' is repeated 3 times.
+
+    
 Solution: (Divide And Conquer)
 
+In the first pass I record counts of every character in a hashmap. In the second pass I locate the first character that appear 
+less than k times in the string. This character is definitely not included in the result, and that separates the string into two parts.
+keep doing this recursively and the maximum of the left/right part is the answer.
 
-//in the first pass I record counts of every character in a hashmap
-//in the second pass I locate the first character that appear less than k times in the //string. this character is definitely not included in the result, and that separates the //string into two parts.
-//keep doing this recursively and the maximum of the left/right part is the answer.
+    def longestSubstring(self, s: str, k: int) -> int:
+        if len(s) == 0 or k > len(s):
+            return 0
+        c = Counter(s)
+        sub1, sub2 = "", ""
+        for i, letter in enumerate(s):
+            if c[letter] < k:
+                sub1 = self.longestSubstring(s[:i], k)
+                sub2 = self.longestSubstring(s[i+1:], k)
+                break
+        else:
+            return len(s)
+        return max(sub1, sub2)
+        
 
-
-class Solution {
-public:
-
-    int longestSubstring(string s, int k) {
-        if(s.size() == 0 || k > s.size())   return 0;
-        if(k == 0)  return s.size();
-        
-        unordered_map<char,int> Map;
-        for(int i = 0; i < s.size(); i++){
-            Map[s[i]]++;
-        }
-        
-        int idx =0;
-        while(idx <s.size() && Map[s[idx]] >= k)    idx++;
-        if(idx == s.size()) return s.size();
-        
-        int left = longestSubstring(s.substr(0 , idx) , k);
-        int right = longestSubstring(s.substr(idx+1) , k);
-        
-        return max(left, right);
-        
-    }
-};
