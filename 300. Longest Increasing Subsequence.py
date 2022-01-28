c9dea071b11c1c83926ffa682a452717541bf899
Q2016 <k.sohraby@gmail.com>
2022-01-27 16:53:46 -0800

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -10,37 +10,38 @@ Explanation: The longest increasing subsequence is [2,3,7,101], therefore the le
     
     
 
-Solution:
+Solution: DP
 
-tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
-For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+First the brute Force's idea is:
+-If the current element is greater than the previous element, then we can either pick it or dont pick it 
+because we may get a smaller element somewhere ahead which is greater than previous and picking that would be optimal. So we try both options.
+-If the current element is smaller or equal to previous element, it can't be picked.
+But it runs as O(2^N): 
+    
+    def lengthOfLIS(nums):
+        return solve(nums, 0, INT_MIN)
+    
+    def solve(nums, i, prev) :
+        if(i >= len(nums)) 
+            return 0                                                // cant pick any more elements
+        take = 0, dontTake = solve(nums, i + 1, prev)               // try skipping the current element
+        if(nums[i] > prev) take = 1 + solve(nums, i + 1, nums[i])   // or pick it if it is greater than previous picked element
+        return max(take, dontTake)                                  // return whichever choice gives max LIS
+    
+    
+So we use Memoization. Let dp[i] is the longest increase subsequence which ends at nums[i].
 
-len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
-len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
-len = 3   :      [4, 5, 6]            => tails[2] = 6
-We can easily prove that tails is a increasing array. Therefore it is possible to do a binary 
-search in tails array to find the one needs update.
-
-Each time we only do one of the two:
-
-(1) if x is larger than all tails, append it, increase the size by 1
-(2) if tails[i-1] < x <= tails[i], update tails[i]
-Doing so will maintain the tails invariant. The the final answer is just the size.
-
-
-def lengthOfLIS(self, nums):
-    tails = [0] * len(nums)
-    size = 0
-    for x in nums:
-        i, j = 0, size
-        while i != j:
-            m = (i + j) / 2
-            if tails[m] < x:
-                i = m + 1
-            else:
-                j = m
-        tails[i] = x
-        size = max(i + 1, size)
-    return size
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
 
 
+Complexity
+
+Time: O(N^2)
+Space: O(N)
