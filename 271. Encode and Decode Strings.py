c4e718353d45b7ac0cabc50b64dcdf45081ba778
Q2016 <k.sohraby@gmail.com>
2022-01-28 02:16:59 -0800

    Update 271. Encode and Decode Strings.py

--- 271. Encode and Decode Strings.py
+++ 271. Encode and Decode Strings.py
@@ -1,27 +1,20 @@
-Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+Question:
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network 
+and is decoded back to the original list of strings.
 
-Machine 1 (sender) has the function:
+Machine 1 (sender) has the function:                  Machine 2 (receiver) has the function:
 
-string encode(vector strs) {
-  // ... your code
-  return encoded_string;
-}
-Machine 2 (receiver) has the function:
-vector decode(string s) {
-  //... your code
-  return strs;
-}
-So Machine 1 does:
-
-string encoded_string = encode(strs);
-and Machine 2 does:
-
-vector strs2 = decode(encoded_string);
-strs2 in Machine 2 should be the same as strs in Machine 1.
+def encode(vector strs):                              def decode(string s) {
+  // ... your code                                       //... your code
+  return encoded_string;                              return strs;
 
 Implement the encode and decode methods.
 
-Note:
-The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
-Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
-Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+ 
+Solution:  
+  
+class Codec:
+    def encode(self, strs): 
+        return ''.join(s.replace('|', '||') + ' | ' for s in strs) 
+    def decode(self, s): 
+        return [t.replace('||', '|') for t in s.split(' | ')[:-1]]
