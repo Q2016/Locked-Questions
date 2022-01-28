c837467cd0822554128d17aa86e673e27522c78c
Q2016 <k.sohraby@gmail.com>
2022-01-28 02:05:11 -0800

    Update 274. H-Index.py

--- 274. H-Index.py
+++ 274. H-Index.py
@@ -1,10 +1,16 @@
-# Counting sort.
-class Solution(object):
+Question
+Given an array of integers citations where citations[i] is the number of citations a researcher received for 
+their ith paper, return compute the researcher's h-index. Definition of h-index: A scientist has an index h if 
+h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
+If there are several possible values for h, the maximum one is taken as the h-index.
+
+Example 1:
+Input: citations = [3,0,6,1,5], Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+    # Counting sort.
     def hIndex(self, citations):
-        """
-        :type citations: List[int]
-        :rtype: int
-        """
         n = len(citations);
         count = [0] * (n + 1)
         for x in citations:
@@ -21,14 +27,11 @@ class Solution(object):
                 return i
         return h
 
-# Time:  O(nlogn)
-# Space: O(1)
-class Solution2(object):
+# Time:  O(n)
+# Space: O(1)    
+    
+
     def hIndex(self, citations):
-        """
-        :type citations: List[int]
-        :rtype: int
-        """
         citations.sort(reverse=True)
         h = 0
         for x in citations:
@@ -37,3 +40,7 @@ class Solution2(object):
             else:
                 break
         return h
+
+
+# Time:  O(nlogn)
+# Space: O(1)    
