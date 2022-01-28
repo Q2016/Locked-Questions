34cb8e311e8e6eb20b131e6b7c091efd7fda3601
Q2016 <k.sohraby@gmail.com>
2022-01-27 19:43:32 -0800

    Update 285. Inorder Successor in BST.py

--- 285. Inorder Successor in BST.py
+++ 285. Inorder Successor in BST.py
@@ -1,20 +1,15 @@
+Question:
 Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
 
-Note: If the given node has no in-order successor in the tree, return null.
-
 Example 1:
-
-Input: root = [2,1,3], p = 1
+Input: root = [2,1,3], p = 1, Output: 2
 
   2
  / \
 1   3
 
-Output: 2
 Example 2:
-
-Input: root = [5,3,6,2,4,null,null,1], p = 6
-
+Input: root = [5,3,6,2,4,null,null,1], p = 6, Output: null
       5
      / \
     3   6
@@ -23,28 +18,14 @@ Input: root = [5,3,6,2,4,null,null,1], p = 6
  /   
 1
 
-Output: null
+
+Solution: Inorder
+The in-order successor of a node is the left-most node in its right sub-tree, if exists. If not, then it is the root of the 
+smallest left sub-tree this node is in.
 
 
-
-Solution
-The in-order successor of a node is the left-most node in its right sub-tree, 
-if exists. If not, then it is the root of the smallest left sub-tree this node is in.
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
     def inorderSuccessor(self, root, p):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :rtype: TreeNode
-        """
+
         successor = None
         if p.right is not None:
             node = p.right
@@ -66,5 +47,4 @@ class Solution(object):
                     node = node.left
                 else:
                     node = node.right
-
         return successor
