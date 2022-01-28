b1204aa12ed4d1b1af18a857a0b05d5d44692fec
Q2016 <k.sohraby@gmail.com>
2022-01-28 08:37:28 -0800

    Update 397. Integer Replacement.py

--- 397. Integer Replacement.py
+++ 397. Integer Replacement.py
@@ -1,44 +1,22 @@
+Question:
+# Given a positive integer n and you can do operations as follow: If n is even, replace n with n/2. If n is odd, 
+you can replace n with either n + 1 or n - 1. What is the minimum number of replacements needed for n to become 1?
 
-  
-# Time:  O(logn)
-# Space: O(1)
+Example 1:
+Input:8, Output: 3, Explanation: 8 -> 4 -> 2 -> 1
 
-# Given a positive integer n and you can do operations as follow:
-#
-# If n is even, replace n with n/2.
-# If n is odd, you can replace n with either n + 1 or n - 1.
-# What is the minimum number of replacements needed for n to become 1?
-#
-# Example 1:
-#
-# Input:
-# 8
-#
-# Output:
-# 3
-#
-# Explanation:
-# 8 -> 4 -> 2 -> 1
-# Example 2:
-#
-# Input:
-# 7
-#
-# Output:
-# 4
-#
-# Explanation:
-# 7 -> 8 -> 4 -> 2 -> 1
-# or
-# 7 -> 6 -> 3 -> 2 -> 1
-
+      
+Solution:      
+Denote f(n) the minimum number of jumps from n to 1. By definition, we have the recurrence 
+f(1) = 0, f(2n) = 1 + f(n), f(2n + 1) = min(f(2n) + 1, f(2n + 2) + 1). First notice that this sequence is well defined 
+because f(2n + 2) = f(n + 1) + 1, so f(2n + 1) = min(f(2n) + 1, f(n + 1) + 2). Every element is defined by some element before it.
+We want to show (*):
+If n % 4 = 3 and n != 3, then f(n) = f(n + 1) + 1.
+If n % 4 = 1 or n = 3, then f(n) = f(n - 1) + 1.
+This gives us an O(log n) time, O(1) space solution.      
 # Iterative solution.
-class Solution(object):
     def integerReplacement(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+
         result = 0
         while n != 1:
             b = n & 3
@@ -50,20 +28,16 @@ class Solution(object):
                 n -= 1
             else:
                 n /= 2
-            result += 1
-        
+            result += 1 
         return result
-            
-
+      
 # Time:  O(logn)
-# Space: O(logn)
+# Space: O(1)            
+
+
 # Recursive solution.
-class Solution2(object):
     def integerReplacement(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+
         if n < 4:
             return [0, 0, 1, 2][n]
         if n % 4 in (0, 2):
@@ -72,3 +46,7 @@ class Solution2(object):
             return self.integerReplacement((n - 1) / 4) + 3
         else:
             return self.integerReplacement((n + 1) / 4) + 3
+
+# Time:  O(logn)
+# Space: O(logn)
+          
