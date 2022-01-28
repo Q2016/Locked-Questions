0b8dac4a9e1353a5692d92420349e636b9a3da48
Q2016 <k.sohraby@gmail.com>
2022-01-27 19:45:29 -0800

    Update 281. Zigzag Iterator.py

--- 281. Zigzag Iterator.py
+++ 281. Zigzag Iterator.py
@@ -1,44 +1,23 @@
+Question:
 Given two 1d vectors, implement an iterator to return their elements alternately.For example, given two 1d vectors:
-v1 = [1, 2]
-v2 = [3, 4, 5, 6]
+v1 = [1, 2], v2 = [3, 4, 5, 6]
 By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
 Follow up: What if you are given k 1d vectors? How well can your code be extended to such cases?
   
   
+Solution:  
   
-  
-  
-  
-
-  
-  
-  
-  https://github.com/ShiqinHuo/LeetCode-Python/blob/master/Python/zigzag-iterator.py
   class ZigzagIterator(object):
 
     def __init__(self, v1, v2):
-        """
-        Initialize your q structure here.
-        :type v1: List[int]
-        :type v2: List[int]
-        """
         self.q = collections.deque([(len(v), iter(v)) for v in (v1, v2) if v])
 
     def next(self):
-        """
-        :rtype: int
-        """
         len, iter = self.q.popleft()
         if len > 1:
             self.q.append((len-1, iter))
         return next(iter)
 
     def hasNext(self):
-        """
-        :rtype: bool
-        """
         return bool(self.q)
 
-# Your ZigzagIterator object will be instantiated and called as such:
-# i, v = ZigzagIterator(v1, v2), []
-# while i.hasNext(): v.append(i.next())
