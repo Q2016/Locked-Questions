211874f2b2a8dc62a69dbbdac25affa580f48c24
Q2016 <k.sohraby@gmail.com>
2022-01-28 14:18:26 -0800

    Update 373. Find K Pairs with Smallest Sums.py

--- 373. Find K Pairs with Smallest Sums.py
+++ 373. Find K Pairs with Smallest Sums.py
@@ -1,71 +1,24 @@
-/*
-Several solutions from naive to more elaborate. I found it helpful to visualize the input as an m×n matrix of sums, for example for nums1=[1,7,11], and nums2=[2,4,6]:
+Question:
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k. Define a pair (u, v) which consists of 
+one element from the first array and one element from the second array. Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with 
+the smallest sums.
 
+Example 1:
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+
+Solution: BST or there is heap solution as well
+It's helpful to visualize the input as an m×n matrix of sums, for example for nums1=[1,7,11], and nums2=[2,4,6]:
       2   4   6
    +------------
  1 |  3   5   7
  7 |  9  11  13
 11 | 13  15  17
+Of course the smallest pair overall is in the top left corner, the one with sum 3. We don't even need to look anywhere else. 
+After including that pair in the output, the next-smaller pair must be the next on the right (sum=5) or the next below (sum=9).
+We can keep a "horizon" of possible candidates, implemented as a heap / priority-queue, and roughly speaking we'll grow from the 
+top left corner towards the right/bottom. 
 
-Of course the smallest pair overall is in the top left corner, the one with sum 3. We don't even need to look anywhere else. After including that pair in the output, the next-smaller pair must be the next on the right (sum=5) or the next below (sum=9). We can keep a "horizon" of possible candidates, implemented as a heap / priority-queue, and roughly speaking we'll grow from the top left corner towards the right/bottom. That's what my solution 5 does. Solution 4 is similar, not quite as efficient but a lot shorter and my favorite.
-
-          
-The previous solution right away considered (the first pair of) all matrix rows (see visualization above). This one doesn't. It starts off only with the very first pair at the top-left corner of the matrix, and expands from there as needed. Whenever a pair is chosen into the output result, the next pair in the row gets added to the priority queue of current options. Also, if the chosen pair is the first one in its row, then the first pair in the next row is added to the queue.
-
-Here's a visualization of a possible state:
-
-# # # # # ? . .
-# # # ? . . . .
-# ? . . . . . .   "#" means pair already in the output
-# ? . . . . . .   "?" means pair currently in the queue
-# ? . . . . . .
-? . . . . . . .
-. . . . . . . .
-
-As I mentioned in the comments, that could be further improved. Two of those ? don't actually need to be in the queue yet. I'll leave that as an exercise for the reader :-)
-
-    def kSmallestPairs(self, nums1, nums2, k):
-        queue = []
-        def push(i, j):
-            if i < len(nums1) and j < len(nums2):
-                heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
-        push(0, 0)
-        pairs = []
-        while queue and len(pairs) < k:
-            _, i, j = heapq.heappop(queue)
-            pairs.append([nums1[i], nums2[j]])
-            push(i, j + 1)
-            if j == 0:
-                push(i + 1, 0)
-        return pairs
-
-*/
-
-class Solution {
-private:
-    struct mycompare{
-        bool operator()(pair<int, int>& p1, pair<int, int>& p2){
-            return p1.first + p1.second < p2.first + p2.second;
-        }
-    };
-public:
-    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int, int>> res;
-        priority_queue<pair<int,int>, vector<pair<int, int> >, mycompare> pq;
-        for(int i = 0; i < min((int)nums1.size(), k); i++){
-            for(int j = 0; j < min((int)nums2.size(), k); j++){
-                if(pq.size() < k)
-                    pq.push(make_pair(nums1[i], nums2[j]));
-                else if(nums1[i] + nums2[j] < pq.top().first + pq.top().second){
-                    pq.push(make_pair(nums1[i], nums2[j]));
-                    pq.pop();
-                }
-            }
-        }
-        while(!pq.empty()){
-            res.push_back(pq.top());
-            pq.pop();
-        }
-        return res;
-    }
-};
+Creating the matrix now it's 378. Kth Smallest Element in a Sorted Matrix problem.
