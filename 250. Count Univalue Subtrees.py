4baa0736a461a3abacf48c5106970bfd3d49d5b8
Q2016 <k.sohraby@gmail.com>
2022-01-28 07:56:37 -0800

    Update 250. Count Univalue Subtrees.py

--- 250. Count Univalue Subtrees.py
+++ 250. Count Univalue Subtrees.py
@@ -1,27 +1,31 @@
-This question is not well done, and the idea is already there, but I always make mistakes in logic. I am really a pig brain.
+Question:
+Given a binary tree, count the number of uni-value subtrees. A Uni-value subtree means all nodes of the subtree have the same value.
+
+For example: Given binary tree,
+
+              5
+             / \
+            1   5
+           / \   \
+          5   5   5
+return 4.    
+
+Solution: bottom-up Postorder (educational)
 
 In traverse, there are two points when judging whether a node is a uni-value tree:
 His left and right subtrees are uni-value trees;
 It is equal to the value of the left and right subtrees.
-is indispensable. .
 
-Once it is a uni-value tree, the total number is ++. .
+1) The first solution wont work
 
-But when comparing the subtree VALUE, it involves the case that the subtree is NULL, and one more case is judged to be not NULL. In the beginning, I wanted to write a line, the logic was messed up, the pretense failed, and I was beaten in the face.
-
-public class Solution 
-{
-    int res = 0;
-    public int countUnivalSubtrees(TreeNode root) 
-    {
+    res = 0;
+    def countUnivalSubtrees(TreeNode root) 
         if(root == null) return 0;
         if(root.left == null && root.right == null) return 1;
         helper(root);
         return res;
-    }
     
-    public boolean helper(TreeNode root)
-    {
+    def helper(TreeNode root)
         if(root == null) return true;
         
         boolean left = helper(root.left);
@@ -30,64 +34,32 @@ public class Solution
         if(left && right)
             if((root.left == null || root.left.val == root.val) && (root.right ==null || root.right.val == root.val))
                 res++;
-            else return false;
-        
-        return left && right;
-        
-    }
+            else return false;        
+        return left and right;
 
-}
+2) Second brush.
 
-/*
-[1,-2,3,-2,null,null,null,null,5]
-[5,1,5,5,5,null,5]
-[5,1,5,5,5,1,1]
-[5,5,5,5,5,5,5]
-[7,82,82,-79,98,98,-79,-79,null,-28,-24,-28,-24,null,-79,null,97,65,-4,null,3,-4,65,3,null,97]
+Similar to problems 124 and 333, but simpler than those two. I made a mistake at the beginning, thinking it was 
+the sum of the total node, but it is not that complicated, just the sum of the subTree.
 
-*/
-Second brush.
-
-Count the number of subtrees where all vals are equal. For example, [2,2] itself is one, and then there is another leaf, so there are two. The example given is a good illustration of what is going on. .
-
-Still the same as the previous one, bottom-up via post order traversal.
-
-The information that needs to be passed on is:
-
-Am I unique-value-subtree.
-Gone. Or my root.val, but it can be obtained directly from root.left.val or root.right.val above, so it is not passed up.
-So there is only 1) this information, then there is no need to create a new class.
-
-It's a bit similar to the two I made today, 124 and 333, but simpler than those two.
-
-I made a mistake at the beginning, thinking it was the sum of the total node, but it is not that complicated, just the sum of the subTree.
-
-Time: O(n)
-Space: O(n)
-
-public class Solution {
-    int res = 0;
-    public int countUnivalSubtrees(TreeNode root) {
+    res = 0;
+    def countUnivalSubtrees(TreeNode root) 
         if (root == null) return 0;
         postOrder(root);
         return res;
-    }
     
-    public boolean postOrder(TreeNode root) {
+    def postOrder(TreeNode root) 
         if (root == null) return true;
         
         boolean left = postOrder(root.left);
         boolean right = postOrder(root.right);
         
-        if (left && right) {
-            if ((root.left == null || root.val == root.left.val) && 
-                (root.right == null || root.val == root.right.val)) {
+        if (left && right) 
+            if ((root.left == null || root.val == root.left.val) && (root.right == null || root.val == root.right.val)) 
                     res ++;
                     return true;
-            }
-        } 
-        
         return false;
-        
-    }
-}
+
+
+Time: O(n)
+Space: O(n)
