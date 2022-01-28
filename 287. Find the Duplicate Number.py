02550d8830ed7ca72337ca4dcffef2055889c4f3
Q2016 <k.sohraby@gmail.com>
2022-01-27 19:32:05 -0800

    Update 287. Find the Duplicate Number.py

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -1,10 +1,12 @@
-Algorithm
+Question:
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and uses only constant extra space.    
+    
 
-This problem has many questions:
-https://leetcode.com/problems/find-the-duplicate-number/solution/
-
-
-class Solution:
+Solution: Sum of Set Bits
+For solution read https://leetcode.com/problems/find-the-duplicate-number/solution/
+    
     def findDuplicate(self, nums: List[int]) -> int:
         duplicate = 0
         n = len(nums)
