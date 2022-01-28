5453932c494018740940bc8be5b0075f1121ffb1
Q2016 <k.sohraby@gmail.com>
2022-01-28 02:40:57 -0800

    Update 254. Factor Combinations.py

--- 254. Factor Combinations.py
+++ 254. Factor Combinations.py
@@ -1,39 +1,10 @@
-Problem:
-Numbers can be regarded as product of its factors. For example,
-
-8 = 2 x 2 x 2;
-  = 2 x 4.
-
-Write a function that takes an integer n and return all possible combinations of its factors.
-
-Note: 
-You may assume that n is always positive.
-Factors should be greater than 1 and less than n.
+Question:
+Numbers can be regarded as product of its factors. For example, 8 = 2 x 2 x 2; = 2 x 4. Write a function that takes an 
+integer n and return all possible combinations of its factors.
 
 Examples: 
-input: 1
-output: 
-[]
-input: 37
-output: 
-[]
 input: 12
-output:
-  [
-    [2, 6],
-    [2, 2, 3],
-    [3, 4]
-  ]  
-input: 32
-output:
-[
-  [2, 16],
-  [2, 2, 8],
-  [2, 2, 2, 4],
-  [2, 2, 2, 2, 2],
-  [2, 4, 4],
-  [4, 8]
-]
+output:[[2, 6],[2, 2, 3],[3, 4]]  
 
 
 class Solution(object):
@@ -41,15 +12,11 @@ class Solution(object):
         self.factors = dict()
 
     def getFactors(self, n):
-        """
-        :type n: int
-        :rtype: List[List[int]]
-        """
+
         if n == 1:
             return []
         elif n in self.factors:
             return self.factors[n]
-
         else:
             res = list()
             for f in range(2, int(sqrt(n)) + 1):
