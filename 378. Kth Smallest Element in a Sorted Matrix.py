495d13f03c72ce7020af959afcfb5166dd4bbb36
Q2016 <k.sohraby@gmail.com>
2022-01-28 10:56:34 -0800

    Update 378. Kth Smallest Element in a Sorted Matrix.py

--- 378. Kth Smallest Element in a Sorted Matrix.py
+++ 378. Kth Smallest Element in a Sorted Matrix.py
@@ -1,34 +1,38 @@
-/*
-Using Heap :- Here, we know that the row and column is already sorted. Therefore, instead of traversing in the entire matrix we can take advantage of that. We can take a Min Heap and push the First row(value, indexes) into Min heap. You can push first column also, and write your answer according to that.
-Now,follow these steps:-
+Question:
+Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+Note that it is the kth smallest element in the sorted order, not the kth distinct element. You must find a solution with a memory 
+complexity better than O(n^2).
 
-pop the element from min heap and
-check whether it is the Kth smallest element, if it is return the value,
-else push the next row value (Note:- column is going to remain same).
-The reason behind this logic is , when we remove the smallest element from Min Heap (i.e our 1st smallest element, which is always present in the 1st row and 1st column), then the 2nd smallest element may lie in that 2nd row and 1st column or it may be present in the First Row and column greater than 1.
-Time Complexity:- O(NlogN)
-Space Complexity:- O(N)
-*/
+Example 1:
+Input: matrix = [[1, 5, 9 ],
+                 [10,11,13],
+                 [12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13    
 
-class Solution {
-public:
-int kthSmallest(vector<vector<int>>& mat, int k) {
-    priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
-    int n=mat.size();
-    for(int i=0; i<n; ++i)
-        pq.push({mat[0][i],{0,i}});
 
-    int ans;
-        
-    while(k--){
-        int val=pq.top().first;
-        int row=pq.top().second.first;
-        int col=pq.top().second.second;
-        pq.pop();
-        ans=val;
-        if(row!=n-1)
-            pq.push({mat[row+1][col],{row+1,col}});
-    }
-        return ans;
-    }
-};
+
+Solution: BST
+    
+    def kthSmallest(self, matrix, k):
+        m, n = len(matrix), len(matrix[0])  # For general, the matrix need not be a square
+
+        def countLessOrEqual(x):
+            cnt = 0
+            c = n - 1  # start with the rightmost column
+            for r in range(m):
+                while c >= 0 and matrix[r][c] > x: c -= 1  # decrease column until matrix[r][c] <= x
+                cnt += (c + 1)
+            return cnt
+
+        left, right = matrix[0][0], matrix[-1][-1]
+        ans = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if countLessOrEqual(mid) >= k:
+                ans = mid
+                right = mid - 1  # try to looking for a smaller value in the left side
+            else:
+                left = mid + 1  # try to looking for a bigger value in the right side
+
+        return ans    
