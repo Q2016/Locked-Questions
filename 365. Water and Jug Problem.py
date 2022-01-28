a251196dd1f0762371ef0b5d3a33d0f09f035dd3
Q2016 <k.sohraby@gmail.com>
2022-01-28 15:04:44 -0800

    Update 365. Water and Jug Problem.py

--- 365. Water and Jug Problem.py
+++ 365. Water and Jug Problem.py
@@ -1,14 +1,22 @@
+Question:
+You are given two jugs with capacities jug1Capacity and jug2Capacity liters. There is an infinite amount of water supply 
+available. Determine whether it is possible to measure exactly targetCapacity liters using these two jugs.
+If targetCapacity liters of water are measurable, you must have targetCapacity liters of water contained within one or 
+both buckets by the end. Operations allowed: Fill any of the jugs with water. Empty any of the jugs.
+Pour water from one jug into another till the other jug is completely full, or the first jug itself is empty.
+ 
+Example 1:
+Input: jug1Capacity = 3, jug2Capacity = 5, targetCapacity = 4
+Output: true
+Explanation: The famous Die Hard example     
+
+
+Solution:    
 It took me a while to understand the GCD method. My first attempt to this problem was using BFS, which is very intuitive and easy to understand.
 The complexity is definitely much longer than GCD-based method.
 
-class Solution(object):
     def canMeasureWater(self, x, y, z):
-        """
-        :type x: int
-        :type y: int
-        :type z: int
-        :rtype: bool
-        """
+
         if x > y:
             temp = x;
             x = y;
