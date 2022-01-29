c54ab28d7bfd9092b090324b5751b94250af51b5
Q2016 <k.sohraby@gmail.com>
2022-01-28 17:59:14 -0800

    Update 36. Valid Sudoku.py

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -1,35 +1,11 @@
-Three flags are used to check whether a number appear.
+Question:
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition. Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.    
 
-used1: check each row
 
-used2: check each column
-
-used3: check each sub-boxes
-
-class Solution
-{
-public:
-    bool isValidSudoku(vector<vector<char> > &board)
-    {
-        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
-        
-        for(int i = 0; i < board.size(); ++ i)
-            for(int j = 0; j < board[i].size(); ++ j)
-                if(board[i][j] != '.')
-                {
-                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
-                    if(used1[i][num] || used2[j][num] || used3[k][num])
-                        return false;
-                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
-                }
-        
-        return true;
-    }
-};
-
-Wow this is an amazing solution.
-k = i / 3 * 3 + j / 3 wow!
-for those who need to visualize k > here
+Solution:
+Visualization of the index k = i / 3 * 3 + j / 3 :
 
 0  0  0 | 1  1  1 | 2  2  2
 0  0  0 | 1  1  1 | 2  2  2
@@ -41,4 +17,24 @@ for those who need to visualize k > here
 --------+----------+--------
 6  6  6 | 7  7  7 | 8  8  8
 6  6  6 | 7  7  7 | 8  8  8
-6  6  6 | 7  7  7 | 8  8  8
+6  6  6 | 7  7  7 | 8  8  8    
+
+    def isValidSudoku(board)
+    
+        used1[9][9] = {0} #check each row
+        used2[9][9] = {0} #check each column
+        used3[9][9] = {0} #check each sub-boxes
+        
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if (board[i][j] != '.'):
+                    num = int(board[i][j])
+                    k = i / 3 * 3 + j / 3 # This gives the index of the matrix below
+                    if (used1[i][num] or used2[j][num] or used3[k][num]):
+                        return false
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1
+        
+        return true
+    
+
+
