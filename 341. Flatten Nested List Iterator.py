0964d419ae5fce93b3d41f10919d1bdfaf3593c3
Q2016 <k.sohraby@gmail.com>
2022-01-29 07:44:44 -0800

    Update 341. Flatten Nested List Iterator.py

--- 341. Flatten Nested List Iterator.py
+++ 341. Flatten Nested List Iterator.py
@@ -1,15 +1,32 @@
-In my opinion an iterator shouldn't copy the entire data (which some solutions have done) but just iterate over the original data structure.
+Question:
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also 
+be integers or other lists. Implement an iterator to flatten it. Implement the NestedIterator class:
+NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+int next() Returns the next integer in the nested list.
+boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+Your code will be tested with the following pseudocode:
+initialize iterator with nestedList
+res = []
+while iterator.hasNext()
+    append iterator.next() to the end of res
+return res
+If res matches the expected flattened list, then your code will be judged as correct.
 
-I keep the current progress in a stack. My hasNext tries to find an integer. My next returns it and moves on. I call hasNext in next because hasNext is optional. Some user of the iterator might call only next and never hasNext, e.g., if they know how many integers are in the structure or if they want to handle the ending with exception handling.
+Example 1:
+Input: nestedList = [[1,1],2,[1,1]]
+Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].    
 
-Python
 
-Using a stack of [list, index] pairs.
+Solution:        
+I keep the current progress in a stack. My hasNext tries to find an integer. My next returns it and moves on. I call hasNext 
+in next because hasNext is optional. Some user of the iterator might call only next and never hasNext, e.g., if they know how 
+many integers are in the structure or if they want to handle the ending with exception handling.
 
 class NestedIterator(object):
 
     def __init__(self, nestedList):
-        self.stack = [[nestedList, 0]]
+        self.stack = [[nestedList, 0]] # Using a stack of [list, index] pairs.
 
     def next(self):
         self.hasNext()
