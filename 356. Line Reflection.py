8ead2bee86958b62300c5f7990a240baf10057cb
Q2016 <k.sohraby@gmail.com>
2022-01-28 20:10:20 -0800

    Update 356. Line Reflection.py

--- 356. Line Reflection.py
+++ 356. Line Reflection.py
@@ -1,23 +1,17 @@
-Problem:
+Question:
 Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points.
+Follow up: Could you do better than O(n^2)?
 
 Example 1: Given points = [[1,1],[-1,1]], return true.
 
-Example 2: Given points = [[1,1],[-1,-1]], return false.
 
-Follow up: Could you do better than O(n2)?
-
-Hint:
-
+Solution:
 Find the smallest and largest x-value for all points. If there is a line then it 
 should be at y = (minX + maxX) / 2. For each point, make sure that it has a reflected point in the opposite side.
 
-class Solution(object):
+
     def isReflected(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: bool
-        """
+
         pointSet = set()
         maxNum = -sys.maxint
         minNum = sys.maxint
