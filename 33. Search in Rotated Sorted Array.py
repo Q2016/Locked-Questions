643d373adb915855dcf5d5223d9dd445dce6e0f3
Q2016 <k.sohraby@gmail.com>
2022-01-29 20:06:14 -0800

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -1,52 +1,41 @@
+Question:
+There is an integer array nums sorted in ascending order (with distinct values). Prior to being passed to your function, 
+nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is 
+[nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at 
+pivot index 3 and become [4,5,6,7,0,1,2]. Given the array nums after the possible rotation and an integer target, return the index 
+of target if it is in nums, or -1 if it is not in nums. You must write an algorithm with O(log n) runtime complexity.
 
-https://leetcode.com/problems/search-in-rotated-sorted-array/
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4    
 
 
-
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-
-        if(nums.size()==0) return -1;
-        int lo = 0, hi = nums.size() - 1;
-        while (lo < hi)
-        {
-            int mid = lo + (hi - lo)/2;
-            //target and mid are on the same side
-            if((nums[mid]-nums[nums.size()-1])*(target-nums[nums.size()-1])>0)
-            {
-                if(nums[mid] < target)
-                    lo = mid + 1;
-                else
-                    hi = mid;
-            }else if(target > nums[nums.size()-1])
-                hi = mid; // target on the left side
-            else
-                lo = mid + 1; // target on the right side
-        }
-        // now hi == lo
-        return nums[lo] == target ? lo : -1;      
-    }
-};
-
-/*
-Explanation
-
+Solution:
 Let's say nums looks like this: [12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
 Because it's not fully sorted, we can't do normal binary search. But here comes the trick:
-
 If target is let's say 14, then we adjust nums to this, where "inf" means infinity:
 [12, 13, 14, 15, 16, 17, 18, 19, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf]
-
 If target is let's say 7, then we adjust nums to this:
 [-inf, -inf, -inf, -inf, -inf, -inf, -inf, -inf, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+And then we can simply do ordinary binary search.    
 
-And then we can simply do ordinary binary search.
 
-Of course we don't actually adjust the whole array but instead adjust only on the fly only the elements we look at. And the adjustment is done by comparing both the target and the actual element against nums[0].
+    def search(nums, target) :
 
-Code
-
-If nums[mid] and target are "on the same side" of nums[0], we just take nums[mid]. Otherwise we use -infinity or +infinity as needed.
-*/
+        if (len(nums)==0): 
+            return -1;
+        lo = 0, 
+        hi = len(nums)-1;
+        while (lo < hi):
+            mid = lo + (hi - lo)/2;
+            #target and mid are on the same side
+            if (nums[mid]-nums[len(nums)-1])*(target-nums[len(nums)-1])>0:    
+                if nums[mid] < target:
+                    lo = mid + 1;
+                else:
+                    hi = mid;
+            else if: target > nums[len(nums)-1]
+                hi = mid; # target on the left side
+            else:
+                lo = mid + 1; # target on the right side
+       return lo if nums[lo] == target else -1;      
