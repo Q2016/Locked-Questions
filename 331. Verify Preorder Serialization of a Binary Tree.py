ae8eb8feeb45729950d03f6aeb68e158ef543268
Q2016 <k.sohraby@gmail.com>
2022-01-29 17:11:30 -0800

    Update 331. Verify Preorder Serialization of a Binary Tree.py

--- 331. Verify Preorder Serialization of a Binary Tree.py
+++ 331. Verify Preorder Serialization of a Binary Tree.py
@@ -1,3 +1,12 @@
+Question:
+One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
+
+
+For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
+
+Given a string of comma-separated values preorder, return true if it is a correct preorder traversal serialization of a binary tree.    
+
+
 The simplest python solution with explanation (no stack, no recursion)
 
 
@@ -9,6 +18,9 @@ for each node we check if we still have empty slots to put it in.
 
 a null node occupies one slot.
 a non-null node occupies one slot before he creates two more. the net gain is one.
+
+
+
 class Solution(object):
     def isValidSerialization(self, preorder):
         """
