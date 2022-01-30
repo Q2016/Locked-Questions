c00c32c7f68f022852b06e92be839b8af4e6ff9d
Q2016 <k.sohraby@gmail.com>
2022-01-29 19:47:39 -0800

    Update 331. Verify Preorder Serialization of a Binary Tree.py

--- 331. Verify Preorder Serialization of a Binary Tree.py
+++ 331. Verify Preorder Serialization of a Binary Tree.py
@@ -1,53 +1,36 @@
 Question:
-One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
+One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the 
+node's value. If it is a null node, we record using a sentinel value such as '#'. For example, the above binary tree 
+can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
+Given a string of comma-separated values preorder, return true if it is a correct preorder traversal serialization of 
+a binary tree.              9
+                          /   \              
+                        3       2
+                      /   \      \
+                    4       1      6
+Note: You are not allowed to reconstruct the tree.        
 
 
-For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
+Solution:
 
-Given a string of comma-separated values preorder, return true if it is a correct preorder traversal serialization of a binary tree.    
+In a binary tree, if we consider null as leaves, then first, all non-null node provides 2 outdegree and 1 indegree (2 children and 1 parent), 
+except root. second, all null node provides 0 outdegree and 1 indegree (0 child and 1 parent).
+Suppose we try to build this tree. During building, we record the difference between out degree and in degree diff = outdegree - indegree. 
+When the next node comes, we then decrease diff by 1, because the node provides an in degree. If the node is not null, we increase diff by 2, 
+because it provides two out degrees. If a serialization is correct, diff should never be negative and diff will be zero when finished.
+
+def isValidSerialization(preorder):
+    nodes = preorder.split(",")
+    diff = 1
+    for node in nodes:
+        if (--diff < 0): 
+            return false
+        if node!="#": 
+            diff += 2
+    
+    return diff == 0
 
 
-The simplest python solution with explanation (no stack, no recursion)
-
-
-We just need to remember how many empty slots we have during the process.
-
-Initially we have one ( for the root ).
-
-for each node we check if we still have empty slots to put it in.
-
-a null node occupies one slot.
-a non-null node occupies one slot before he creates two more. the net gain is one.
 
 
 
-class Solution(object):
-    def isValidSerialization(self, preorder):
-        """
-        :type preorder: str
-        :rtype: bool
-        """
-        # remember how many empty slots we have
-        # non-null nodes occupy one slot but create two new slots
-        # null nodes occupy one slot
-        
-        p = preorder.split(',')
-        
-        #initially we have one empty slot to put the root in it
-        slot = 1
-        for node in p:
-            
-            # no empty slot to put the current node
-            if slot == 0:
-                return False
-                
-            # a null node?
-            if node == '#':
-                # ocuppy slot
-                slot -= 1
-            else:
-                # create new slot
-                slot += 1
-        
-        #we don't allow empty slots at the end
-        return slot==0
