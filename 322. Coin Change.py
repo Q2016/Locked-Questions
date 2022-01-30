c952efd878de88b4183a0b4474651edda529d45d
Q2016 <k.sohraby@gmail.com>
2022-01-29 21:59:30 -0800

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,32 +1,16 @@
-class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
+Question:
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin.
 
-        if(amount<1) return 0;
-        vector<int> count;
-        count.resize(amount);
-        return helper(coins, amount, count);
-    }
-
-    int helper(vector<int>& coins, int rem, vector<int>& count) { 
-      // rem: remaining coins after the last step; count[rem]: minimum number of coins to sum up to rem
-        if(rem<0) return -1; // not valid
-        if(rem==0) return 0; // completed
-        if(count[rem-1] != 0) return count[rem-1]; // already computed, so reuse
-        int min = INT_MAX;
-        for(int coin : coins) {
-            int res = helper(coins, rem-coin, count);
-            if(res>=0 && res < min)
-                min = 1+res;
-        }
-        count[rem-1] = (min==INT_MAX) ? -1 : min;
-        return count[rem-1];
-    }
-};
+Example 1:
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
 
 
-/*
-#Recursive Method:#
+Solution: Recursive Method
+    
 The idea is very classic dynamic programming: think of the last step we take. 
 Suppose we have already found out the best way to sum up to amount a, then for the last step, 
 we can choose any coin type which gives us a remainder r where r = a-coins[i] for all i's. 
@@ -34,4 +18,16 @@ For every remainder, go through exactly the same process as before until either 
 remainder is 0 or less than 0 (meaning not a valid solution). With this idea, the only 
 remaining detail is to store the minimum number of coins needed to sum up to r so that 
 we don't need to recompute it over and over again.
-*/
+
+    def coinChange(self, coins, amount):
+
+        rs = [amount+1] * (amount+1)
+        rs[0] = 0
+        for i in xrange(1, amount+1):
+            for c in coins:
+                if i >= c:
+                    rs[i] = min(rs[i], rs[i-c] + 1)
+
+        if rs[amount] == amount+1:
+            return -1
+        return rs[amount]
