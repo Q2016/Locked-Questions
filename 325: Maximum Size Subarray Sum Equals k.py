c99a43d0f0ef51a6b889d63e0e25ed24f299d3f4
Q2016 <k.sohraby@gmail.com>
2022-01-29 21:28:03 -0800

    Update 325: Maximum Size Subarray Sum Equals k.py

--- 325: Maximum Size Subarray Sum Equals k.py
+++ 325: Maximum Size Subarray Sum Equals k.py
@@ -1,29 +1,20 @@
-https://github.com/ShiqinHuo/LeetCode-Python/blob/master/Python/maximum-size-subarray-sum-equals-k.py
-
-Description: Given an array nums and target value k, find the maximum length of a subarray that sums to k. If there isn’t one, return 0 instead.
+Question:
+Given an array nums and target value k, find the maximum length of a subarray that sums to k. If there isn’t one, return 0 instead.
 
 Example:
 Input: nums = [1, -1, 5, -2, 3], k = 3
 Output: 4
 
 
-Explanation
+Solution:
 Record the sum at each index location i. If sum – k has been found before at index j, 
 that means the sum of nums[j: i] is equal to k (because the sum of nums[:i] is the sum and the sum of 
 nums[:j] equals sum – k). Every time we see a previous sum – k found for location i, we can check if 
 the difference between the current position and the sum – k position is the longest.  
   
   
-# Time:  O(n)
-# Space: O(n)
-
-class Solution(object):
     def maxSubArrayLen(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+
         sums = {}
         cur_sum, max_len = 0, 0
         for i in xrange(len(nums)):
@@ -35,3 +26,7 @@ class Solution(object):
             if cur_sum not in sums:
                 sums[cur_sum] = i  # Only keep the smallest index.
         return max_len
+
+      
+# Time:  O(n)
+# Space: O(n)      
