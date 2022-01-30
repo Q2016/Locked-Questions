395d6649948fb1c8f7bd410d1e43d14d0e3572d5
Q2016 <k.sohraby@gmail.com>
2022-01-29 22:21:32 -0800

    Update 314. Binary Tree Vertical Order Traversal.py

--- 314. Binary Tree Vertical Order Traversal.py
+++ 314. Binary Tree Vertical Order Traversal.py
@@ -1,12 +1,9 @@
+Question:
 Given a binary tree, return thevertical ordertraversal of its nodes' values. (ie, from top to bottom, column by column).
-
 If two nodes are in the same row and column, the order should be from left to right.
 
 Examples 1:
-
-Input:
-[3,9,20,null,null,15,7]
-
+Input: [3,9,20,null,null,15,7], Output: [[9],[3,15],[20],[7]]
    3
   /\
  /  \
@@ -15,82 +12,11 @@ Input:
    /  \
   15   7 
 
-Output:
 
-[
-  [9],
-  [3,15],
-  [20],
-  [7]
-]
-Examples 2:
+Solution: BFS + hash solution.
 
-Input: 
-[3,9,8,4,0,1,7]
-
-     3
-    /\
-   /  \
-   9   8
-  /\  /\
- /  \/  \
- 4  01   7 
-
-Output:
-
-[
-  [4],
-  [9],
-  [3,0,1],
-  [8],
-  [7]
-]
-Examples 3:
-
-Input:
-[3,9,8,4,0,1,7,null,null,null,2,5]
- (0's right child is 2 and 1's left child is 5)
-
-     3
-    /\
-   /  \
-   9   8
-  /\  /\
- /  \/  \
- 4  01   7
-    /\
-   /  \
-   5   2
-
-
-Output:
-
-[
-  [4],
-  [9,5],
-  [3,0,1],
-  [8,2],
-  [7]
-]
-FB: Complexity!
-
-Thoughts
-
-Use map <col, list<val>> to record the col and list value pair. Use BFS to expand the tree to add entry
-
-# Time:  O(n)
-# Space: O(n)
-
-# BFS + hash solution.
-
-
-
-class Solution(object):
     def verticalOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+
         cols = collections.defaultdict(list)
         queue = [(root,0)]
 
@@ -98,5 +24,8 @@ class Solution(object):
             if node:
                 cols[col].append(node.val)
                 queue += (node.left, col - 1), (node.right, col + 1)
-
         return [cols[col] for col in sorted(cols)]
+   
+   
+# Time:  O(n)
+# Space: O(n)   
