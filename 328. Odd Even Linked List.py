1e94affd2e21a12531cab3c330e627f172d8c8f9
Q2016 <k.sohraby@gmail.com>
2022-01-29 21:00:29 -0800

    Update 328. Odd Even Linked List.py

--- 328. Odd Even Linked List.py
+++ 328. Odd Even Linked List.py
@@ -1,9 +1,19 @@
-class Solution:
+Question:
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even 
+indices, and return the reordered list. The first node is considered odd, and the second node is even, and so on. Note that the 
+relative order inside both the even and odd groups should remain as it was in the input. You must solve the problem in O(1) 
+extra space complexity and O(n) time complexity.    
+
+Example 1:
+Input: head = [1,2,3,4,5]    1--->2--->3--->4--->5
+Output: [1,3,5,2,4]          1--->3--->5--->2--->4
+
+    
+    
+Soluion:    
+    
     def oddEvenList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
+
         if not head:
             return head
         
@@ -26,13 +36,3 @@ class Solution:
         return head # We keep the start of the odd-node list in the first of our code
       
       
-Four conditions I doubt for the while-loop:
-
-odd and odd.next -> wrong when 1->2->3->4->None ( even nodes ) because even.next is None, which has no attribute 'next'
-odd and even.next -> wrong when 1->2->3->4->5->None ( odd nodes ) because even is None, which has no attribute 'next'
-even and odd.next -> wrong when 1->2->3->4->None ( even nodes ) because even.next is None, which has no attribute 'next'
-even and even.next -> correct
-1. when 1->2->3->4->5->None ( odd nodes ) even will become None first and at the same time, odd points at the last node 
-of the linked list; therefore, breaks from the while loop.
-2. when 1->2->3->4->None ( even nodes ) even.next will become None first and at the same time, odd points at the last-2 
-node of the linked list and even points at the last node of the linked list; therefore, breaks from the while loop.
