5076135cf5e35b1c9bcd61aabb71f8468dedb113
Q2016 <k.sohraby@gmail.com>
2022-01-31 10:44:58 -0800

    Update 48. Rotate Image.py

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -1,20 +1,12 @@
-
-https://leetcode.com/problems/rotate-image/
+Question:
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 
+2D matrix and do the rotation.
 
 
 Solution
-Approach 1: Rotate Groups of Four Cells
-Intuition
+The corners all move, we can iterate over each group of four cells and rotate them.
 
-Observe how the cells move in groups when we rotate the image.
-
-The corners all move
-
-We can iterate over each group of four cells and rotate them.
-
-Implementation
-
-class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix[0])
         for i in range(n // 2 + n % 2):
