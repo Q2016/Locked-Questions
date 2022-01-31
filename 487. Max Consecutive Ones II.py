713892f8ebb7ce039480629d7d958ce99dad8c8d
Q2016 <k.sohraby@gmail.com>
2022-01-30 21:05:39 -0800

    Update 487. Max Consecutive Ones II.py

--- 487. Max Consecutive Ones II.py
+++ 487. Max Consecutive Ones II.py
@@ -1,29 +1,18 @@
-Problem:
-Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0.
+Question:
+Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0. Follow up: What if the input 
+numbers come in one by one as an infinite stream? In other words, you can't store all numbers coming from the stream as it's too large 
+to hold in memory. Could you solve it efficiently?
 
 Example 1:
-
 Input: [1,0,1,1,0]
 Output: 4
-Explanation: Flip the first zero will get the the maximum number of consecutive 1s.
-    After flipping, the maximum number of consecutive 1s is 4.
-Note:
 
-The input array will only contain 0 and 1.
-The length of input array is a positive integer and will not exceed 10,000
-Follow up: What if the input numbers come in one by one as an infinite stream? 
-In other words, you can't store all numbers coming from the stream as it's too 
-large to hold in memory. Could you solve it efficiently?
 
-# Time:  O(n)
-# Space: O(1)
-
-class Solution(object):
+Solution:
+Explanation: Flip the first zero will get the the maximum number of consecutive 1s. After flipping, the maximum number of consecutive 1s is 4.
+    
     def findMaxConsecutiveOnes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+
         result, prev, curr = 0, 0, 0
         for n in nums:
             if n == 0:
@@ -32,3 +21,8 @@ class Solution(object):
             else:
                 curr += 1
         return min(max(result, prev+curr+1), len(nums))
+
+    
+    
+# Time:  O(n)
+# Space: O(1)    
