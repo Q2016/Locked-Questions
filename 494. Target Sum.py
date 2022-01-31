bb70966acdbbd8386011cfd592458ada76785746
Q2016 <k.sohraby@gmail.com>
2022-01-30 20:08:35 -0800

    Update 494. Target Sum.py

--- 494. Target Sum.py
+++ 494. Target Sum.py
@@ -1,25 +1,40 @@
-class Solution {
-public:
-    
-    int findTargetSumWays(vector<int>& nums, int target){
-         int index = nums.size() - 1;
-         int curr_sum = 0;
-         return dp(nums, target, index, curr_sum);
-    }
-    
-    int dp(vector<int>& nums, int target, int index, int curr_sum){
-        // Base Cases
-         if (index < 0 and curr_sum == target){
-             return 1;
-         }
-         if (index < 0){
-             return 0;
-         }
+Question:
+You are given an integer array nums and an integer target. You want to build an expression out of nums by adding one of 
+the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
 
-        // Decisions
-         int positive = dp(nums, target, index-1, curr_sum + nums[index]);
-         int negative = dp(nums, target, index-1, curr_sum + -nums[index]);
+Example 1:
+Input: nums = [1,1,1,1,1], target = 3, Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3    
 
-         return positive + negative;
-    }
-};
+
+Solution: Brute force- recursive
+    
+The brute force approach is based on recursion. We need to try to put both the + and - symbols at every location in the 
+given numsnums array and find out the assignments which lead to the required result S.
+For this, we make use of a recursive function calculate(nums, i, sum, S), which returns the assignments leading to the sum S, starting from the i^{th}
+index onwards, provided the sum of elements up to the i^{th} element is sumsum. This function appends a + sign and a - sign both 
+to the element at the current index and calls itself with the updated sumsum as sum + nums[i] and sum - nums[i] respectively along 
+with the updated current index as i+1. Whenever we reach the end of the array, we compare the sum obtained with S. If they are equal, 
+we increment the countcount value to be returned. Thus, the function call calculate(nums, 0, 0, S) returns the required number of assignments.
+
+
+    count = 0;
+    def findTargetSumWays(nums, S) :
+        calculate(nums, 0, 0, S)
+        return count
+     
+    def calculate(nums, i, sum, S) :
+        if (i == len(nums)) :
+            if (sum == S) :
+                count++
+        else :
+            calculate(nums, i + 1, sum + nums[i], S)
+            calculate(nums, i + 1, sum - nums[i], S)
+        
