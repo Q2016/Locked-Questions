156a52d614cdce7b85c3ebeef8dbd03ccc87aa7c
Q2016 <k.sohraby@gmail.com>
2022-01-31 12:27:38 -0800

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -1,4 +1,12 @@
-List of backtracking questions:
+Question:
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]    
+    
+Solution: Backtracking    
+List of backtracking questions in python (same list in 39. ):
 https://leetcode.com/problems/permutations/discuss/18284/Backtrack-Summary%3A-General-Solution-for-10-Questions!!!!!!!!-Python-(Combination-Sum-Subsets-Permutation-Palindrome)
 
     def permute(self, nums):
