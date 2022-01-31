8ffe0cd66f31368d935cdc88e68b5079857f1bff
Q2016 <k.sohraby@gmail.com>
2022-01-30 20:38:03 -0800

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,17 +1,14 @@
-# Time:  O(n * glogg), g is the max size of groups.
-# Space: O(n)
-#
-# Given an array of strings, return all groups of strings that are anagrams.
-#
-# Note: All inputs will be in lower-case.
-#
+Question:
+Given an array of strings strs, group the anagrams together. You can return the answer in any order. An Anagram is a word 
+or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
-class Solution(object):
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]    
+
+
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+
         anagrams_map, result = collections.defaultdict(list), []
         for s in strs:
             sorted_str = ("").join(sorted(s))
@@ -22,13 +19,5 @@ class Solution(object):
         return result
 
     
-my own solution:
-    
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-                
-        groups = defaultdict(list)
-        for s in strs:
-            key = "".join(sorted(s))
-            groups[key].append(s)
-        return groups.values()    
+# Time:  O(n * glogg), g is the max size of groups.
+# Space: O(n)
