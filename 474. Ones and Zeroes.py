7cbe874ab401b3f6038d3ac7d926d29729eab427
Q2016 <k.sohraby@gmail.com>
2022-01-31 12:04:13 -0800

    Update 474. Ones and Zeroes.py

--- 474. Ones and Zeroes.py
+++ 474. Ones and Zeroes.py
@@ -1,4 +1,16 @@
-Idea:
+Question:
+You are given an array of binary strings strs and two integers m and n. Return the size of the largest subset of strs 
+such that there are at most m 0's and n 1's in the subset. A set x is a subset of a set y if all elements of x are also 
+elements of y.
+
+Example 1:
+Input: strs = ["10","0001","111001","1","0"], m = 5, n = 3
+Output: 4
+Explanation: The largest subset with at most 5 0's and 3 1's is {"10", "0001", "1", "0"}, so the answer is 4.
+Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
+{"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3.    
+
+Solution: 2D Knapsack Problem
 This problem is a variation on the 0-1 Knapsack Problem with a wrinkle: each item has a 2-dimensional weight, 
 but a constant value. If we were to naively attempt every single permutation of up to 600 strings, that 
 would be 2^600 permutations.
@@ -18,11 +30,6 @@ if we were to update entries that will be the basis for later updates in the sam
 Once we reach the end, we return dp[M][N].
 
 
-
-
-
-
-class Solution:
     def findMaxForm(self, S: List[str], M: int, N: int) -> int:
         dp = [[0 for _ in range(N+1)] for _ in range(M+1)]
         for str in S:
