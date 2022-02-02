39784f6bc992b7756fea9b620537161e8a637e3c
Q2016 <k.sohraby@gmail.com>
2022-02-02 07:43:00 -0800

    Update 400. Nth Digit.py

--- 400. Nth Digit.py
+++ 400. Nth Digit.py
@@ -1,3 +1,14 @@
+Question:
+Given an integer n, return the nth digit of the infinite integer sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
+
+Example 1:
+Input: n = 11
+Output: 0, (we count digits not numbers)
+Explanation: The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+
+    
+Solution:    
+
 How many digits of size size can we have?
 
 1 * 9 (size 1, 1... 9)
