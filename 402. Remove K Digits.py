a5f385c73f2d870f830a3cdf2e335e4daf0fe05d
Q2016 <k.sohraby@gmail.com>
2022-02-02 08:20:01 -0800

    Update 402. Remove K Digits.py

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -1,38 +1,41 @@
-# Time:  O(n)
-# Space: O(n)
+Question:
+Given a non-negative integer num represented as a string,
+remove k digits from the number so that the new number is the smallest possible.
 
-# Given a non-negative integer num represented as a string,
-# remove k digits from the number so that the new number is the smallest possible.
-#
-# Note:
-# The length of num is less than 10^5 and will be >= k.
-# The given num does not contain any leading zero.
-# Example 1:
-#
-# Input: num = "1432219", k = 3
-# Output: "1219"
-# Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
-# Example 2:
-#
-# Input: num = "10200", k = 1
-# Output: "200"
-# Explanation: Remove the leading 1 and the number is 200. 
-# Note that the output must not contain leading zeroes.
-# Example 3:
-#
-# Input: num = "10", k = 2
-# Output: "0"
-# Explanation: Remove
+Example 1:
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
 
-Intuition: Using stack
 
-class Solution(object):
+Solution: Using stack
+The first algorithm is straight-forward. Let's think about the simplest case: how to remove 1 digit from the number so 
+that the new number is the smallest possible？ Well, one can simply scan from left to right, and remove the first "peak" 
+digit; the peak digit is larger than its right neighbor. One can repeat this procedure k times, and obtain the first algorithm:
+
+string removeKdigits(string num, int k) {
+        while (k > 0) {
+            int n = num.size();
+            int i = 0;
+            while (i+1<n && num[i]<=num[i+1])  i++;
+            num.erase(i, 1);
+            k--;
+        }
+        // trim leading zeros
+        int s = 0;
+        while (s<(int)num.size()-1 && num[s]=='0')  s++;
+        num.erase(0, s);
+        
+        return num=="" ? "0" : num;
+    }
+
+The above algorithm is a bit inefficient because it frequently remove a particular element from a string and has complexity O(k*n).
+One can simulate the above procedure by using a stack, and obtain a O(n) algorithm. Note, when the result stack (i.e. res) pop a digit, 
+it is equivalent as remove that "peak" digit.
+
+
     def removeKdigits(self, num, k):
-        """
-        :type num: str
-        :type k: int
-        :rtype: str
-        """
+
         result = []
         for d in num:
             while k and result and result[-1] > d:
@@ -40,3 +43,7 @@ class Solution(object):
                 k -= 1
             result.append(d)
         return ''.join(result).lstrip('0')[:-k or None] or '0'
+    
+    
+# Time:  O(n)
+# Space: O(n)    
