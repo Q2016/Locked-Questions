eb1d4175c6fd5bdb5090bf321adea409be52f024
Q2016 <k.sohraby@gmail.com>
2022-02-02 08:38:19 -0800

    Update 406. Queue Reconstruction by Height.py

--- 406. Queue Reconstruction by Height.py
+++ 406. Queue Reconstruction by Height.py
@@ -1,70 +1,58 @@
+Question:
+You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). 
+Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height 
+greater than or equal to hi. Reconstruct and return the queue that is represented by the input array people. The returned 
+queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue 
+(queue[0] is the person at the front of the queue).
 
-The question wants to arrange the input queue such that the number of people infront of a person matches with heights of everybody else:
- nput: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+Example 1:
+Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
 Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
- 
- Element in queue:
- #1 is placed at the begining so nobody has higher heigh than him
- #2 is placed there since nobody infront(hight 5) is taller
- #3 two people (hight 5 and 7) are infront of him
- #4 only (hight 7) is taller than him
- #5 4 people have hights higher
- #6 one person (hight 7) infront of him
+Explanation:
+Person 0 has height 5 with no other people taller or the same height in front.
+Person 1 has height 7 with no other people taller or the same height in front.
+Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue. 
 
 
-"""        
- Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+Solution: Sort
+
+Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, 
+therefore each guy's index will be just as same as his k value.
 For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
 E.g.
 input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 subarray after step 1: [[7,0], [7,1]]
-subarray after step 2: [[7,0], [6,1], [7,1]]       
-        
-def reconstructQueue(self, people):
-    people.sort(key=lambda (h, k): (-h, k))
-    queue = []
-    for p in people:
-        queue.insert(p[1], p)
-    return queue
-    
-"""
+subarray after step 2: [[7,0], [6,1], [7,1]]
+
+It's not the most concise code, but I think it well explained the concept.
 
 
-
-
-
-
-
-class Solution(object):
     def reconstructQueue(self, people):
-        """
-        :type people: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        m=len(people)
-        queue=[]
-        for p in people:
-            if p[1]==0:
-                queue.append(p)
-                people.remove(p)
-                
-        for p in people:
-            if p[1]==len(queue):
-                queue.append(p)
-                people.remove(p)
+        if not people: return []
+
+        # obtain everyone's info
+        # key=height, value=k-value, index in original array
+        peopledct, height, res = {}, [], []
         
-        for _ in range(len(people)):        
-            for p in people:
-                n=0
-                print(p)
-                for q in queue:
-                    if p[0]<=q[0]:
-                        n+=1
-                print(n)
-                print(len(queue))
-                
-                if n==len(queue):
-                    queue.append(p)
-                    people.remove(p)
-        
-        return queue
+        for i in xrange(len(people)):
+            p = people[i]
+            if p[0] in peopledct:
+                peopledct[p[0]] += (p[1], i),
+            else:
+                peopledct[p[0]] = [(p[1], i)]
+                height += p[0],
+
+        height.sort() # here are different heights we have
+
+        # sort from the tallest group
+        for h in height[::-1]:
+            peopledct[h].sort()
+            for p in peopledct[h]:
+                res.insert(p[0], people[p[1]])
+
+        return res
+  
