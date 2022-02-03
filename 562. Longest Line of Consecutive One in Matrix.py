a9188e97274fcd980a039ef353244410e067d874
Q2016 <k.sohraby@gmail.com>
2022-02-02 17:54:02 -0800

    Update 562. Longest Line of Consecutive One in Matrix.py

--- 562. Longest Line of Consecutive One in Matrix.py
+++ 562. Longest Line of Consecutive One in Matrix.py
@@ -1,43 +1,33 @@
+Question:
 Given a 01 matrix M, find the longest line of consecutive one in the matrix. The line could be horizontal, vertical, diagonal or anti-diagonal.
 
 Example:
-
-Input:
+Input:     , Output: 3
 [[0,1,1,0],
  [0,1,1,0],
  [0,0,0,1]]
-Output: 3
- 
 
-We can also consider using the DP solution, we build a three-dimensional DP array, 
-where DP [i] [j] [k] is used to traverse the number NUMS [i] [j] from the beginning, 
-and the row of rings 1 The number of K, the value of K is 0, 1, 2, 3,
-The four situations corresponding to level, vertical, diagonal, and reverse angle. 
-Then the process of updating the DP array, if the number is jumped directly, then the 
-horizontal direction adds the previous DP value, plus the top vertical direction
-A number of DP values, the diagonal direction plus the DP value of the number in the 
-upper right, and the reverse diagonal plus the DP value of the upper left number, then 
-each value is used to update the result RES, see the code as follows:
+Solution: DP
 
-  
-  
- class Solution2 {
-     int longestLine(int[][] M) {
-         if (M == null || M.length == 0 || M[0].length == 0) return 0;
-         int m = M.length, n = M[0].length, res = 0;
-         int[][][] dp = new int[m][n][4];
-         for (int i = 0; i < m; ++i) {
-             for (int j = 0; j < n; ++j) {
+We use DP solution, we build a three-dimensional DP array, where DP[i][j][k] is used to traverse 
+the matrix M[i][j]. Value of K is 0, 1, 2, 3. The four situations corresponding to level, vertical, diagonal, and reverse angle. 
+
+     def longestLine(M) :
+         if (M == None or M.size() == 0 or M[0].size() == 0) :
+             return 0
+         m = M.size() 
+         n = M[0].size() 
+         res = 0
+         #dp = initialize matrix of int[m][n][4]
+         for i in range(m) : 
+             for j in range(n) :
                  if (M[i][j] == 0) continue;
-                 for (int k = 0; k < 4; ++k) dp[i][j][k] = 1;
-                 if (j > 0) dp[i][j][0] += dp[i][j - 1][0]; // horizonal
-                 if (i > 0) dp[i][j][1] += dp[i - 1][j][1]; // vertical
-                 if (i > 0 && j < n - 1) dp[i][j][2] += dp[i - 1][j + 1][2]; // diagonal
-                 if (i > 0 && j > 0) dp[i][j][3] += dp[i - 1][j - 1][3]; // anti-diagonal
-                 res = Math.max(res, Math.max(dp[i][j][0], dp[i][j][1]));
-                 res = Math.max(res, Math.max(dp[i][j][2], dp[i][j][3]));
-             }
-         }
-         return res;
-     }
- }
+                 for k in range(4): dp[i][j][k] = 1
+                 if (j > 0): dp[i][j][0] += dp[i][j - 1][0] # horizonal
+                 if (i > 0) dp[i][j][1] += dp[i - 1][j][1]; # vertical
+                 if (i > 0 and j < n - 1) : dp[i][j][2] += dp[i - 1][j + 1][2] # diagonal
+                 if (i > 0 and j > 0) : dp[i][j][3] += dp[i - 1][j - 1][3] # anti-diagonal
+                 res = max(res, max(dp[i][j][0], dp[i][j][1]))
+                 res = max(res, max(dp[i][j][2], dp[i][j][3]))
+         return res
+     
