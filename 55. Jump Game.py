183c4908d602cc0838925e9f19d4a8cbb9222c69
Q2016 <k.sohraby@gmail.com>
2022-02-02 20:30:03 -0800

    Update 55. Jump Game.py

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -1,8 +1,20 @@
+Question:
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the 
+array represents your maximum jump length at that position. Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.    
+
+Solution:
 I just iterate and update the maximal index that I can reach
 
-bool canJump(int A[], int n) {
-    int i = 0;
-    for (int reach = 0; i < n && i <= reach; ++i)
-        reach = max(i + A[i], reach);
-    return i == n;
-}
+def canJump(A, n) :
+    i = 0
+    reach=0
+    while i < n and i <= reach:
+        reach = max(i + A[i], reach)
+        i+=1
+    return i == n
+
