4440733c84c911352ed92e08f5ba664615f97e08
Q2016 <k.sohraby@gmail.com>
2022-02-07 10:22:52 -0800

    Update 526. Beautiful Arrangement.py

--- 526. Beautiful Arrangement.py
+++ 526. Beautiful Arrangement.py
@@ -1,37 +1,34 @@
-Backtracking
+Question:
+Suppose you have n integers labeled 1 through n. A permutation of those n integers perm (1-indexed) is considered a beautiful arrangement 
+if for every i (1 <= i <= n), either of the following is true:
+perm[i] is divisible by i.
+i is divisible by perm[i].
+Given an integer n, return the number of the beautiful arrangements that you can construct.
 
-https://leetcode.com/problems/beautiful-arrangement/solution/
+Example 1:
+Input: n = 2, Output: 2
+Explanation: 
+The first beautiful arrangement is [1,2]: - perm[1] = 1 is divisible by i = 1     - perm[2] = 2 is divisible by i = 2
+The second beautiful arrangement is [2,1]: - perm[1] = 2 is divisible by i = 1    - i = 2 is divisible by perm[2] = 1    
 
-class Solution {
-public:
-    int count=0;
+
+Solution: Backtracking
+
+   int count=0;
     
-   int countArrangement(int N) {
-    
+   def countArrangement(int N): 
         if (N == 0) return 0;
-        vector<int> used;
-       used.resize(N+1);
+        used=(N+1)*[0];
         helper(N, 1, used);
         return count;
     
-    }
-    
-    
-        void helper(int N, int pos, vector<int> used) {
-        if (pos > N) {
-            count++;
-            return;
-        }
-        
-        for (int i = 1; i <= N; i++) {
-            if (used[i] == 0 && (i % pos == 0 || pos % i == 0)) {
+   def helper(N, pos, used):
+        if (pos > N):
+            count++
+            return
+        for (int i = 1; i <= N; i++):
+            if (used[i] == 0 && (i % pos == 0 || pos % i == 0)) :
                 used[i] = 1;
                 helper(N, pos + 1, used);
                 used[i] = 0;
-            }
-        }
-    }
 
-    
-    
-};
