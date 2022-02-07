88fa0fc3cd342a3a9500d619eaf181d5ae65d403
Q2016 <k.sohraby@gmail.com>
2022-02-07 08:00:08 -0800

    Update 540. Single Element in a Sorted Array.py

--- 540. Single Element in a Sorted Array.py
+++ 540. Single Element in a Sorted Array.py
@@ -1,15 +1,42 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
+Question:
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+Return the single element that appears only once. Your solution must run in O(log n) time and O(1) space.
 
-        int start=0, end = nums.size()-1, mid;
-        while( start < end ) {
-            mid = start + (end-start)/2;
-            if( nums[mid] == nums[mid ^ 1] )
-                start = mid + 1;
-            else
-                end = mid;
-        }
-        return nums[start];
-    }
-};
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+    
+Solution:
+Suppose array is [1, 1, 2, 2, 3, 3, 4, 5, 5]
+we can observe that for each pair, 
+first element takes even position and second element takes odd position
+for example, 1 is appeared as a pair,
+so it takes 0 and 1 positions. similarly for all the pairs also.
+
+this pattern will be missed when single element is appeared in the array.
+
+From these points, we can implement algorithm.
+1. Take left and right pointers . 
+    left points to start of list. right points to end of the list.
+2. find mid.
+    if mid is even, then it's duplicate should be in next index.
+	or if mid is odd, then it's duplicate  should be in previous index.
+	check these two conditions, 
+	if any of the conditions is satisfied,
+	then pattern is not missed, 
+	so check in next half of the array. i.e, left = mid + 1
+	if condition is not satisfied, then the pattern is missed.
+	so, single number must be before mid.
+	so, update end to mid.
+3. At last return the nums[left]
+
+
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = int((left + right)/2)
+            if (mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid%2 == 0 and nums[mid] == nums[mid + 1]):
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]    
