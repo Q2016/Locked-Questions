20e933e76a0cbfd9c3fb534b1328afd9d85c3115
Q2016 <k.sohraby@gmail.com>
2022-02-07 15:33:52 -0800

    Update 729. My Calendar I.py

--- 729. My Calendar I.py
+++ 729. My Calendar I.py
@@ -1,14 +1,69 @@
-/*overlap of 2 interval a b is (max(a0, b0), min(a1, b1))
-detail is in: https://discuss.leetcode.com/topic/111198
-*/
+Question:
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), 
+the range of real numbers x such that start <= x < end.
+Implement the MyCalendar class:
+MyCalendar() Initializes the calendar object.
+boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. 
+Otherwise, return false and do not add the event to the calendar.
+ 
+Example 1:
+Input: ["MyCalendar", "book", "book", "book"], [[], [10, 20], [15, 25], [20, 30]]
+Output: [null, true, false, true]
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.    
+    
+    
+Solution: BST
 
-class MyCalendar {
-    vector<pair<int, int>> books;
-public:    
-    bool book(int start, int end) {
-        for (pair<int, int> p : books)
-            if (max(p.first, start) < min(end, p.second)) return false;
-        books.push_back({start, end});
-        return true;
-    }
-};
+# Brute force O(n^2)                                                                                                                     
+class MyCalendar(object):
+    def __init__(self):
+        self.calendar = []
+
+    def book(self, start, end):
+        for s, e in self.calendar:
+            if s < end and start < e:
+                return False
+        self.calendar.append((start, end))
+        return True
+
+# Balanced Tree O(nlogn)       
+# If we maintained our events in sorted order, we could check whether an event could be booked in O(logN) time 
+# (where N is the number of events already booked) by binary searching for where the event should be placed. We would also 
+# have to insert the event in our sorted structure.                                        
+                                                                                                                      
+class Node:
+    __slots__ = 'start', 'end', 'left', 'right'
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = self.right = None
+
+    def insert(self, node):
+        if node.start >= self.end:
+            if not self.right:
+                self.right = node
+                return True
+            return self.right.insert(node)
+        elif node.end <= self.start:
+            if not self.left:
+                self.left = node
+                return True
+            return self.left.insert(node)
+        else:
+            return False
+
+class MyCalendar(object):
+    def __init__(self):
+        self.root = None
+
+    def book(self, start, end):
+        if self.root is None:
+            self.root = Node(start, end)
+            return True
+        return self.root.insert(Node(start, end))                                                                                                                      
+                                                                                                                      
