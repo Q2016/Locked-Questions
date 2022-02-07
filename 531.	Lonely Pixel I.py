9f5780b9c15f4e0e12b43417d9784e3834e0d7f1
Q2016 <k.sohraby@gmail.com>
2022-02-07 08:33:31 -0800

    Update 531.	Lonely Pixel I.py

--- 531.	Lonely Pixel I.py
+++ 531.	Lonely Pixel I.py
@@ -1,34 +1,23 @@
+Question:
 Given a picture consisting of black and white pixels, find the number of black lonely pixels.
-
 The picture is represented by a 2D char array consisting of 'B' and 'W', which means black and white pixels respectively.
-
 A black lonely pixel is character 'B' that located at a specific position where the same row and same column don't have any other black pixels.
 
 Example:
-
 Input: 
 [['W', 'W', 'B'],
  ['W', 'B', 'W'],
  ['B', 'W', 'W']]
-
 Output: 3
 Explanation: All the three 'B's are black lonely pixels.
   
   
-Two passes of the matrix.
-First pass: build the auxiliary table.
-Second pass: do the counting.  
-  
-  
-# Time:  O(m * n)
-# Space: O(m + n)
+Solution:  
+Two passes of the matrix. First pass: build the auxiliary table. Second pass: do the counting.  
 
-class Solution(object):
+
     def findLonelyPixel(self, picture):
-        """
-        :type picture: List[List[str]]
-        :rtype: int
-        """
+
         rows, cols = [0] * len(picture),  [0] * len(picture[0])
         for i in xrange(len(picture)):
             for j in xrange(len(picture[0])):
@@ -44,16 +33,9 @@ class Solution(object):
         return result
 
 
-class Solution2(object):
-    def findLonelyPixel(self, picture):
-        """
-        :type picture: List[List[str]]
-        :type N: int
-        :rtype: int
-        """
-        return sum(col.count('B') == 1 == picture[col.index('B')].count('B') \
-               for col in zip(*picture))
-      
+   
+# Time:  O(m * n)
+# Space: O(m + n)   
 
       
       
