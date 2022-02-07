7c6b9e1c98ca210343a667daeb3954853a1f381f
Q2016 <k.sohraby@gmail.com>
2022-02-07 09:58:14 -0800

    Update 528. Random Pick with Weight.py

--- 528. Random Pick with Weight.py
+++ 528. Random Pick with Weight.py
@@ -1,25 +1,43 @@
-# Time:  O(n)
-# Space: O(1)
+Question:
+You are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+You need to implement the function pickIndex(), which randomly picks an index in the range [0, w.length - 1] (inclusive) and returns it. 
+The probability of picking an index i is w[i] / sum(w).
+For example, if w = [1, 3], the probability of picking index 0 is 1 / (1 + 3) = 0.25 (i.e., 25%), and the probability of picking 
+index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).    
+    
+Example:    
+Input: ["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"] [[[1,3]],[],[],[],[],[]]
+Output: [null,1,1,1,1,0]
 
+Explanation
+Solution solution = new Solution([1, 3]);
+solution.pickIndex(); // return 1. It is returning the second element (index = 1) that has a probability of 3/4.
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 0. It is returning the first element (index = 0) that has a probability of 1/4.
 
+Since this is a randomization problem, multiple answers are allowed.
+All of the following outputs can be considered correct:
+[null,1,1,1,1,0]
+[null,1,1,1,1,1]
+[null,1,1,1,0,0]
+[null,1,1,1,0,1]
+[null,1,0,1,0,0]
+......
+and so on.    
+
+Solution:
 
 from random import randint
 
 class Solution(object):
 
     def __init__(self, nums):
-        """
-        
-        :type nums: List[int]
-        :type numsSize: int
-        """
+
         self.__nums = nums
 
     def pick(self, target):
-        """
-        :type target: int
-        :rtype: int
-        """
         reservoir = -1
         n = 0
         for i in xrange(len(self.__nums)):
@@ -30,6 +48,5 @@ class Solution(object):
         return reservoir
 
 
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.pick(target)
+# Time:  O(n)
+# Space: O(1)
