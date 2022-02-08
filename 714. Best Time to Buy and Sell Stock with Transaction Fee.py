50181b41dc5e4824ec5816b0a2a803258270028d
Q2016 <k.sohraby@gmail.com>
2022-02-07 17:35:54 -0800

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.py

--- 714. Best Time to Buy and Sell Stock with Transaction Fee.py
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -1,15 +1,31 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int sell[prices.size()];
-        int buy[prices.size()];
-        memset(sell,0,sizeof(sell));
-        memset(buy,0,sizeof(buy));
-        buy[0] = -prices[0];
-        for(int i=1; i<prices.size();i++) {
-            buy[i] = max(buy[i-1], sell[i-1] - prices[i]);
-            sell[i] = max(sell[i-1],prices[i] + buy[i-1] - fee);
-        }
-        return max(buy[prices.size()-1],sell[prices.size()-1]);
-    }
-};
+Question:
+You are given an array prices where prices[i] is the price of a given stock on the ith day, and an integer fee representing a transaction fee.
+Find the maximum profit you can achieve. You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+Input: prices = [1,3,2,8,4,9], fee = 2
+Output: 8
+Explanation: The maximum profit can be achieved by:
+- Buying at prices[0] = 1
+- Selling at prices[3] = 8
+- Buying at prices[4] = 4
+- Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
+
+Solution : DP
+At the end of the i-th day, we maintain cash, the maximum profit we could have if we did not have a share of stock, and hold, 
+the maximum profit we could have if we owned a share of stock. To transition from the i-th day to the i+1-th day, we either 
+sell our stock cash = max(cash, hold + prices[i] - fee) or buy a stock hold = max(hold, cash - prices[i]). At the end, we want 
+to return cash. We can transform cash first without using temporary variables because selling and buying on the same day can't 
+be better than just continuing to hold the stock.
+
+
+    def maxProfit(self, prices, fee):
+        cash, hold = 0, -prices[0]
+        for i in range(1, len(prices)):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+        return cash    
+
