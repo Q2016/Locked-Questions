b7ef696aca3565a4e71c9ff392f342885cba3c2d
Q2016 <k.sohraby@gmail.com>
2022-02-08 07:03:19 -0800

    Update 684. Redundant Connection.py

--- 684. Redundant Connection.py
+++ 684. Redundant Connection.py
@@ -1,11 +1,20 @@
-Approach #1: DFS [Accepted]
-Intuition and Algorithm
+Question:
+In this problem, a tree is an undirected graph that is connected and has no cycles. You are given a graph that started as a tree with n 
+nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an 
+edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an 
+edge between nodes ai and bi in the graph. Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there 
+are multiple answers, return the answer that occurs last in the input.
+1----2
+|  /
+3
+Example 1:
+Input: edges = [[1,2],[1,3],[2,3]]
+Output: [2,3]    
 
-For each edge (u, v), traverse the graph with a depth-first search to see if we can 
-connect u to v. If we can, then it must be the duplicate edge.
 
+Solution: DFS
+For each edge (u, v), traverse the graph with a depth-first search to see if we can connect u to v. If we can, then it must be the duplicate edge.
 
-class Solution(object):
     def findRedundantConnection(self, edges):
         graph = collections.defaultdict(set)
 
@@ -25,8 +34,6 @@ class Solution(object):
             
 
 Complexity Analysis
-
 Time Complexity: O(N^2) where N is the number of vertices (and also the number of edges) in the graph. 
 In the worst case, for every edge we include, we have to search every previously-occurring edge of the graph.
-
 Space Complexity: O(N). The current construction of the graph has at most N nodes.
