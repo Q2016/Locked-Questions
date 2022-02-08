9572c8a6b1a78e1f64a888d58e4616062cac29a1
Q2016 <k.sohraby@gmail.com>
2022-02-08 13:33:12 -0800

    Update 6. Zigzag Conversion.py

--- 6. Zigzag Conversion.py
+++ 6. Zigzag Conversion.py
@@ -1,10 +1,19 @@
-class Solution(object):
+Question:
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR". Write the code that will take a string and make this conversion given a number of rows.
+ 
+
+Example 1:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+    
+    
+Solution:
+    
     def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
         if numRows == 1:
             return s
         step, zigzag = 2 * numRows - 2, ""
