0f228b6ec41c199bf9f5300277ba7b0f53dba26d
Q2016 <k.sohraby@gmail.com>
2022-02-08 10:48:06 -0800

    Update 640. Solve the Equation.py

--- 640. Solve the Equation.py
+++ 640. Solve the Equation.py
@@ -1,9 +1,14 @@
-class Solution(object):
+Question:
+Solve a given equation and return the value of 'x' in the form of a string "x=#value". The equation contains only '+', '-' operation, 
+the variable 'x' and its coefficient. You should return "No solution" if there is no solution for the equation, or "Infinite solutions" 
+if there are infinite solutions for the equation. If there is exactly one solution for the equation, we ensure that the value of 'x' is an integer.
+
+Example 1:
+Input: equation = "x+5-3+x=6+x-2", Output: "x=2"
+    
+Solution: Regix
+    
     def solveEquation(self, equation):
-        """
-        :type equation: str
-        :rtype: str
-        """
         a, b, side = 0, 0, 1
         for eq, sign, num, isx in re.findall('(=)|([-+]?)(\d*)(x?)', equation):
             if eq:
