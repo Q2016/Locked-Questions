336b89c4c9e20634d69feb062f526ab00578b3d3
Q2016 <k.sohraby@gmail.com>
2022-02-07 17:49:45 -0800

    Update 708. Insert into a Sorted Circular Linked List.py

--- 708. Insert into a Sorted Circular Linked List.py
+++ 708. Insert into a Sorted Circular Linked List.py
@@ -1,26 +1,16 @@
-https://ttzztt.gitbooks.io/lc/content/linked-list/insert-into-a-cyclic-sorted-list.html
+Question:
+Given a node from a cyclic linked list which is sorted in ascending order, write a function to insert a value into the list such that 
+it remains a cyclic sorted list. The given node can be a reference to_any_single node in the list, and may not be necessarily the 
+smallest value in the cyclic list. If there are multiple suitable places for insertion, you may choose any place to insert the new value. 
+After the insertion, the cyclic list should remain sorted.
 
-Given a node from a cyclic linked list which is sorted in ascending order, write a function to insert a value into the list such that it remains a cyclic sorted list. The given node can be a reference to_any_single node in the list, and may not be necessarily the smallest value in the cyclic list.
 
-If there are multiple suitable places for insertion, you may choose any place to insert the new value. After the insertion, the cyclic list should remain sorted.
-
-If the list is empty (i.e., given node isnull), you should create a new single cyclic list and return the reference to that single node. Otherwise, you should return the original given node.
-
-Thoughts:
-
-No head:
-prev.val <= val <= cur.val
+Solution:
+No head: prev.val <= val <= cur.val
 prev.val > cur.val and (val < cur.val or prev.val < cur): cur is either the min or the max with not all nodes with the same value
 val != every nodes's value in a cyclic linked list where every node has the same value
 
-
-class Solution(object):
     def insert(self, head, val):
-        """
-        :type head: Node
-        :type insertVal: int
-        :rtype: Node
-        """
         node = Node(val, head)
         # case 1 no head
         if not head:
