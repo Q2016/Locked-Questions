c9f1e91939112f100384daed4cd6b7b0bd2ea556
Q2016 <k.sohraby@gmail.com>
2022-02-07 21:42:01 -0800

    Update 688. Knight Probability in Chessboard.py

--- 688. Knight Probability in Chessboard.py
+++ 688. Knight Probability in Chessboard.py
@@ -1,20 +1,29 @@
-Approach #1: Dynamic Programming [Accepted]
-Intuition and Algorithm
+Question:
+On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, 
+so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1). A chess knight has eight possible moves it can make, as illustrated 
+below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction. Each time the knight is to move, it chooses one 
+of eight possible moves uniformly at random (even if the piece would go off the chessboard) and moves there. The knight continues moving until 
+it has made exactly k moves or has moved off the chessboard. Return the probability that the knight remains on the board after it has stopped moving.
 
-Let f[r][c][steps] be the probability of being on square (r, c) after steps steps. 
-Based on how a knight moves, we have the following recursion:
+Example 1:
+Input: n = 3, k = 2, row = 0, column = 0
+Output: 0.06250
+Explanation: There are two moves (to (1,2), (2,1)) that will keep the knight on the board.
+From each of those positions, there are also two moves that will keep the knight on the board.
+The total probability the knight stays on the board is 0.0625.    
 
-f[r][c][steps] = \sum_{dr, dc} f[r+dr][c+dc][steps-1] / 8.0f[r][c][steps]=\sum_{dr, dc} f[r+dr][c+dc][steps−1]/8.0
 
+Solution: Dynamic Programming
+
+Let f[r][c][steps] be the probability of being on square (r, c) after steps steps. Based on how a knight moves, we have the following recursion:
+f[r][c][steps] = \sum_{dr, dc} f[r+dr][c+dc][steps-1] / 8.0 f[r][c][steps]=\sum_{dr, dc} f[r+dr][c+dc][steps−1]/8.0
 where the sum is taken over the eight (dr, dc)(dr,dc) pairs (2, 1),(2,1), (2, -1),(2,−1), 
 (-2, 1),(−2,1), (-2, -1),(−2,−1), (1, 2),(1,2), (1, -2),(1,−2), (-1, 2),(−1,2), (-1, -2)(−1,−2).
-
 Instead of using a three-dimensional array f, we will use two two-dimensional ones dp and dp2, storing 
 the result of the two most recent layers we are working on. dp2 will represent f[][][steps], and dp 
 will represent f[][][steps-1].
 
 
-class Solution(object):
     def knightProbability(self, N, K, r, c):
         dp = [[0] * N for _ in xrange(N)]
         dp[r][c] = 1
@@ -22,8 +31,7 @@ class Solution(object):
             dp2 = [[0] * N for _ in xrange(N)]
             for r, row in enumerate(dp):
                 for c, val in enumerate(row):
-                    for dr, dc in ((2,1),(2,-1),(-2,1),(-2,-1),
-                                   (1,2),(1,-2),(-1,2),(-1,-2)):
+                    for dr, dc in ((2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)):
                         if 0 <= r + dr < N and 0 <= c + dc < N:
                             dp2[r+dr][c+dc] += val / 8.0
             dp = dp2
