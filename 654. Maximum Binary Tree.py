0396bd293c1b67861fa4134387d65daf251b7cc9
Q2016 <k.sohraby@gmail.com>
2022-02-08 10:12:50 -0800

    Update 654. Maximum Binary Tree.py

--- 654. Maximum Binary Tree.py
+++ 654. Maximum Binary Tree.py
@@ -1,25 +1,38 @@
+Question:
+You are given an integer array nums with no duplicates. A maximum binary tree can be built recursively from nums using the following algorithm:
+Create a root node whose value is the maximum value in nums. Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+Recursively build the right subtree on the subarray suffix to the right of the maximum value. Return the maximum binary tree built from nums.
 
+Example 1:
+Input: nums = [3,2,1,6,0,5]
+Output: [6,3,5,null,2,0,null,null,1]
+Explanation: The recursive calls are as follow:
+- The largest value in [3,2,1,6,0,5] is 6. Left prefix is [3,2,1] and right suffix is [0,5].
+    - The largest value in [3,2,1] is 3. Left prefix is [] and right suffix is [2,1].
+        - Empty array, so no child.
+        - The largest value in [2,1] is 2. Left prefix is [] and right suffix is [1].
+            - Empty array, so no child.
+            - Only one element, so child is a node with value 1.
+    - The largest value in [0,5] is 5. Left prefix is [0] and right suffix is [].
+        - Only one element, so child is a node with value 0.
+        - Empty array, so no child.
+         
+Solution: Devide and Conquer
+   
 The idea is to use divide and conquer. Each time we create a node root for the maximum value 
 in the range. Then, we split it into a left range and a right range, which are the left subtree 
 and right subtree of this maximum node root.
 
-
-
-   TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return helper(nums, 0, nums.size() - 1);
-    }
-    
-    //max_index denotes the index of the maximum number in range [left, right]
-    TreeNode* helper(vector<int>& nums, int left, int right){
-        if(left>right)return NULL;
-        
-        int max_index = left;
-        for(int i = left; i<=right; i++){
-            if(nums[i] > nums[max_index])max_index = i; 
-        }
-        
-        TreeNode* root = new TreeNode(nums[max_index]);
-        root->left = helper(nums, left, max_index - 1);
-        root->right = helper(nums, max_index + 1, right);
-        return root;
-    }
+    def constructMaximumBinaryTree(self, nums):
+        if not nums:
+            return
+        if len(nums) == 1:
+            return TreeNode(nums[0])
+        pivot = max(nums)
+        idx = nums.index(pivot)
+        ret = TreeNode(pivot)
+        ret.left = self.constructMaximumBinaryTree(nums[:idx])
+        ret.right = self.constructMaximumBinaryTree(nums[idx+1:])
+        return ret
+      
+Complexity: nlog n      
