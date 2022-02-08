fffa3f11818068d1173f0b8a9031631a663d82f4
Q2016 <k.sohraby@gmail.com>
2022-02-08 13:29:20 -0800

    Update 616. Add Bold Tag in String.py

--- 616. Add Bold Tag in String.py
+++ 616. Add Bold Tag in String.py
@@ -1,7 +1,7 @@
-Given a string s and a list of strings dict, you need to add a closed 
-pair of bold tag <b>and </b> to wrap the substrings in s that exist in dict. 
-If two such substrings overlap, you need to wrap them together by only one pair 
-of closed bold tag. Also, if two substrings wrapped by bold tags are consecutive, you need to combine them.
+Question:
+Given a string s and a list of strings dict, you need to add a closed pair of bold tag <b>and </b> to wrap the substrings in s that exist in dict. 
+If two such substrings overlap, you need to wrap them together by only one pair of closed bold tag. Also, if two substrings wrapped by bold tags 
+are consecutive, you need to combine them.
 
 Example 1:
 Input: 
@@ -18,25 +18,10 @@ Output:
 
 
 
-
-https://shareablecode.com/snippets/add-bold-tag-in-string-python-solution-leetcode-KdxM-WmgU
-
-
-# Time:  O(n * d * l), l is the average string length
-# Space: O(n)
-
-import collections
-import functools
-
-
-# 59ms
-class Solution(object):
+Solution:
+    
     def addBoldTag(self, s, dict):
-        """
-        :type s: str
-        :type dict: List[str]
-        :rtype: str
-        """
+
         lookup = [0] * len(s)
         for d in dict:
             pos = s.find(d)
@@ -54,39 +39,5 @@ class Solution(object):
         return "".join(result)
 
 
-# Time:  O(n * l), l is the average string length
-# Space: O(t)    , t is the size of trie
-# trie solution, 439ms
-class Solution2(object):
-    def addBoldTag(self, s, words):
-        """
-        :type s: str
-        :type words: List[str]
-        :rtype: str
-        """
-        _trie = lambda: collections.defaultdict(_trie)
-        trie = _trie()
-        for i, word in enumerate(words):
-            functools.reduce(dict.__getitem__, word, trie).setdefault("_end")
-
-        lookup = [False] * len(s)
-        for i in xrange(len(s)):
-            curr = trie
-            k = -1
-            for j in xrange(i, len(s)):
-                if s[j] not in curr:
-                    break
-                curr = curr[s[j]]
-                if "_end" in curr:
-                    k = j
-            for j in xrange(i, k+1):
-                lookup[j] = True
-
-        result = []
-        for i in xrange(len(s)):
-            if lookup[i] and (i == 0 or not lookup[i-1]):
-                result.append("<b>")
-            result.append(s[i])
-            if lookup[i] and (i == len(s)-1 or not lookup[i+1]):
-                result.append("</b>")
-        return "".join(result)
+# Time:  O(n * d * l), l is the average string length
+# Space: O(n)
