d6028641d33fcea933503488449920521b9cb98b
Q2016 <k.sohraby@gmail.com>
2022-02-08 17:27:25 -0800

    Update 583. Delete Operation for Two Strings.py

--- 583. Delete Operation for Two Strings.py
+++ 583. Delete Operation for Two Strings.py
@@ -1,26 +1,30 @@
-class Solution {
-public:
+Question:
+Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+In one step, you can delete exactly one character in either string.
 
-    int minDistance(string word1, string word2) {
-        int m = word1.length(),n = word2.length();
-        int dp[m+1][n+1];    
-        for(int i=0;i<=m;i++)    //LCS for "XXXXXX" and "" will be 0
+Example 1:
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".    
+
+    
+    
+Solution:
+Find the longest common subsequence (LCS). Number of deletions will be : (word1.length - LCS) + (word2.length - LCS)
+
+    def minDistance(word1, word2):
+        m = len(word1)
+        n = len(word2)
+        #dp[m+1][n+1];    
+        for i in range(m+1):    # LCS for "XXXXXX" and "" will be 0
             dp[i][0] = 0;
-        for(int j=0;j<=n;j++)   //LCS for "" and "XXXXX" will be 0
+        for j in range(n+1):    # LCS for "" and "XXXXX" will be 0
             dp[0][j] = 0;
-        for(int i=1;i<=m;i++)
-            for(int j=1;j<=n;j++){
-              if(word1[i-1] == word2[j-1])        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if (word1[i-1] == word2[j-1]):        
                   dp[i][j] = dp[i-1][j-1] + 1;
-              else 
+              else: 
                   dp[i][j] = max(dp[i-1][j],dp[i][j-1]);  
-            }
         return m - dp[m][n] + n - dp[m][n];
-    }
-};
-
-/*Algorithm:
-
-Find the longest common subsequence (LCS)
-no of deletions will be : (word1.length - LCS) + (word2.length - LCS)
-*/
+    
