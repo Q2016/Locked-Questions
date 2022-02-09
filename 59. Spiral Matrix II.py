0c9ffa0fc2cdd0586782fe5f6314982e51632541
Q2016 <k.sohraby@gmail.com>
2022-02-08 17:14:25 -0800

    Update 59. Spiral Matrix II.py

--- 59. Spiral Matrix II.py
+++ 59. Spiral Matrix II.py
@@ -1,30 +1,28 @@
-class Solution {
-public:
+Question:
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
 
-    vector<vector<int>> generateMatrix(int n) {
+Example 1:
+Input: n = 3
+Output: [[1,2,3],
+         [8,9,4],
+         [7,6,5]]    
 
-        vector<vector<int>> result (n, vector<int>(n));
-        int cnt = 1;
-        
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            // direction 1 - traverse from left to right
-            for (int ptr = layer; ptr < n - layer; ptr++) {
+Solution:
+    def generateMatrix(n):
+        #result [n, n]
+        cnt = 1;
+        for (int layer = 0; layer < (n + 1) / 2; layer++):
+            # direction 1 - traverse from left to right
+            for (int ptr = layer; ptr < n - layer; ptr++):
                 result[layer][ptr] = cnt++;
-            }
-            // direction 2 - traverse from top to bottom
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
+            # direction 2 - traverse from top to bottom
+            for (int ptr = layer + 1; ptr < n - layer; ptr++):
                 result[ptr][n - layer - 1] = cnt++;
-            }
-            // direction 3 - traverse from right to left
-            for (int ptr = n - layer - 2; ptr >= layer; ptr--) {
+            # direction 3 - traverse from right to left
+            for (int ptr = n - layer - 2; ptr >= layer; ptr--):
                 result[n - layer - 1][ptr] = cnt++;
-            }
-            // direction 4 - traverse from bottom to top
-            for (int ptr = n - layer - 2; ptr > layer; ptr--) {
+            # direction 4 - traverse from bottom to top
+            for (int ptr = n - layer - 2; ptr > layer; ptr--):
                 result[ptr][layer] = cnt++;
-            }
-        }
-
         return result;
-    }
-};
+    
