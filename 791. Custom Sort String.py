0a052c5143f0a814d4575aabb75e2ba8de8235f9
Q2016 <k.sohraby@gmail.com>
2022-02-14 10:06:10 -0800

    Update 791. Custom Sort String.py

--- 791. Custom Sort String.py
+++ 791. Custom Sort String.py
@@ -1,12 +1,22 @@
-class Solution {
-  public:
-    string customSortString(string S, string T) {
-        unordered_map<char, int> dir;
-        for (int i = 0; i < S.size(); i++) {
-            dir[S[i]] = i + 1;
-        }
-        sort(T.begin(), T.end(),
-             [&](char a, char b) { return dir[a] < dir[b]; });
-        return T;
-    }
-};
+Question:
+You are given two strings order and s. All the words of order are unique and were sorted in some custom order previously. Permute the characters of s 
+so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur 
+before y in the permuted string. Return any permutation of s that satisfies this property.
+
+Example 1:
+Input: order = "cba", s = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.  
+
+Solution:
+Similar to 1122. Relative Sort Array
+
+    def customSortString(self, S: str, T: str) -> str:
+        ans, cnt = [], collections.Counter(T)           # count each char in T. 
+        for c in S:
+            if cnt[c]: ans.extend(c * cnt.pop(c))       # sort chars both in T and S by the order of S.
+        for c, v in cnt.items():
+            ans.extend(c * v)                           # group chars in T but not in S.
+        return ''.join(ans);
