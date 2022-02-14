e842a2495511ec90d1621803073d5bed2eeb83a5
Q2016 <k.sohraby@gmail.com>
2022-02-13 19:39:25 -0800

    Update 814. Binary Tree Pruning.py

--- 814. Binary Tree Pruning.py
+++ 814. Binary Tree Pruning.py
@@ -1,26 +1,43 @@
-//Postorder
+Question:
+Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+A subtree of a node node is node plus every node that is a descendant of node.
 
+Example 1:
+Input: root = [1,null,0,0,1]
+Output: [1,null,0,null,1]
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.    
+    
+Solution: Recursion [Accepted]
+Prune children of the tree recursively. The only decisions at each node are whether to prune the left child or the right child.
+We'll use a function containsOne(node) that tells us whether the subtree at this node contains a 1, and prunes all subtrees that do not contain 1.
+If for example, node.left subtree does not contain a one, then we should prune it via node.left = null.
+Also, the parent needs to be checked. If for example the tree is a single node 0, the answer is an empty tree.    
 
-class Solution {
-    public TreeNode pruneTree(TreeNode root) {
-        return containsOne(root) ? root : null;
-    }
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        
+        def contains_one(node: TreeNode) -> bool:
+            if not node: 
+                return False
+            
+            # Check if any node in the left subtree contains a 1.
+            left_contains_one = contains_one(node.left)
+            
+            # Check if any node in the right subtree contains a 1.
+            right_contains_one = contains_one(node.right)
+            
+            # If the left subtree does not contain a 1, prune the subtree.
+            if not left_contains_one: 
+                node.left = None
+                
+            # If the right subtree does not contain a 1, prune the subtree.
+            if not right_contains_one: 
+                node.right = None
+            
+            # Return True if the current node or its left or right subtree contains a 1.
+            return node.val or left_contains_one or right_contains_one
 
-    public boolean containsOne(TreeNode node) {
-        if (node == null) return false;
-        
-        // Check if any node in the left subtree contains a 1.
-        boolean leftContainsOne = containsOne(node.left);
-        
-        // Check if any node in the right subtree contains a 1.
-        boolean rightContainsOne = containsOne(node.right);
-
-        // If the left subtree does not contain a 1, prune the subtree.
-        if (!leftContainsOne) node.left = null;
-        
-        // If the right subtree does not contain a 1, prune the subtree.
-        if (!rightContainsOne) node.right = null;
-        
-        // Return true if the current node, its left or right subtree contains a 1.
-        return node.val == 1 || leftContainsOne || rightContainsOne;
-    }
+        # Return the pruned tree if the tree contains a 1, otherwise return None.
+        return root if contains_one(root) else None
