649954eeac1b3c240e4ab7c63bf472cfa6403d0f
Q2016 <k.sohraby@gmail.com>
2022-02-14 09:46:13 -0800

    Update 794. Valid Tic-Tac-Toe State.py

--- 794. Valid Tic-Tac-Toe State.py
+++ 794. Valid Tic-Tac-Toe State.py
@@ -1,5 +1,18 @@
-To find the validity of a given board, we could first think about the cases where the board is invalid
+Question:
+Given a Tic-Tac-Toe board as a string array board, return true if and only if it is possible to reach this board position during the 
+course of a valid tic-tac-toe game.
+The board is a 3 x 3 array that consists of characters ' ', 'X', and 'O'. The ' ' character represents an empty square.
+Here are the rules of Tic-Tac-Toe:
+Players take turns placing characters into empty squares ' '.
+The first player always places 'X' characters, while the second player always places 'O' characters.
+'X' and 'O' characters are always placed into empty squares, never filled ones.
+The game ends when there are three of the same (non-empty) character filling any row, column, or diagonal.
+The game also ends if all squares are non-empty.
+No more moves can be played if the game is over.
 
+
+Solution:
+To find the validity of a given board, we could first think about the cases where the board is invalid
 Since X starts first, x_count >= o_count. So if o_count > x_count, we can return False
 Since the players take turns, we could also return False if x_count-o_count>1
 After the corner cases, this is the algorithm used:
