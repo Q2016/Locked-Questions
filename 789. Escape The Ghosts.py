28813453922dd735ef5944a8fd303a83dd5a5d7e
Q2016 <k.sohraby@gmail.com>
2022-02-14 11:20:17 -0800

    Update 789. Escape The Ghosts.py

--- 789. Escape The Ghosts.py
+++ 789. Escape The Ghosts.py
@@ -1,3 +1,19 @@
+Question:
+You are playing a simplified PAC-MAN game on an infinite 2-D grid. You start at the point [0, 0], and you are given a destination point 
+target = [xtarget, ytarget] that you are trying to get to. There are several ghosts on the map with their starting positions given as a 2D 
+array ghosts, where ghosts[i] = [xi, yi] represents the starting position of the ith ghost. All inputs are integral coordinates.
+Each turn, you and all the ghosts may independently choose to either move 1 unit in any of the four cardinal directions: north, east, south, or west, 
+or stay still. All actions happen simultaneously. You escape if and only if you can reach the target before any ghost reaches you. If you reach any 
+square (including the target) at the same time as a ghost, it does not count as an escape.
+Return true if it is possible to escape regardless of how the ghosts move, otherwise return false.
+
+Example 1:
+Input: ghosts = [[1,0],[0,3]], target = [0,1]
+Output: true
+Explanation: You can reach the destination (0, 1) after 1 turn, while the ghosts located at (1, 0) and (0, 3) cannot catch up with you.    
+    
+    
+Solution:
 The best tactic for any ghost is to reach the target before pacman and block the exit.
 
 Note that we do not require that any ghost reaches pacman (which will never happen on an infinite grid for a single ghost and be much harder to determine for multiple ghosts).
