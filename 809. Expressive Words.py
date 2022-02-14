8bea335e805ed9ee79249d743f53c24504aefc0e
Q2016 <k.sohraby@gmail.com>
2022-02-13 19:46:18 -0800

    Update 809. Expressive Words.py

--- 809. Expressive Words.py
+++ 809. Expressive Words.py
@@ -1,107 +1,88 @@
-My solution:
+Question:
+Sometimes people repeat letters to represent extra feeling. For example:
+"hello" -> "heeellooo"
+"hi" -> "hiiii"
+In these strings like "heeellooo", we have groups of adjacent letters that are all the same: "h", "eee", "ll", "ooo".
+You are given a string s and an array of query strings words. A query word is stretchy if it can be made to be equal to s by any number 
+of applications of the following extension operation: choose a group consisting of characters c, and add some number of characters c to the 
+    group so that the size of the group is three or more.
+For example, starting with "hello", we could do an extension on the group "o" to get "hellooo", but we cannot get "helloo" since the group 
+"oo" has a size less than three. Also, we could do another extension like "ll" -> "lllll" to get "helllllooo". If s = "helllllooo", then the query 
+word "hello" would be stretchy because of these two extension operations: query = "hello" -> "hellooo" -> "helllllooo" = s.
+Return the number of query strings that are stretchy.
 
-def expressiveWords(s, words):
-    
-    
-    l2=len(s)
-    n=0
-    for word in words:
-        
-        
-        start1=0
-        start2=0
-        end2=0
-        flag=True
-        
-        l1=len(word)
-        
-        while start1<l1 and end2 <l2:
-            # assuming the first char is equal
-            if start2+1<l2 and start1+1<l1:
-                if s[start2]==word[start1] and s[start2+1]==word[start1+1]:
-                    start1+=1
-                    start2+=1
-                    end2=start2
+Example 1:
+Input: s = "heeellooo", words = ["hello", "hi", "helo"]
+Output: 1
+Explanation: 
+We can extend "e" and "o" in the word "hello" to get "heeellooo".
+We can't extend "helo" to get "heeellooo" because the group "ll" is not size 3 or more.
+
+
+Solution:
+To make the code more readable, a possible way is to define helper functions.
+
+compress(word)
+A word can be compressed to be a letter sequence and a corresponding counts.
+For example, 'abbaaa' will be compressed to be ['a', 'b', 'a'] and [1, 2, 3].
+A helper function compress(word) is defined to do it.
+
+is_strechy(word)
+Before checking if a word is strechy, we compress both the word and S.
+The word will be compressed to be w_letters and w_counts, and S will be S_letters and S_counts.
+check letters sequence
+It is obvious that if w_letters and S_letters are not identical, the word is not stretchy.
+check counts sequence
+For a index i,
+
+S_counts[i] < 3 means the letter is not expended, so the count of this letter should be identical in S and in the word. In other word, 
+if w_letters[i] != S_letters[i], the word is not strechy.
+S_counts[i] >= 3 means either (i) the letter is expended to be larger than 3, or (ii) not streched with an original count larger than 3. 
+Whicever case it is, if S_counts[i] < w_counts[i], the word is not stretchy.
+If none of above happens, the word is stretchy.
+
+
+class Solution:
+    def expressiveWords(self, S: str, words: List[str]) -> int:
+
+        # compress a word into a letter sequence and corresponding counts
+        def compress(word):
+            letters = []
+            counts = []
+            for l in word:
+                if not letters or l != letters[-1]:
+                    letters.append(l)
+                    counts.append(1)
                 else:
-                    print(word)
-                    print(start1,end2)
-                    while s[end2+1] != word[start1+1]:
-                        end2+=1
-                    cnt=end2-start2
-                    if 0<cnt<2:
-                        flag=False
-                        break   
-                    start2=end2
-                
-                
-        if flag==True:
-            n+=1
+                    counts[-1] += 1
+                    
+            return letters, counts
+        
+        # check if a word is stretchy by comparing to compressed S
+        def is_stretchy(w):
             
-    return n
+            # S_letters, S_counts are the output of compress(S)
+            nonlocal S_letters, S_counts
+            
+            w_letters, w_counts = compress(w)
+            
+            if w_letters != S_letters:
+                return False
+            
+            for i in range(len(S_counts)):
+                if S_counts[i] < 3 and S_counts[i] != w_counts[i]:
+                    return False
+                if S_counts[i] >= 3 and S_counts[i] < w_counts[i]:
+                    return False
+                    
+            return True
 
-s="heeellooo"
-words=["hello", "hi", "helo"]
-print(expressiveWords(s, words))
+        # main body
+        S_letters, S_counts = compress(S)
+        res = 0
+        for w in words:
+            if is_stretchy(w):
+                res += 1
+        
+        return res    
 
-
-
-
-
-
-
-
-
-
-https://leetcode.com/problems/expressive-words/discuss/121706/Java-Solution-using-Two-Pointers-with-Detailed-Explanation
-
-We have two pointers, use i to scan S, and use j to scan each word in words.
-Firstly, for S and word, we can calculate the length of the susbtrings which contains the repeated letters of the letter currently pointed by the two pointers, and get len1 and len2.
-
-The two letters currently pointed by the two pointers must be equal, otherwise the word is not stretchy, we return false. Then, if we find that len1 is smaller than 3, it means the letter cannot be extended, so len1 must equals to len2, otherwise this word is not stretchy. In the other case, if len1 equals to or larger than 3, we must have len1 equals to or larger than len2, otherwise there are not enough letters in S to match the letters in word.
-
-Finally, if the word is stretchy, we need to guarantee that both of the two pointers has scanned the whole string.
-
-class Solution {
-    public int expressiveWords(String S, String[] words) {
-        if (S == null || words == null) {
-            return 0;
-        }
-        int count = 0;
-        for (String word : words) {
-            if (stretchy(S, word)) {
-                count++;
-            }
-        }
-        return count;
-    }
-    
-    public boolean stretchy(String S, String word) {
-        if (word == null) {
-            return false;
-        }
-        int i = 0;
-        int j = 0;
-        while (i < S.length() && j < word.length()) {
-            if (S.charAt(i) == word.charAt(j)) {
-                int len1 = getRepeatedLength(S, i);
-                int len2 = getRepeatedLength(word, j);
-                if (len1 < 3 && len1 != len2 || len1 >= 3 && len1 < len2) {
-                    return false;
-                }
-                i += len1;
-                j += len2;
-            } else {
-                return false;
-            }
-        }
-        return i == S.length() && j == word.length();
-    }
-    
-    public int getRepeatedLength(String str, int i) {
-        int j = i;
-        while (j < str.length() && str.charAt(j) == str.charAt(i)) {
-            j++;
-        }
-        return j - i;
-    }
-}
