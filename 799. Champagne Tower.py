801ab8f73f09fdc8bb2ea3051d5276e1592a63de
Q2016 <k.sohraby@gmail.com>
2022-02-14 09:36:58 -0800

    Update 799. Champagne Tower.py

--- 799. Champagne Tower.py
+++ 799. Champagne Tower.py
@@ -1,12 +1,25 @@
-Approach #1: Simulation [Accepted]
-Intuition
+Question:
+We stack glasses in a pyramid, where the first row has 1 glass, the second row has 2 glasses, and so on until the 100th row.  Each glass holds 
+one cup of champagne. Then, some champagne is poured into the first glass at the top.  When the topmost glass is full, any excess liquid poured 
+will fall equally to the glass immediately to the left and right of it.  When those glasses become full, any excess champagne will fall equally 
+to the left and right of those glasses, and so on.  (A glass at the bottom row has its excess champagne fall on the floor.)
+For example, after one cup of champagne is poured, the top most glass is full.  After two cups of champagne are poured, the two glasses on the 
+second row are half full.  After three cups of champagne are poured, those two cups become full - there are 3 full glasses total now.  After four 
+cups of champagne are poured, the third row has the middle glass half full, and the two outside glasses are a quarter full, as pictured below.
+Now after pouring some non-negative integer cups of champagne, return how full the jth glass in the ith row is (both i and j are 0-indexed.)
 
+Example 1:
+Input: poured = 2, query_row = 1, query_glass = 1
+Output: 0.50000
+Explanation: We poured 2 cups of champange to the top glass of the tower (which is indexed as (0, 0)). There is one cup of excess liquid. 
+The glass indexed as (1, 0) and the glass indexed as (1, 1) will share the excess liquid equally, and each will get half cup of champange.    
+
+
+Solution: Simulation [Accepted]
 Instead of keeping track of how much champagne should end up in a glass, keep 
 track of the total amount of champagne that flows through a glass. For example, 
 if poured = 10 cups are poured at the top, then the total flow-through of the top 
 glass is 10; the total flow-through of each glass in the second row is 4.5, and so on.
-
-Algorithm
 
 In general, if a glass has flow-through X, then Q = (X - 1.0) / 2.0 quantity of champagne 
 will equally flow left and right. We can simulate the entire pour for 100 rows of glasses. 
