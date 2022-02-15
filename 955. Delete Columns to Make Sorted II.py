7d8fc06c8c75d947e14919ca0ff462213f598e95
Q2016 <k.sohraby@gmail.com>
2022-02-15 09:39:08 -0800

    Update 955. Delete Columns to Make Sorted II.py

--- 955. Delete Columns to Make Sorted II.py
+++ 955. Delete Columns to Make Sorted II.py
@@ -1,3 +1,12 @@
+Question:
+You are given an array of n strings strs, all of the same length. We may choose any deletion indices, and we delete all the characters in those 
+indices for each string. For example, if we have strs = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions 
+is ["bef", "vyz"]. Suppose we chose a set of deletion indices answer such that after deletions, the final array has its elements in lexicographic 
+order (i.e., strs[0] <= strs[1] <= strs[2] <= ... <= strs[n - 1]). Return the minimum possible value of answer.length.	
+	
+	
+Solution: Greedy
+	
 https://leetcode.com/problems/delete-columns-to-make-sorted-ii/discuss/844457/Python-3-or-Greedy-DP-(28-ms)-or-Explanation
 
 Explanation
@@ -28,3 +37,24 @@ class Solution:
             # not necessary, but speed things up
             if all(in_order): return ans   
         return ans
+
+
+class Solution(object):
+    def minDeletionSize(self, A):
+
+        A_len, str_len, res = len(A), len(A[0]), 0
+        is_need_compare = [True] * A_len
+
+        for i in xrange(str_len):
+            tmp = copy(is_need_compare)
+            for j in xrange(1, A_len):
+                if is_need_compare[j]:
+                    if A[j][i] < A[j - 1][i]:
+                        res += 1
+                        is_need_compare = tmp
+                        break
+
+                    elif A[j][i] > A[j - 1][i]:
+                        is_need_compare[j] = False
+                            
+        return res
