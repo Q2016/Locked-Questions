d2be32ef66d521be4cfc8e9c6fe2f0ff7ba38d45
Q2016 <k.sohraby@gmail.com>
2022-02-14 19:25:10 -0800

    Update 743. Network Delay Time.py

--- 743. Network Delay Time.py
+++ 743. Network Delay Time.py
@@ -1,11 +1,21 @@
+Question:
+You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges 
+times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+We will send a signal from a given node k. Return the time it takes for all the n nodes to receive the signal. If it is impossible 
+for all the n nodes to receive the signal, return -1.
+
+Solution:
+https://www.youtube.com/watch?v=EaphyqKU4PQ
 Approach #1: Depth-First Search [Accepted]
 Intuition
 
-Let's record the time dist[node] when the signal reaches the node. If some signal arrived earlier, we don't need to broadcast it anymore. Otherwise, we should broadcast the signal.
+Let's record the time dist[node] when the signal reaches the node. If some signal arrived earlier, we don't need to broadcast it anymore. 
+Otherwise, we should broadcast the signal.
 
 Algorithm
 
-We'll maintain dist[node], the earliest that we arrived at each node. When visiting a node while elapsed time has elapsed, if this is the currently-fastest signal at this node, let's broadcast signals from this node.
+We'll maintain dist[node], the earliest that we arrived at each node. When visiting a node while elapsed time has elapsed, if this is the 
+currently-fastest signal at this node, let's broadcast signals from this node.
 
 To speed things up, at each visited node we'll consider signals exiting the node that are faster first, by sorting the edges.
 
@@ -28,25 +38,20 @@ class Solution(object):
         return ans if ans < float('inf') else -1
         
         
- Complexity Analysis
+Complexity Analysis
+Time Complexity: O(N^N + E log E) where E is the length of times. We can only fully visit each node up to N−1 times, one per each other node. 
+Plus, we have to explore every edge and sort them. Sorting each small bucket of outgoing edges is bounded by sorting all of them, because of repeated use 
+of the inequality xlogx+ylogy≤(x+y)log(x+y).
 
-Time Complexity: O(N^N + E \log E)O(N 
-N
- +ElogE) where EE is the length of times. We can only fully visit each node up to N-1N−1 times, one per each other node. Plus, we have to explore every edge and sort them. Sorting each small bucket of outgoing edges is bounded by sorting all of them, because of repeated use of the inequality x \log x + y \log y \leq (x+y) \log (x+y)xlogx+ylogy≤(x+y)log(x+y).
-
-Space Complexity: O(N + E)O(N+E), the size of the graph (O(E)O(E)), plus the size of the implicit call stack in our DFS (O(N)O(N)).
+Space Complexity: O(N + E), the size of the graph O(E), plus the size of the implicit call stack in our DFS O(N).
 
 
 Approach #2: Dijkstra's Algorithm [Accepted]
 Intuition and Algorithm
 
 We use Dijkstra's algorithm to find the shortest path from our source to all targets. This is a textbook algorithm, refer to this link for more details.
-
 Dijkstra's algorithm is based on repeatedly making the candidate move that has the least distance travelled.
-
-In our implementations below, we showcase both O(N^2)O(N 
-2
- ) (basic) and O(N \log N)O(NlogN) (heap) approaches.
+In our implementations below, we showcase both O(N^2) (basic) and O(NlogN) (heap) approaches.
 
 Basic Implementation
 
