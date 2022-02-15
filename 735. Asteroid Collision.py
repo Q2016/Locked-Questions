726c870aad083334f68dc358ccab9a043bfcae54
Q2016 <k.sohraby@gmail.com>
2022-02-14 20:00:56 -0800

    Update 735. Asteroid Collision.py

--- 735. Asteroid Collision.py
+++ 735. Asteroid Collision.py
@@ -1,14 +1,20 @@
-https://leetcode.com/problems/asteroid-collision/solution/
+Question:
+We are given an array asteroids of integers representing asteroids in a row. For each asteroid, the absolute value represents its size, and 
+the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, 
+both will explode. Two asteroids moving in the same direction will never meet.
+
+Example 1:
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.    
 
 
-Approach #1: Stack [Accepted]
-Intuition
+Solution: Stack
 
 A row of asteroids is stable if no further collisions will occur. After adding a new asteroid 
 to the right, some more collisions may happen before it becomes stable again, and all of those 
 collisions (if they happen) must occur right to left. This is the perfect situation for using a stack.
-
-Algorithm
 
 Say we have our answer as a stack with rightmost asteroid top, and a new asteroid comes in. If 
 new is moving right (new > 0), or if top is moving left (top < 0), no collision occurs.
@@ -35,11 +41,8 @@ class Solution(object):
       
       
 Complexity Analysis
-
 Time Complexity: O(N), where N is the number of asteroids. Our stack pushes and pops each asteroid at most once.
-
 Space Complexity: O(N). We use a stack to keep track of the intermediate results. 
-  In the worst case, the states do not evolve at the end, i.e. we need O(N)
-  space where N is the number of input asteroids.
+In the worst case, the states do not evolve at the end, i.e. we need O(N) space where N is the number of input asteroids.
 
       
