b428004aa155e65daa7df4e7c99833b428827d8e
Q2016 <k.sohraby@gmail.com>
2022-02-15 12:25:55 -0800

    Update 951. Flip Equivalent Binary Trees.py

--- 951. Flip Equivalent Binary Trees.py
+++ 951. Flip Equivalent Binary Trees.py
@@ -1,18 +1,22 @@
-class Solution {
-public:
-    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        // Two null trees are flip equivalent
-        // A non-null and null tree are NOT flip equivalent
-        // Two non-null trees with different root values are NOT flip equivalent
-        // Two non-null trees are flip equivalent if
-        //      The left subtree of tree1 is flip equivalent with the left subtree of tree2 and the right subtree of tree1 is   
-        //      flipequivalent with the right subtree of tree2 (no flip case)
-        //      OR
-        //      The right subtree of tree1 is flip equivalent with the left subtree of tree2 and the left subtree of tree1 is
-        //      flipequivalent with the right subtree of tree2 (flip case)
-        if ( !root1 && !root2 ) return true;
-        if ( !root1 && root2 || root1 &&!root2 || root1->val != root2->val ) return false;
-        return flipEquiv( root1->left, root2->left ) && flipEquiv( root1->right, root2->right )
-            || flipEquiv( root1->right, root2->left ) && flipEquiv( root1->left, root2->right );
-    }
-};
+Question:
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.    
+
+
+Solution: Recursion
+If root1 and root2 have the same root value, then we only need to check if their children are equal (up to ordering.)
+There are 3 cases:
+If root1 or root2 is null, then they are equivalent if and only if they are both null.
+Else, if root1 and root2 have different values, they aren't equivalent.
+Else, let's check whether the children of root1 are equivalent to the children of root2. There are two different ways to pair these children.
+
+class Solution(object):
+    def flipEquiv(self, root1, root2):
+        if root1 is root2:
+            return True
+        if not root1 or not root2 or root1.val != root2.val:
+            return False
+
+        return (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right) or  \\
+                self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left))
