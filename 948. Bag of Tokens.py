1582afbfe62cb8e8e9621e3290935c111ef45bf3
Q2016 <k.sohraby@gmail.com>
2022-02-15 12:53:13 -0800

    Update 948. Bag of Tokens.py

--- 948. Bag of Tokens.py
+++ 948. Bag of Tokens.py
@@ -1,19 +1,22 @@
-Approach 1: Greedy
-Intuition
+Question:
+You have an initial power of power, an initial score of 0, and a bag of tokens where tokens[i] is the value of the ith token (0-indexed).
+Your goal is to maximize your total score by potentially playing each token in one of two ways:
+If your current power is at least tokens[i], you may play the ith token face up, losing tokens[i] power and gaining 1 score.
+If your current score is at least 1, you may play the ith token face down, gaining tokens[i] power and losing 1 score.
+Each token may be played at most once and in any order. You do not have to play all the tokens.
+Return the largest possible score you can achieve after playing any number of tokens.
+
+
+
+Solution: Greedy
 
 If we play a token face up, we might as well play the one with the smallest value. 
 Similarly, if we play a token face down, we might as well play the one with the largest value.
-
-Algorithm
-
 We don't need to play anything until absolutely necessary. Let's play tokens face up 
 until we can't, then play a token face down and continue.
-
 We must be careful, as it is easy for our implementation to be incorrect if we do not handle 
 corner cases correctly. We should always play tokens face up until exhaustion, then play one token face down and continue.
-
 Our loop must be constructed with the right termination condition: we can either play a token face up or face down.
-
 Our final answer could be any of the intermediate answers we got after playing tokens face up (but before playing them face down.)
 
 class Solution(object):
