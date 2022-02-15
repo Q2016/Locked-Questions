3c1d76f642853eb81f1187f845e8982596a918f6
Q2016 <k.sohraby@gmail.com>
2022-02-14 19:40:10 -0800

    Update 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -1,22 +1,34 @@
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int> > &matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int l = 0, r = m * n - 1;
-        while (l != r){
-            int mid = (l + r - 1) >> 1;
-            if (matrix[mid / m][mid % m] < target)
-                l = mid + 1;
-            else 
-                r = mid;
-        }
-        return matrix[r / m][r % m] == target;
-    }
-};
+Question:
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true    
 
-/*
-Use binary search.
-n * m matrix convert to an array => matrix[x][y] => a[x * m + y]
-an array convert to n * m matrix => a[x] =>matrix[x / m][x % m];
-*/
+    
+Solution:
+It is basically an advanced version of the binary search
+
+class Solution:
+
+    def searchMatrix(self, matrix, target):
+        if not matrix or target is None:
+            return False
+
+        rows, cols = len(matrix), len(matrix[0])
+        low, high = 0, rows * cols - 1
+        
+        while low <= high:
+            mid = (low + high) / 2
+            num = matrix[mid / cols][mid % cols]
+
+            if num == target:
+                return True
+            elif num < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return False    
