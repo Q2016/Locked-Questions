656a0693ad5899730017ea54a60dd697ad2f841d
Q2016 <k.sohraby@gmail.com>
2022-02-15 09:21:27 -0800

    Update 979. Distribute Coins in Binary Tree.py

--- 979. Distribute Coins in Binary Tree.py
+++ 979. Distribute Coins in Binary Tree.py
@@ -1,17 +1,28 @@
+Question:
+You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+Return the minimum number of moves required to make every node have exactly one coin.    
+    
+    
+Solution: Depth First Search
 
-class Solution {
-    int ans;
-    public int distributeCoins(TreeNode root) {
-        ans = 0;
-        dfs(root);
-        return ans;
-    }
+If the leaf of a tree has 0 coins (an excess of -1 from what it needs), then we should push a coin from its parent onto the leaf. If it has say, 
+4 coins (an excess of 3), then we should push 3 coins off the leaf. In total, the number of moves from that leaf to or from its parent is 
+excess = Math.abs(num_coins - 1). Afterwards, we never have to consider this leaf again in the rest of our calculation.
+We can use the above fact to build our answer. Let dfs(node) be the excess number of coins in the subtree at or below this node: namely, the number 
+of coins in the subtree, minus the number of nodes in the subtree. Then, the number of moves we make from this node to and from its children 
+is abs(dfs(node.left)) + abs(dfs(node.right)). After, we have an excess of node.val + dfs(node.left) + dfs(node.right) - 1 coins at this node.
 
-    public int dfs(TreeNode node) {
-        if (node == null) return 0;
-        int L = dfs(node.left);
-        int R = dfs(node.right);
-        ans += Math.abs(L) + Math.abs(R);
-        return node.val + L + R - 1;
-    }
-}
+
+class Solution(object):
+    def distributeCoins(self, root):
+        self.ans = 0
+
+        def dfs(node):
+            if not node: return 0
+            L, R = dfs(node.left), dfs(node.right)
+            self.ans += abs(L) + abs(R)
+            return node.val + L + R - 1
+
+        dfs(root)
+        return self.ans
