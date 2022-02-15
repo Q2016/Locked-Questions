4d880cb126d7cd85bd754a3995b893c3d09d18d3
Q2016 <k.sohraby@gmail.com>
2022-02-15 08:42:35 -0800

    Update 988. Smallest String Starting From Leaf.py

--- 988. Smallest String Starting From Leaf.py
+++ 988. Smallest String Starting From Leaf.py
@@ -1,7 +1,12 @@
+Question:
+You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+As a reminder, any shorter prefix of a string is lexicographically smaller. For example, "ab" is lexicographically smaller than "aba".
+A leaf of a node is a node that has no children.
+    
+    
+Solution:    
 https://leetcode.com/problems/smallest-string-starting-from-leaf/discuss/328119/Simple-Python-Solution%3A-top-down-DFS
-
-
-Please see and vote for my solutions for these similar problems.
 
 257. Binary Tree Paths
 
