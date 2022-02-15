39dde9a145935f546c18d831510ff84852f102b7
Q2016 <k.sohraby@gmail.com>
2022-02-15 08:44:03 -0800

    Update 987. Vertical Order Traversal of a Binary Tree.py

--- 987. Vertical Order Traversal of a Binary Tree.py
+++ 987. Vertical Order Traversal of a Binary Tree.py
@@ -1,3 +1,12 @@
+Question:
+Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+For each node at position (row, col), its left and right children will be at positions (row + 1, col - 1) and (row + 1, col + 1) respectively. 
+The root of the tree is at (0, 0). The vertical order traversal of a binary tree is a list of top-to-bottom orderings for each column index starting 
+from the leftmost column and ending on the rightmost column. There may be multiple nodes in the same row and same column. In such a case, sort 
+these nodes by their values. Return the vertical order traversal of the binary tree.
+
+
+Solution:
 Approach 1: queue + hash map
 https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231256/python-queue-%2B-hash-map  
 
