86f0543db7b1c926ed88cead907dfbdb82fe67c7
Q2016 <k.sohraby@gmail.com>
2022-02-15 17:26:13 -0800

    Update 919. Complete Binary Tree Inserter.py

--- 919. Complete Binary Tree Inserter.py
+++ 919. Complete Binary Tree Inserter.py
@@ -1,55 +1,58 @@
-/*
-Logic is simple:
+Question:
+A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.
+Design an algorithm to insert a new node to a complete binary tree keeping it complete after the insertion.
+Implement the CBTInserter class:
+CBTInserter(TreeNode root) Initializes the data structure with the root of the complete binary tree.
+int insert(int v) Inserts a TreeNode into the tree with value Node.val == val so that the tree remains complete, and returns the value of the 
+parent of the inserted TreeNode.
+TreeNode get_root() Returns the root node of the tree.
 
-Init with root, push all nodes whose has no left or right child;
-Insert new node to first node that has no left or right child.
-O(1) Insert and O(nleaf) space for queue only keep nodes has no left or right child
-Here is the C++ and Python code:
-[C++]
-*/
+Example 1:
+Input
+["CBTInserter", "insert", "insert", "get_root"]
+[[[1, 2]], [3], [4], []]
+Output
+[null, 1, 2, [1, 2, 3, 4]]
 
-class CBTInserter {
-private:
-    queue<TreeNode*> q;
-    TreeNode* head;
-public:
-    CBTInserter(TreeNode* root) {
-        head = root;
-        init_queue(root);
-    }
-    
-    void init_queue(TreeNode* root){
-        queue<TreeNode*> tq;
-        tq.push(root);
-        while(!tq.empty()){
-            TreeNode* p = tq.front();
-            tq.pop();
-            if(p->left == NULL || p->right == NULL){
-                q.push(p);
-            }
-            if(p->left != NULL){
-                tq.push(p->left);
-            }
-            if(p->right != NULL){
-                tq.push(p->right);
-            }
-        }
-    }
-    
-    int insert(int v) {
-        TreeNode* no = new TreeNode(v);
-        TreeNode* p = q.front();
-        if(p->left == NULL){
-            p->left = no;
-        }else{
-            p->right = no;
-            q.pop();
-        }
-        q.push(no);
-        return p->val;
-    }
-    
-    TreeNode* get_root() {
-        return head;
-    }
-};
+Explanation
+CBTInserter cBTInserter = new CBTInserter([1, 2]);
+cBTInserter.insert(3);  // return 1
+cBTInserter.insert(4);  // return 2
+cBTInserter.get_root(); // return [1, 2, 3, 4]    
+
+Solution: Deque
+Consider all the nodes numbered first by level and then left to right. Call this the "number order" of the nodes.
+At each insertion step, we want to insert into the node with the lowest number (that still has 0 or 1 children).
+By maintaining a deque (double ended queue) of these nodes in number order, we can solve the problem. After inserting a node, 
+that node now has the highest number and no children, so it goes at the end of the deque. To get the node with the lowest number, 
+we pop from the beginning of the deque.
+
+First, perform a breadth-first search to populate the deque with nodes that have 0 or 1 children, in number order.
+Now when inserting a node, the parent is the first element of deque, and we add this new node to our deque.
+
+class CBTInserter(object):
+    def __init__(self, root):
+        self.deque = collections.deque()
+        self.root = root
+        q = collections.deque([root])
+        while q:
+            node = q.popleft()
+            if not node.left or not node.right:
+                self.deque.append(node)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+    def insert(self, v):
+        node = self.deque[0]
+        self.deque.append(TreeNode(v))
+        if not node.left:
+            node.left = self.deque[-1]
+        else:
+            node.right = self.deque[-1]
+            self.deque.popleft()
+        return node.val
+
+    def get_root(self):
+        return self.root
