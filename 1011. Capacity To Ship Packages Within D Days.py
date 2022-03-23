cda929c59a60a3268ea02419ca6af62e252d0f32
Q2016 <k.sohraby@gmail.com>
2022-03-23 12:59:44 -0700

    Update 1011. Capacity To Ship Packages Within D Days.py

--- 1011. Capacity To Ship Packages Within D Days.py
+++ 1011. Capacity To Ship Packages Within D Days.py
@@ -1,8 +1,10 @@
-Binary search
-
+Question:
+A conveyor belt has packages that must be shipped from one port to another within days days.
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt. 
+We may not load more weight than the maximum weight capacity of the ship.
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
 
 Example 1:
-
 Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
 Output: 15
 Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
@@ -11,10 +13,12 @@ Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 
 3rd day: 8
 4th day: 9
 5th day: 10
+Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.    
 
-Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and 
-splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
 
+
+
+Solution: Binary search
 
 // for binary search look at: 
 //[Python/ Clear explanation] Powerful Ultimate Binary Search Template. Solved many problems.
