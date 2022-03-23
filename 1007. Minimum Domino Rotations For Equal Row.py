b3b113be0f726691ffad87df17ca40b1190c5b46
Q2016 <k.sohraby@gmail.com>
2022-03-23 12:58:04 -0700

    Update 1007. Minimum Domino Rotations For Equal Row.py

--- 1007. Minimum Domino Rotations For Equal Row.py
+++ 1007. Minimum Domino Rotations For Equal Row.py
@@ -1,4 +1,20 @@
-# hash frequency
+Question:
+In a row of dominoes, tops[i] and bottoms[i] represent the top and bottom halves of the ith domino. 
+We may rotate the ith domino, so that tops[i] and bottoms[i] swap values.
+Return the minimum number of rotations so that all the values in tops are the same, or all the values in bottoms are the same.
+If it cannot be done, return -1.
+
+
+
+
+
+
+
+
+
+
+
+Solution: hash frequency
 
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
