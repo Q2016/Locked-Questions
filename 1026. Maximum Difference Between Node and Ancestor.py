4b5b08011d61dbf8ec7535d6f85e6cf3c1eaf809
Q2016 <k.sohraby@gmail.com>
2022-03-23 17:21:08 -0700

    Update 1026. Maximum Difference Between Node and Ancestor.py

--- 1026. Maximum Difference Between Node and Ancestor.py
+++ 1026. Maximum Difference Between Node and Ancestor.py
@@ -1,11 +1,9 @@
 Question:
 Given the root of a binary tree, find the maximum value v for which there exist different 
 nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
-
 A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
 
 Example 1:
-
 Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
 Output: 7
 Explanation: We have various ancestor-node differences, some of which are given below :
@@ -16,33 +14,29 @@ Explanation: We have various ancestor-node differences, some of which are given 
 Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.    
 
 
-Solution: Inorder
 
-class Solution {
-    // record the required maximum difference
-    int result = 0;
 
-    public int maxAncestorDiff(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        result = 0;
-        helper(root, root.val, root.val);
-        return result;
-    }
+Solution: Maximum Minus Minimum (seems like a pre/post order)
 
-    void helper(TreeNode node, int curMax, int curMin) {
-        if (node == null) {
-            return;
-        }
-        // update `result`
-        int possibleResult = Math.max(Math.abs(curMax - node.val), Math.abs(curMin - node.val));
-        result = Math.max(result, possibleResult);
-        // update the max and min
-        curMax = Math.max(curMax, node.val);
-        curMin = Math.min(curMin, node.val);
-        helper(node.left, curMax, curMin);
-        helper(node.right, curMax, curMin);
-        return;
-    }
-}
+Given any two nodes on the same root-to-leaf path, they must have the required ancestor relationship.
+Therefore, we just need to record the maximum and minimum values of all root-to-leaf paths and return the maximum difference.
+To achieve this, we can record the maximum and minimum values during the recursion and return the difference when encountering leaves.
+
+
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        def helper(node, cur_max, cur_min):
+            # if encounter leaves, return the max-min along the path
+            if not node:
+                return cur_max - cur_min
+            # else, update max and min
+            # and return the max of left and right subtrees
+            cur_max = max(cur_max, node.val)
+            cur_min = min(cur_min, node.val)
+            left = helper(node.left, cur_max, cur_min)
+            right = helper(node.right, cur_max, cur_min)
+            return max(left, right)
+
+        return helper(root, root.val, root.val)
