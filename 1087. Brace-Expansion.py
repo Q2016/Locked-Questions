4b857c393b5a4e6875c5454151ae62f726049a55
Q2016 <k.sohraby@gmail.com>
2022-03-25 08:08:30 -0700

    Update 1087. Brace-Expansion.py

--- 1087. Brace-Expansion.py
+++ 1087. Brace-Expansion.py
@@ -16,12 +16,8 @@ Output: ["abcd"]
 
 
 
-Solution: Recursion
-    
-https://goodtecher.com/leetcode-1087-brace-expansion/
-
-Time Complexity: O(N).
-Space Complexity: O(N).    
+Solution: Recursion (Why is the time complexity o(n)? )
+it can also be solved using backtrackng or BFS (Breath-First-Search)  
     
     
 Explanation
@@ -29,43 +25,45 @@ The goal of the problem is to find all combination strings. Whenever we encounte
 we can build combinations from different options, after that, we go to the character after ‘}’ 
 to continue building combinations. When we encounter a letter, we just go to the next character 
 in the string. In the end, sort the results in alphabetically ascending order.
-
+Example the code must do this {a,b,c}d{e,f}          ""
+                                                   /  |  \
+                                                  a   b    c
+                                                  |   |    |
+                                                  d   d    d
+                                                 /\   /\   /\
+                                                e  f  e f  e  f
+                
 
 class Solution:
     def expand(self, s: str) -> List[str]:
         results = []
-        
         end = len(s)
-        
-        self.helper(results, "", 0, s)
-        
+        self.helper(results, "", 0, s)       
         results = sorted(results)
-        
         return results
                 
     def helper(self, results, combination, start, s):        
-        if start == len(s):
+        if start == len(s): # start is zero
             results.append(combination)
             return
         
         i = start
-        
         if s[i] == '{':
-            j = i + 1            
-            
-            options = []
-
+            j = i + 1 # go one step ahead                      
+            options = [] # we are inside braces
             while s[j] != '}':
                 if s[j] != ',':
                     options.append(s[j])
                 j += 1
-            
+            # so options will be a list [a,b,c] coming from {a,b,c}
             for option in options:                
                 self.helper(results, combination + option, j + 1, s)
-
-        elif s[i] == '}':
+        elif s[i] == '}': # getting out of braces
             return
-        
         else:                
             self.helper(results, combination + s[i], i + 1, s)
 
+
+            
+Time Complexity: O(N).
+Space Complexity: O(N).              
