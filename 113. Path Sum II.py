d4db465db128d4bb86c6224b52ca9c94e3ca4556
Q2016 <k.sohraby@gmail.com>
2022-03-26 15:12:49 -0700

    Update 113. Path Sum II.py

--- 113. Path Sum II.py
+++ 113. Path Sum II.py
@@ -17,29 +17,66 @@ Question:
 #    [5,8,4,5]
 # ]
 
-Solution: Preorder
+
+
+
+
+Solution: Backtracking
     
     
 # Time:  O(n)
 # Space: O(h), h is height of binary tree
     
-
-class Solution:
-
-    def pathSum(self, root, sum):
-        return self.pathSumRecu([], [], root, sum)
-
+# Backtracking
     
-    def pathSumRecu(self, result, cur, root, sum):
-        if root is None:
-            return result
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        def dfs(root, targetSum, path):
+            if root == None: return None
+            targetSum -= root.val
+            path.append(root.val)
+            if root.left == None and root.right == None:  # Is leaf node
+                if targetSum == 0:  # Found a valid path
+                    ans.append(path.copy())
+            else:
+                dfs(root.left, targetSum, path)
+                dfs(root.right, targetSum, path)
+            path.pop()  # backtrack
+
+        ans = []
+        dfs(root, targetSum, [])
+        return ans
+  
+
+Complexity
+
+Time: O(N^2), where N is the number of elements in the binary tree.
+First, we think the time complexity is O(N) because we only visit each node once.
+But we forgot to calculate the cost to copy the current path when we found a valid path, which in the worst case 
+can cost O(N^2), let see the following example for more clear.
+photo: https://leetcode.com/problems/path-sum-ii/discuss/1382332/C%2B%2BPython-DFS-Clean-and-Concise-Time-complexity-explained
+    
+    
+    
+#  Simple Recursive
+    
+    def pathSum(self, root, sum):
+        res = []
+        self.dfs(root, sum, [], res)
+        return res
         
-        if root.left is None and root.right is None and root.val == sum:
-            result.append(cur + [root.val])
-            return result
-        
-        cur.append(root.val)
-        self.pathSumRecu(result, cur, root.left, sum - root.val)
-        self.pathSumRecu(result, cur, root.right, sum - root.val)
-        cur.pop()
-        return result
+    def dfs(self, root, sum, ls, res):
+        if root:
+			if not root.left and not root.right and sum == root.val:
+				ls.append(root.val)
+				res.append(ls)
+            self.dfs(root.left, sum-root.val, ls+[root.val], res)
+            self.dfs(root.right, sum-root.val, ls+[root.val], res)
+            
+    def pathSum2(self, root, sum):
+        if not root:
+            return []
+        if not root.left and not root.right and sum == root.val:
+            return [[root.val]]
+        tmp = self.pathSum(root.left, sum-root.val) + self.pathSum(root.right, sum-root.val)
+        return [[root.val]+i for i in tmp]
