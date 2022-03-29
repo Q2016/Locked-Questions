6caa68a4a4cc93b9d4393f0303d58d743b544639
Q2016 <k.sohraby@gmail.com>
2022-03-29 12:50:23 -0700

    Update 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py

--- 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py
+++ 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py
@@ -11,29 +11,36 @@ Explanation: The maximum side length of square with sum less than 4 is 2 as show
     
     
     
-Solution:   DP + Prefix sum + Binary search
+Solution: Prefix sum
 
-Use dynamic programming to store the prefix sum. Since value in matrix is positive, 
-so we can use binary search to find the appropriate square length k.
+PrefixSum prefixsum[i][j] is the sum of every element from (0,0) to (i,j).
+By using prefix sum, we can calculate the sum of every element from (i,j) to (i+k,j+k) easily.
+sum = prefixSum[i+k][j+k] - prefixSum[i-1][j+k] - prefixSum[i+k][j-1] + prefixSum[i-1][j-1]
 
-class Solution(object):
-    def maxSideLength(self, mat, threshold):
+Time: O(m*n*Min(m,n))
 
-        if not mat:
-            return 0
-        max_square = 0
-        dp = [[0] * (len(mat[0]) + 1) for _ in range(len(mat) + 1)]
-        for i in range(1, len(mat) + 1):
-            for j in range(1, len(mat[0]) + 1):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + mat[i - 1][j - 1]
-                l = 1
-                r = min(i, j)
-                while l <= r:
-                    k = (l + r) / 2
-                    cur_sum = dp[i][j] - dp[i - k][j] - dp[i][j - k] + dp[i - k][j - k]
-                    if cur_sum <= threshold:
-                        max_square = max(max_square, k)
-                        l = k + 1
-                    else:
-                        r = k - 1
-        return max_square
+class Solution {
+    public int maxSideLength(int[][] mat, int threshold) {
+        int m = mat.length, n = mat[0].length;
+        // Find prefix sum
+        int[][] prefixSum = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                sum += mat[i-1][j-1];
+                prefixSum[i][j] = prefixSum[i-1][j] + sum;
+            }
+        }
+        // Start from the largest side length
+        for(int k = Math.min(m, n)-1; k > 0; k--) {
+            for(int i = 1; i+k <= m; i++) {
+                for(int j = 1; j+k <= n; j++) {
+                    if(prefixSum[i+k][j+k] - prefixSum[i-1][j+k] - prefixSum[i+k][j-1] + prefixSum[i-1][j-1] <= threshold) {
+                        return k+1;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
