775a1aeeb802cf0e9475dd5a2b499a00949dc104
Q2016 <k.sohraby@gmail.com>
2022-03-29 19:05:43 -0700

    Update 1296. Divide Array in Sets of K Consecutive Numbers.py

--- 1296. Divide Array in Sets of K Consecutive Numbers.py
+++ 1296. Divide Array in Sets of K Consecutive Numbers.py
@@ -6,6 +6,10 @@ Example 1:
 Input: nums = [1,2,3,3,4,4,5,6], k = 4
 Output: true
 Explanation: Array can be divided into [1,2,3,4] and [3,4,5,6].
+Example 2:
+Input: nums = [3,2,1,2,3,4,3,4,5,9,10,11], k = 3
+Output: true
+Explanation: Array can be divided into [1,2,3] , [2,3,4] , [3,4,5] and [9,10,11].    
     
     
     
@@ -13,8 +17,7 @@ Explanation: Array can be divided into [1,2,3,4] and [3,4,5,6].
     
     
     
-    
-Solution: Greedy 
+Solution: Greedy (look at the comments at the python question)
 
 We can use a greedy approach and start with the smallest number and see if the numbers from that number + k exist and then keep 
 removing them from the numbers we have, if there is a case where it's not possible then we return false.
@@ -52,14 +55,17 @@ Space: O(n). Since we store the count of all the numbers.
 Time: O(nlogn). Since we use map which is sorted and each lookup is O(logn).    
     
 
-    def isPossibleDivide(self, A, k):
-        c = collections.Counter(A)
-        for i in sorted(c):
-            if c[i] > 0:
-                for j in range(k)[::-1]:
-                    c[i + j] -= c[i]
-                    if c[i + j] < 0:
+from collections import Counter
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        count = Counter(nums)
+        for n in sorted(count):
+            if count[n] > 0:
+                need = count[n]
+                for i in range(n,n+k): # because we want k consecutive numbers
+                    if count[i] < need: # what we need (at least), otherwise cant have consequtives in one group
                         return False
+                    count[i] -= need
         return True
         
 
