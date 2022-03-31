3a9308f062f4a39cd2aab09207d491ce914cbc17
Q2016 <k.sohraby@gmail.com>
2022-03-30 21:41:24 -0700

    Update 137. Single Number II.py

--- 137. Single Number II.py
+++ 137. Single Number II.py
@@ -1,12 +1,26 @@
 Question:
-# Given an array of integers, every element appears three times except for one. Find that single one.
-# 
-# Note:
-# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Given an array of integers, every element appears three times except for one. Find that single one. 
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 Solution:
 
+    def singleNumber(self, nums):
+        
+        return (sum(set(nums)) * 3 - sum(nums)) / 2
+    
 
     def singleNumber(self, A):
         one, two = 0, 0
@@ -30,10 +44,6 @@ Solution:
 
         return (collections.Counter(list(set(nums)) * 3) - collections.Counter(nums)).keys()[0]
 
-
-    def singleNumber(self, nums):
-        
-        return (sum(set(nums)) * 3 - sum(nums)) / 2
 
 
     def singleNumber(self, A):
