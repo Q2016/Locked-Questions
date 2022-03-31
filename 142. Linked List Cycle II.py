67edf50cd60fe59584bebde737bc2f36c3a99618
Q2016 <k.sohraby@gmail.com>
2022-03-31 10:10:30 -0700

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -1,9 +1,6 @@
 Question:
 Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously 
-following the next pointer. Internally, pos is used to denote the index of the node that tail's 
-next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
-Do not modify the linked list.
+Do not modify the linked list. Follow up: Can you solve it using O(1) (i.e. constant) memory?
 
 
 
@@ -11,6 +8,8 @@ Solution: ---
     
 https://leetcode.com/problems/linked-list-cycle-ii/discuss/44902/Sharing-my-Python-solution
 
+Python:    
+    
 def detectCycle(self, head):
     slow = fast = head
     while fast and fast.next:
@@ -20,7 +19,41 @@ def detectCycle(self, head):
             break
     else:
         return None
-    while head != slow:
+    while head != slow: # we do another cycle to find the node
         slow = slow.next
         head = head.next
     return head
+
+
+C++:
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return NULL;
+        
+        ListNode *slow=head,*fast=head;
+        
+        //cycle checking
+        do{
+            //move slow by one
+            slow = slow->next;
+            if(!slow) break;
+            
+            //move fast by 2
+            fast= fast->next;
+            if(!fast) break;
+            fast = fast->next;
+            if(!fast) break;            
+            
+        }while(slow!=fast);
+        //no cycle
+        if(!slow or !fast)
+            return NULL;
+        
+        //check the entry point
+        fast = head;
+        while(fast !=slow)
+            fast=fast->next, slow=slow->next;
+        return fast;
+    }
+};    
