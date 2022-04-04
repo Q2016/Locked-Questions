0e4b5afeb96de11b74c3e709e528d0339a5f371e
Q2016 <k.sohraby@gmail.com>
2022-04-03 19:41:42 -0700

    Create 1456. Maximum Number of Vowels in a Substring of Given Length.py

---
+++ 1456. Maximum Number of Vowels in a Substring of Given Length.py
@@ -0,0 +1,47 @@
+Question:
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+Example 1:
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+
+
+  
+Solution:
+  
+Maintain a sliding window of size k, (i - k, i], and count in/out the vowels inside/outside during sliding;
+
+    public int maxVowels(String s, int k) {
+        int ans = 0;
+        // Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        var vowels = Set.of('a', 'e', 'i', 'o', 'u'); // Java 11 Collection factory method, credit to @Sithis
+        for (int i = 0, winCnt = 0; i < s.length(); ++i) {
+            if (vowels.contains(s.charAt(i))) {
+                ++winCnt; 
+            }
+            if (i >= k && vowels.contains(s.charAt(i - k))) {
+                --winCnt;
+            }
+            ans = Math.max(winCnt, ans);
+        }
+        return ans;
+    }
+                                      
+                                      
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        ans = cnt = 0
+        for i, c in enumerate(s):
+            if c in vowels:
+                cnt += 1
+            if i >= k and s[i - k] in vowels:
+                cnt -= 1
+            ans  = max(cnt, ans)
+        return ans    
+                                      
+                                      
+Analysis:
+
+Time: O(n), space: O(1), where n = s.length().
