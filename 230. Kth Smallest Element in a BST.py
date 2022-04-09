53283f6da506ed176429bd615e2f4805b8b96afa
Q2016 <k.sohraby@gmail.com>
2022-04-09 16:29:19 -0700

    Update 230. Kth Smallest Element in a BST.py

--- 230. Kth Smallest Element in a BST.py
+++ 230. Kth Smallest Element in a BST.py
@@ -1,13 +1,28 @@
 Question:
-Given the root of a binary search tree, and an integer k, return the kth smallest value 
-(1-indexed) of all the values of the nodes in the tree.    
+Given the root of a binary search tree, and an integer k, return the kth smallest value of all the values of the nodes in the tree.    
 
 
 Solution: Inorder
     
-    def kthSmallest(self, root, k):
-        
-        def inorder(r):
-            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+To solve the problem, one could use the property of BST : inorder traversal of BST is an array sorted in the ascending order.    
     
-        return inorder(root)[k - 1]    
+    
+def kthSmallest_dfs_early_stopping(self, root, k):
+	res = []
+	
+    def inorder(node):
+		if not node: return
+		inorder(node.left)
+		if len(res) == k:
+			return
+		res.append(node.val)
+		inorder(node.right)
+        
+	inorder(root)
+	return res[-1] 
+
+    
+    
+Complexity Analysis
+Time complexity : O(N) to build a traversal.
+Space complexity : O(N) to keep an inorder traversal.    
