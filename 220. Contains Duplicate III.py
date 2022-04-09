82f9909fecc12103583a7aa9b70d7bb9b226c08e
Q2016 <k.sohraby@gmail.com>
2022-04-09 11:25:38 -0700

    Update 220. Contains Duplicate III.py

--- 220. Contains Duplicate III.py
+++ 220. Contains Duplicate III.py
@@ -8,32 +8,30 @@ Output: true
 
 Solution:
     
-In this problem we need to iterate over window of size k+1 and check if there is numbers with difference <=t in this window. 
-What we need to do efficiently is to add and remove elements from our window, and my choice of data structure is BST, 
-which is implemented in SortedList in python. So on each step we have sorted list of elements in this window. Imagine the case:
-[1, 3, 7, 12] and new number we need to insert is 10, and t = 2. Then we need to consider range [8,12] and check if we have 
-numbers in our SList in this range. We can do two binary searches here: bisect_left for left boundary and bisect_right 
-for right boundary. Also we need to check if pos1 != len(SList), if this is the case, it means that new number is bigger 
-than bigges number in list + t, so in this case we just put it directly to our list. If pos1 != pos2, this means, that we 
-found some number i our [nums[i] - t, nums[i] + t] range, so we immediatly return True.
+The idea is like the bucket sort algorithm. Suppose we have consecutive buckets covering the range of nums with each bucket 
+a width of (t+1). If there are two item with difference <= t, one of the two will happen:
+
+(1) the two in the same bucket
+(2) the two in neighbor buckets
+
+Python
+
+def containsNearbyAlmostDuplicate(self, nums, k, t):
+    if t < 0: return False
+    n = len(nums)
+    d = {}
+    w = t + 1
+    for i in xrange(n):
+        m = nums[i] / w
+        if m in d:
+            return True
+        if m - 1 in d and abs(nums[i] - d[m - 1]) < w:
+            return True
+        if m + 1 in d and abs(nums[i] - d[m + 1]) < w:
+            return True
+        d[m] = nums[i]
+        if i >= k: del d[nums[i - k] / w]
+    return False  
 
 
-
-from sortedcontainers import SortedList
-
-class Solution:
-    def containsNearbyAlmostDuplicate(self, nums, k, t):
-        SList = SortedList()
-        for i in range(len(nums)):
-            if i > k: SList.remove(nums[i-k-1])   
-            pos1 = SortedList.bisect_left(SList, nums[i] - t)
-            pos2 = SortedList.bisect_right(SList, nums[i] + t)
-            
-            if pos1 != pos2 and pos1 != len(SList): return True
-            
-            SList.add(nums[i])
-        
-        return False
-    
-Complexity: time complexity is O(n log k), because we do n steps, each one with O(log k) complexity to do binary search, 
-remove and add elements. Space complexity is O(k) to keep our SortedList updated.    
+O(n) time O(n) space using buckets
