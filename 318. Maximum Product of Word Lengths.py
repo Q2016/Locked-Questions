991b2caa6edf0398870a24b5a17b52af6c861250
Q2016 <k.sohraby@gmail.com>
2022-04-11 13:40:14 -0700

    Update 318. Maximum Product of Word Lengths.py

--- 318. Maximum Product of Word Lengths.py
+++ 318. Maximum Product of Word Lengths.py
@@ -10,27 +10,50 @@ Explanation: The two words can be "abcw", "xtfn".
 	
 Solution:
 
-Straightforward solution is to compare each pair of words. We can use the fact that each word has only letters from a to z and for 
-each word what is matter if it has some letter or not. Let d[word] be bitmask of word. For example for word aaabe we will have 
-mask ...10011, here I for simplicity show only the last part, all other elements are zero. We have 1 in the end, because we have 
-a in our word, we have 1 as next elements, because we have b, then we have 0, because we do not have c and so on. So, the whole 
-algorithm can be separated into two stages:
-Compute all masks for all words, we will use d[word] |= 1<<(ord(l) - 97) for this, where 97 is code of symbol a. 
-Also we use | (or) here to update zero-elements, but if we have one-elements, they will not change.
-For every pair of words check if d[w1] & d[w2] == 0, this is condition that pair of words will not have any intersections 
-and update our ans if they not.
+Simple brute force solution is to test if every pair for similarity and take the product is they are not similar. Return the maximum product if 
+several dissimilar pairs are found otherwise return 0.
+First optimization is to run the two for loops from** i = (0 to N-1) and j = (i+1, N-1)**. This is lesser than N^2.
+How do we find if two words are similar?
 
+First approach will be to take two words, put all characters into a set and test for membership in that set for the other words. Can we do better? 
+Can we pre-process somehow?
+Second approach will be to prepprocess each word and generate a unique sign for it. Take a boolean array of 26. Mark the position for every 
+character in this word as True. Now traverse this array in order and generate a unique signature. O(26 + len(word)). Can we still do better?
+Can we use bit-wise manipulation? int32 is 32 bits. There are 26 letters. Set a bit for every character. How do you test if two words have no 
+similar letters? Just AND them. Testing them now becomes a constant time operation
+
+class Solution(object):
+    def sign(self, word):
+        value = 0
+        for c in word:
+            value = value | (1 << (ord(c)-97))
+        return value
+    
     def maxProduct(self, words):
-        d, ans = defaultdict(int), 0
-        for word in words:
-            for l in word:
-                d[word] |= 1<<(ord(l) - 97)
-                
-        for w1, w2 in combinations(d.keys(), 2):
-            if d[w1] & d[w2] == 0: 
-                ans = max(ans, len(w1)*len(w2))            
-        return ans	
 
+        signature = [self.sign(x) for x in words]
+        max_product, N = 0, len(words)
+        for i in range(N):
+            for j in range(i+1, N):
+                if signature[i] & signature[j] == 0:
+                    max_product = max(max_product, len(words[i])*len(words[j]))
+        return max_product
 
-Compexity
-Time complexity is O(n*s) + O(n^2), where s is the average length of word and n is number of words. Space complexity is O(n).
+Use set as a signature
+Yes another way is to use sets in an interesting manner.
+
+Use this for generating signatures: signature = {w:set(w) for w in words}
+Use this for testing: bool(signature[words[i]] & signature[words[j]]) == False
+https://docs.python.org/2/library/sets.html
+
+class Solution(object):
+    def maxProduct(self, words):
+
+        signature = {w:set(w) for w in words}
+        max_product, N = 0, len(words)
+        for i in range(N):
+            for j in range(i+1, N):
+                # Intersection of two sets
+                if bool(signature[words[i]] & signature[words[j]]) == False:
+                    max_product = max(max_product, len(words[i])*len(words[j]))
+        return max_product 
