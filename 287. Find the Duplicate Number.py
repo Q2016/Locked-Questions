f9a2623d848b5eafdec493465732ae0fd3e8edeb
Q2016 <k.sohraby@gmail.com>
2022-04-10 22:28:09 -0700

    Update 287. Find the Duplicate Number.py

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -4,29 +4,31 @@ There is only one repeated number in nums, return this repeated number.
 You must solve the problem without modifying the array nums and uses only constant extra space.    
     
 
-Solution: Sum of Set Bits
-For solution read https://leetcode.com/problems/find-the-duplicate-number/solution/
-    
-    def findDuplicate(self, nums: List[int]) -> int:
-        duplicate = 0
-        n = len(nums)
-        bits = n.bit_length()
-        for bit in range(bits):
-            mask = 1 << bit
-            base_count = 0
-            nums_count = 0
-            for i in range(n):
-                # If bit is set in number (i) then add 1 to base_count
-                if i & mask:
-                    base_count += 1
-                    
-                # If bit is set in nums[i] then add 1 to nums_count
-                if nums[i] & mask:
-                    nums_count += 1
-                    
-            # If the current bit is more frequently set in nums than it is in 
-            # the range [1, 2, ..., n] then it must also be set in the duplicate number.
-            if nums_count - base_count > 0:
-                duplicate |= mask
-                
-        return duplicate
+Solution: bit manipulation
+A so-called O(1) space but essencially O(N) space algorithm using bit manipulation: use each bit of number seen as the seen array in solution 1.
+
+def findDuplicate(self, nums: List[int]) -> int:
+        seen = 0
+        for num in nums:
+            if seen & (1 << num):
+                return num
+            seen |= 1 << num
+            
+            
+            
+Finding cycle
+
+Complexity: Time complexity is O(n), because we potentially can traverse all list. Space complexity is O(1), because we actually do 
+not use any extra space: our linked list is virtual.
+
+class Solution:
+    def findDuplicate(self, nums):
+        slow, fast = nums[0], nums[0]
+        while True:
+            slow, fast = nums[slow], nums[nums[fast]]
+            if slow == fast: break
+           
+        slow = nums[0];
+        while slow != fast:
+            slow, fast = nums[slow], nums[fast]
+        return slow
