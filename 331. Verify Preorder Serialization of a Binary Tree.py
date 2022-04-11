543b490a40c3e82bfdaa6a702add7f870a2cbce1
Q2016 <k.sohraby@gmail.com>
2022-04-11 16:59:25 -0700

    Update 331. Verify Preorder Serialization of a Binary Tree.py

--- 331. Verify Preorder Serialization of a Binary Tree.py
+++ 331. Verify Preorder Serialization of a Binary Tree.py
@@ -10,25 +10,32 @@ a binary tree.              9
                     4       1      6
 Note: You are not allowed to reconstruct the tree.        
 
+Example 1:
+Input: preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+Output: true
 
+  
+  
+  
 Solution:
 
-In a binary tree, if we consider null as leaves, then first, all non-null node provides 2 outdegree and 1 indegree (2 children and 1 parent), 
-except root. second, all null node provides 0 outdegree and 1 indegree (0 child and 1 parent).
+Some used stack. Some used the depth of a stack. Here I use a different perspective. In a binary tree, if we consider null as leaves, then
+
+-all non-null node provides 2 outdegree and 1 indegree (2 children and 1 parent), except root
+-all null node provides 0 outdegree and 1 indegree (0 child and 1 parent).
 Suppose we try to build this tree. During building, we record the difference between out degree and in degree diff = outdegree - indegree. 
 When the next node comes, we then decrease diff by 1, because the node provides an in degree. If the node is not null, we increase diff by 2, 
 because it provides two out degrees. If a serialization is correct, diff should never be negative and diff will be zero when finished.
 
-def isValidSerialization(preorder):
-    nodes = preorder.split(",")
-    diff = 1
-    for node in nodes:
-        if (--diff < 0): 
-            return false
-        if node!="#": 
-            diff += 2
-    
-    return diff == 0
+public boolean isValidSerialization(String preorder) {
+    String[] nodes = preorder.split(",");
+    int diff = 1;
+    for (String node: nodes) {
+        if (--diff < 0) return false;
+        if (!node.equals("#")) diff += 2;
+    }
+    return diff == 0;
+}
 
 
 
