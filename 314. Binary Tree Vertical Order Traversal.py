c21f9b0d2d2b6bf0d20e7d3735fb1de360c9622e
Q2016 <k.sohraby@gmail.com>
2022-04-11 13:16:33 -0700

    Update 314. Binary Tree Vertical Order Traversal.py

--- 314. Binary Tree Vertical Order Traversal.py
+++ 314. Binary Tree Vertical Order Traversal.py
@@ -1,31 +1,35 @@
 Question:
-Given a binary tree, return thevertical ordertraversal of its nodes' values. (ie, from top to bottom, column by column).
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
 If two nodes are in the same row and column, the order should be from left to right.
 
 Examples 1:
 Input: [3,9,20,null,null,15,7], Output: [[9],[3,15],[20],[7]]
    3
-  /\
- /  \
- 9  20
-    /\
-   /  \
-  15   7 
+  / \
+ /   \
+9     20
+     / \
+    /   \
+  15     7 
 
 
-Solution: BFS + hash solution.
-
-    def verticalOrder(self, root):
-
-        cols = collections.defaultdict(list)
-        queue = [(root,0)]
-
-        for node, col in queue:
-            if node:
-                cols[col].append(node.val)
-                queue += (node.left, col - 1), (node.right, col + 1)
-        return [cols[col] for col in sorted(cols)]
    
    
-# Time:  O(n)
-# Space: O(n)   
+   
+   
+Solution: 
+https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231318/How-is-this-different-from-%22314.-Binary-Tree-Vertical-Order-Traversal%22/258772/
+
+The difference is the way of handling nodes that have same x and y coordinates (for picture above link):
+
+-LC 314 If two nodes are in the same row and column, the order should be from left to right.
+When two nodes are in the same position, the order is decided by their x coordinates (node from the left subtree comes first). node6 -> node3
+-LC 987 If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
+When two nodes are in the same position, the order is decided by their values (node with smaller value comes first). node3 -> node6    
+
+
+
+   
+      
+      
+      
