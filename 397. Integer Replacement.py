6a0ace7d7aaa11d3bffeb03bcde24b566a180925
Q2016 <k.sohraby@gmail.com>
2022-04-13 06:55:05 -0700

    Update 397. Integer Replacement.py

--- 397. Integer Replacement.py
+++ 397. Integer Replacement.py
@@ -5,8 +5,16 @@ you can replace n with either n + 1 or n - 1. What is the minimum number of repl
 Example 1:
 Input:8, Output: 3, Explanation: 8 -> 4 -> 2 -> 1
 
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
       
-Solution:      
+Solution:  (We do direct approach similar to 1342.)    
 Denote f(n) the minimum number of jumps from n to 1. By definition, we have the recurrence 
 f(1) = 0, f(2n) = 1 + f(n), f(2n + 1) = min(f(2n) + 1, f(2n + 2) + 1). First notice that this sequence is well defined 
 because f(2n + 2) = f(n + 1) + 1, so f(2n + 1) = min(f(2n) + 1, f(n + 1) + 2). Every element is defined by some element before it.
