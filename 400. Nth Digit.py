8e0039b2c4d277436642ac607a79d11596321ad5
Q2016 <k.sohraby@gmail.com>
2022-04-13 07:34:47 -0700

    Update 400. Nth Digit.py

--- 400. Nth Digit.py
+++ 400. Nth Digit.py
@@ -7,6 +7,15 @@ Output: 0, (we count digits not numbers)
 Explanation: The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
 
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
 Solution:    
 
 How many digits of size size can we have?
