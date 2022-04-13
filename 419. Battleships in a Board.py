ac0486b0fbb20f624b52370bb5e4314ac16edc16
Q2016 <k.sohraby@gmail.com>
2022-04-13 16:56:27 -0700

    Update 419. Battleships in a Board.py

--- 419. Battleships in a Board.py
+++ 419. Battleships in a Board.py
@@ -9,6 +9,13 @@ Input: board = [["X",".",".","X"], Output: 2
                 [".",".",".","X"],
                 [".",".",".","X"]]
   
+  
+  
+  
+  
+  
+  
+  
 Solution:
 Going over all cells, we can count only those that are the "first" cell of the battleship. First cell will be defined as the most top-left cell. 
 We can check for first cells by only counting cells that do not have an 'X' to the left and do not have an 'X' above them.
