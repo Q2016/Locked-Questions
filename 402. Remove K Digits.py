b087c33b476813f91cdc75d61f78a4824f8f40dd
Q2016 <k.sohraby@gmail.com>
2022-04-13 09:15:14 -0700

    Update 402. Remove K Digits.py

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -7,6 +7,14 @@ Input: num = "1432219", k = 3
 Output: "1219"
 Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
 
+        
+        
+        
+        
+        
+        
+        
+        
 
 Solution: Using stack
 The first algorithm is straight-forward. Let's think about the simplest case: how to remove 1 digit from the number so 
