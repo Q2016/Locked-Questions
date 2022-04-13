6797b31e80d5765d7cf525fd7c63f34f8efff44f
Q2016 <k.sohraby@gmail.com>
2022-04-12 19:44:09 -0700

    Update 378. Kth Smallest Element in a Sorted Matrix.py

--- 378. Kth Smallest Element in a Sorted Matrix.py
+++ 378. Kth Smallest Element in a Sorted Matrix.py
@@ -12,27 +12,33 @@ Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8
 
 
 
-Solution: BST
-    
-    def kthSmallest(self, matrix, k):
-        m, n = len(matrix), len(matrix[0])  # For general, the matrix need not be a square
+Solution: Binary Search
+  
+We are done here, but let's think about this problem in another way:
+The key point for any binary search is to figure out the "Search Space". For me, I think there are two kind of "Search Space" -- index and 
+range(the range from the smallest number to the biggest number). Most usually, when the array is sorted in one direction, we can use index as 
+"search space", when the array is unsorted and we are going to find a specific number, we can use "range".
 
-        def countLessOrEqual(x):
-            cnt = 0
-            c = n - 1  # start with the rightmost column
-            for r in range(m):
-                while c >= 0 and matrix[r][c] > x: c -= 1  # decrease column until matrix[r][c] <= x
-                cnt += (c + 1)
-            return cnt
+Let me give you two examples of these two "search space"
 
-        left, right = matrix[0][0], matrix[-1][-1]
-        ans = -1
-        while left <= right:
-            mid = (left + right) // 2
-            if countLessOrEqual(mid) >= k:
-                ans = mid
-                right = mid - 1  # try to looking for a smaller value in the left side
-            else:
-                left = mid + 1  # try to looking for a bigger value in the right side
+index -- A bunch of examples -- https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/ ( the array is sorted)
+range -- https://leetcode.com/problems/find-the-duplicate-number/ (Unsorted Array)
+The reason why we did not use index as "search space" for this problem is the matrix is sorted in two directions, we can not find a linear way 
+to map the number and its index.
 
-        return ans    
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1;//[lo, hi)
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0,  j = matrix[0].length - 1;
+            for(int i = 0; i < matrix.length; i++) {
+                while(j >= 0 && matrix[i][j] > mid) j--;
+                count += (j + 1);
+            }
+            if(count < k) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
