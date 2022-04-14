0e6bde0e0b97df67eb61b2e765b2dacfb3d7575b
Q2016 <k.sohraby@gmail.com>
2022-04-13 17:20:14 -0700

    Update 424. Longest Repeating Character Replacement.py

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -8,8 +8,39 @@ Input: s = "ABAB", k = 2, Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.    
     
     
+    
+    
+    
+    
+    
+    
+    
+    
 Solution: Sliding window 
 
+    
+There's no edge case for this question. The initial step is to extend the window to its limit, that is, the longest we can get to 
+with maximum number of modifications. Until then the variable start will remain at 0.
+Then as end increase, the whole substring from 0 to end will violate the rule, so we need to update start accordingly (slide the window). 
+We move start to the right until the whole string satisfy the constraint again. Then each time we reach such situation, we update our max length. 
+
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+   
+    
+    
     def characterReplacement(self, s, k):
         counts = collections.Counter()
         start = res = 0
