0852b82b30cd346233337d9d783f9f043a1facd4
Q2016 <k.sohraby@gmail.com>
2022-04-13 18:01:00 -0700

    Update 43. Multiply Strings.py

--- 43. Multiply Strings.py
+++ 43. Multiply Strings.py
@@ -12,66 +12,47 @@ For the animation: https://leetcode.com/problems/multiply-strings/solution/
 
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == "0" or num2 == "0": 
+        if num1 == "0" or num2 == "0":
             return "0"
         
-        # Reverse both numbers.
+        # Initialize answer as a string of zeros of length N.
+        N = len(num1) + len(num2)
+        answer = [0] * N
+        
+        # Reverse num1 and num2
         first_number = num1[::-1]
         second_number = num2[::-1]
         
-        # For each digit in second_number, multipy the digit by first_number and then
-        # store the multiplication result (reversed) in the results array.
-        results = []
-        for index, digit in enumerate(second_number):
-            results.append(self.multiply_one_digit(digit, index, first_number))
+        for place2, digit2 in enumerate(second_number):
+            # For each digit in second_number multiply the digit by all digits in first_number.
+            for place1, digit1 in enumerate(first_number):
+                # The number of zeros from multiplying to digits depends on the place
+                # of digit2 in second_number and the place of the digit1 in first_number.
+                num_zeros = place1 + place2
+                
+                # The digit currently at position numZeros in the answer string
+                # is carried over and summed with the current result.
+                carry = answer[num_zeros]
+                multiplication = int(digit1) * int(digit2) + carry
+                
+                # Set the ones place of the multiplication result.
+                answer[num_zeros] = multiplication % 10
+                
+                # Carry the tens place of the multiplication result by 
+                # adding it to the next position in the answer array.
+                answer[num_zeros + 1] += multiplication // 10
         
-        # Add all of the results together to get our final answer (in reverse order)
-        answer = self.sum_results(results)
-
-        # Reverse answer and join the digits to get the final answer.
+        # Pop the excess 0 from the end of answer.
+        if answer[-1] == 0:
+            answer.pop()
+            
         return ''.join(str(digit) for digit in reversed(answer))
+      
+      
+      
+Complexity Analysis
 
-    def multiply_one_digit(self, digit2: str, num_zeros: int, first_number: List[str]) -> List[int]:
-        """Multiplies first_number by a digit from second_number (digit2)."""
-        # Insert zeros at the beginning of the current result based on the current digit's place.
-        current_result = [0] * num_zeros
-        carry = 0
-
-        # Multiply each digit in first_number with the current digit of the second_number.
-        for digit1 in first_number:
-            multiplication = int(digit1) * int(digit2) + carry
-            # Set carry equal to the tens place digit of multiplication.
-            carry = multiplication // 10
-            # Append last digit to the current result.
-            current_result.append(multiplication % 10)
-
-        if carry != 0:
-            current_result.append(carry)
-        return current_result
-    
-    def sum_results(self, results: List[List[int]]) -> List[int]:
-        # Initialize answer as a number from results.
-        answer = results.pop()
-
-        # Add each result to answer one at a time.
-        for result in results:
-            new_answer = []
-            carry = 0
-
-            # Sum each digit from answer and result. Note: zip_longest is the
-            # same as zip, except that it pads the shorter list with fillvalue.
-            for digit1, digit2 in zip_longest(result, answer, fillvalue=0):
-                # Add current digit from both numbers.
-                curr_sum = digit1 + digit2 + carry
-                # Set carry equal to the tens place digit of curr_sum.
-                carry = curr_sum // 10
-                # Append the ones place digit of curr_sum to the new answer.
-                new_answer.append(curr_sum % 10)
-
-            if carry != 0:
-                new_answer.append(carry)
-
-            # Update answer to new_answer which equals answer + result
-            answer = new_answer
-
-        return answer
+Here N and M are the number of digits in num1 and num2 respectively.
+Time complexity: O(M⋅N).
+During multiplication, we perform N operations for each of the M digits of the second number, so we need M⋅N time for it.
+Space complexity: O(M+N).      
