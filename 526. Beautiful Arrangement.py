5de11457543981df1667780af5872cba68bc23d2
Q2016 <k.sohraby@gmail.com>
2022-04-19 11:19:19 -0700

    Update 526. Beautiful Arrangement.py

--- 526. Beautiful Arrangement.py
+++ 526. Beautiful Arrangement.py
@@ -12,23 +12,44 @@ The first beautiful arrangement is [1,2]: - perm[1] = 1 is divisible by i = 1   
 The second beautiful arrangement is [2,1]: - perm[1] = 2 is divisible by i = 1    - i = 2 is divisible by perm[2] = 1    
 
 
+   
+   
+   
+   
 Solution: Backtracking
+The idea behind this approach is simple. We try to create all the permutations of numbers from 1 to N. We can fix one number at a particular 
+position and check for the divisibility criteria of that number at the particular position. But, we need to keep a track of the numbers which 
+have already been considered earlier so that they aren't reconsidered while generating the permutations. If the current number doesn't satisfy 
+the divisibility criteria, we can leave all the permutations that can be generated with that number at the particular position. This helps to 
+prune the search space of the permutations to a great extent. We do so by trying to place each of the numbers at each position.
 
-   int count=0;
-    
-   def countArrangement(int N): 
-        if (N == 0) return 0;
-        used=(N+1)*[0];
-        helper(N, 1, used);
+We make use of a visited array of size N. Here, visited[i] refers to the i^{th} number being already placed/not placed in the array being formed 
+till now(True indicates that the number has already been placed).
+
+We make use of a calculate function, which puts all the numbers pending numbers from 1 to N(i.e. not placed till now in the array), indicated 
+by a FalseFalse at the corresponding visited[i] position, and tries to create all the permutations with those numbers starting from the pospos 
+index onwards in the current array. While putting the pos^{th} number, we check whether the i^{th} satisfies the divisibility criteria on the go 
+i.e. we continue forward with creating the permutations with the number ii at the pos^{th} position only if the number ii and pospos satisfy 
+the given criteria. Otherwise, we continue with putting the next numbers at the same position and keep on generating the permutations.
+
+
+
+public class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        boolean[] visited = new boolean[N + 1];
+        calculate(N, 1, visited);
         return count;
-    
-   def helper(N, pos, used):
-        if (pos > N):
-            count++
-            return
-        for (int i = 1; i <= N; i++):
-            if (used[i] == 0 && (i % pos == 0 || pos % i == 0)) :
-                used[i] = 1;
-                helper(N, pos + 1, used);
-                used[i] = 0;
-
+    }
+    public void calculate(int N, int pos, boolean[] visited) {
+        if (pos > N)
+            count++;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+                visited[i] = true;
+                calculate(N, pos + 1, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
