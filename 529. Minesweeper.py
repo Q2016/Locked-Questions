84ec3d3f4f2a06027260515ea38cc0875caf6606
Q2016 <k.sohraby@gmail.com>
2022-04-19 15:17:18 -0700

    Update 529. Minesweeper.py

--- 529. Minesweeper.py
+++ 529. Minesweeper.py
@@ -12,6 +12,17 @@ unrevealed squares should be revealed recursively.
 If an empty square 'E' with at least one adjacent mine is revealed, then change it to a digit ('1' to '8') representing the number of adjacent mines.
 Return the board when no more squares will be revealed.    
 
+Eample:
+Input: board = [["B","1","E","1","B"],["B","1","M","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]], click = [1,2]
+Output: [["B","1","E","1","B"],["B","1","X","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]]    
+
+
+    
+    
+    
+
+
+
 Solution: DFS
 
     def updateBoard(board, click) :
