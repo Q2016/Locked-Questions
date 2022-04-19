cdf4da69b52b13e15ae112ffabc85ed80848179d
Q2016 <k.sohraby@gmail.com>
2022-04-19 07:59:21 -0700

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -1,8 +1,25 @@
 Question:
 Implement pow(x, n), which calculates x raised to the power n.    
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Solution: Recursive, Iterative
 
+If we have very small value of x we can directly return 0, the smallest value of float is 1.175494 × 10^(-38).    
+    
 Recursive:
 
     def myPow(self, x, n):
@@ -27,3 +44,8 @@ Iterative:
             x *= x
             n >>= 1
         return pow
+    
+since you're dividing N by 2 every iteration that's why it is O(Log N). However, since N is a fixed number of bits (32) you could 
+view it as O(1) where the maximum number iterations is 32. Hope this helps.
+
+
