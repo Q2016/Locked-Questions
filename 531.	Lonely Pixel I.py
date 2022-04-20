c4775823c603ce4b490793f57b0dcf11061f01ba
Q2016 <k.sohraby@gmail.com>
2022-04-19 19:20:18 -0700

    Update 531.	Lonely Pixel I.py

--- 531.	Lonely Pixel I.py
+++ 531.	Lonely Pixel I.py
@@ -37,5 +37,53 @@ Two passes of the matrix. First pass: build the auxiliary table. Second pass: do
 # Time:  O(m * n)
 # Space: O(m + n)   
 
+
+
+First pass: does not create the auxiliary table, instead, store the auxiliary table’s info into the 1st row and 1st col.
+Second pass: do the counting.
+Note: the chr(int) will wrap round when it reaches 255, so we need to set a ceiling of ‘Z’ to avoid that.
+
+
+public class Solution {
+    
+    public int findLonelyPixel(char[][] picture) {
+        int firstColCount = 0;
+        int ret = 0;
+        for (int i=0; i<picture.length; i++) {
+            for (int j=0; j<picture[0].length; j++) {
+                if (picture[i][j] == 'B') {
+                    if (j == 0) {
+                        firstColCount++;
+                        if (picture[0][j] != 'Z') picture[i][0]++;
+                    } 
+                    else {
+                        if (picture[0][j] != 'Z') picture[0][j]++; # use 1st row to store "B" count for that col
+                        if (picture[i][0] != 'Z') picture[i][0]++; # use 1st col to store "B" count for that row
+                    }
+                }
+            }
+        }
+            
+        for (int i=0; i<picture.length; i++) {
+            if (picture[i][0]=='X' || picture[i][0]=='C') {
+                for (int j=0; j<picture[0].length; j++){
+                    if (picture[i][j] == 'B' || picture[i][j] == 'C') {
+                        if (j == 0) {
+                            if (firstColCount==1)  ret++;
+                        }
+                        else{
+                            if ((picture[0][j]=='X' || picture[0][j]=='C')) ret++;
+                        }        
+                    }    
+                }   
+            }
+        }
+            
+        return ret;
+            
+    }
+}
       
-      
+Time: O(n) (n = num of elements in matrix)
+Space: O(1)
+ 
