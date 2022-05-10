67288e994df6d1bc83f8e9bc61f5a9db59759ab5
Q2016 <k.sohraby@gmail.com>
2022-05-10 07:55:52 -0700

    Update 554. Brick Wall.py

--- 554. Brick Wall.py
+++ 554. Brick Wall.py
@@ -14,21 +14,54 @@ Input:      Output: 2
 [3,1,2],
 [1,3,1,1]]
 
+
+
+
+
+
+
+
 Solution: Hashmap
+    
+For the picture of the problem, look at the leetcode.    
+Number of Bricks Crossed by Line = Number of Rows in Wall - Frequency of Most Occuring Edge
+    
+    
+    
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) 
+    {
+        unordered_map<int, int> edge_frequency;     //HashMap to store the number of common edges among the rows
 
-    def leastBricks(self, wall):
-
-        widths = collections.defaultdict(int)
-        result = len(wall)
-        for row in wall:
-            width = 0
-            for i in xrange(len(row)-1):
-                width += row[i]
-                widths[width] += 1
-                result = min(result, len(wall) - widths[width]);
-        return result
+        int max_frequency = 0;         //Variable to store the frequency of most occuring edge
+        
+        for(int row=0; row<wall.size(); row++)        //Iterating through each row
+        {
+            int edge_postion = 0;       //Variable to store different edge postion
+            
+            for(int brick_no=0; brick_no< wall[row].size() -1; brick_no++)    //Iterating through each brick inside a row
+            { 
+                int current_brick_length = wall[row][brick_no];  //Length of the current brick
+                
+                edge_postion = edge_postion + current_brick_length ;  //Next Edge Position = Previous Edge Position + Current Brick's Length
+                
+                edge_frequency[edge_postion]++;  //Incrementing the Frequency of just calculated Edge Postion
+                
+                max_frequency = max(edge_frequency[edge_postion],max_frequency);  //Comparing the "Frequency of just calculated Edge Postion" with "Max Frequency seen till now" & storing whichever is greater.
+            }
+        }
+        return wall.size() - max_frequency; // returning (Number of Bricks Crossed by Line) i.e. (Number of Rows in Wall - Frequency of Most Occuring Edge) 
+    }
+};
 
     
     
-# Time:  O(n), n is the total number of the bricks
-# Space: O(m), m is the total number different widths    
+Time Complexity : O(N * M),
+where N = hieght of the wall OR number of rows in wall
+where M = approx width of each row OR approx number of bricks in each row
+where N * M = Total number of bricks in the wall
+
+
+Space Complexity : O(M),
+where M = Approx number of bricks in each row    
