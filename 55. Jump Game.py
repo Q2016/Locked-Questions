9933339701d7ae7764a2ba919a30b148f36da2a8
Q2016 <k.sohraby@gmail.com>
2022-05-10 06:45:44 -0700

    Update 55. Jump Game.py

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -7,14 +7,26 @@ Input: nums = [2,3,1,1,4]
 Output: true
 Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.    
 
-Solution:
-I just iterate and update the maximal index that I can reach
 
-def canJump(A, n) :
-    i = 0
-    reach=0
-    while i < n and i <= reach:
-        reach = max(i + A[i], reach)
-        i+=1
-    return i == n
+    
+    
+    
+    
+    
+    
+    
+    
+Solution: Greedy
+Looking from the start and selecting the locally optimum in the hope of reaching global optimum    
+    
+    bool canJump(vector<int>& nums) {
+      int n = nums.size(), farest = 0;
+      for(int i = 0;i < n; i++)
+      {
+        if(farest < i) return false;
+        farest = max(i + nums[i], farest);
+      }
+      
+      return true;
+    }
 
