b0d27ba7061c04d1c3328e33ebef3362262328d4
Q2016 <k.sohraby@gmail.com>
2022-05-12 12:24:20 -0700

    Update 698. Partition to K Equal Sum Subsets.py

--- 698. Partition to K Equal Sum Subsets.py
+++ 698. Partition to K Equal Sum Subsets.py
@@ -7,17 +7,28 @@ Output: true
 Explanation: It is possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
 
  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 Solution: Naive Backtracking
 Our goal is to break the given array into k subsets of equal sums. Firstly, we will check if the array sum can be evenly divided into k 
-parts by ensuring that totalArraySum % k is equal to 0. Now, if the array sum can be evenly divided into k parts, as previously mentioned, 
-we will try to build those k subsets using backtracking. We will keep a currentSum variable denoting the sum of the current subset. One by one, 
-try to include each element from the array that has not already been picked and then make a recursive call to pick the next element.
+parts by ensuring that totalArraySum % k is equal to 0. 
+Now, if the array sum can be evenly divided into k parts, as previously mentioned, 
+we will try to build those k subsets using backtracking. 
+We will keep a currentSum variable denoting the sum of the current subset. One by one, try to include each element from the array that has not 
+already been picked and then make a recursive call to pick the next element.
 To keep track of already picked elements we will use a vector (taken) to denote if the element at the ith index has already been picked or not.
 When we pick the ith element, we will set taken[i] equal to true. Then after we try all combinations, we will backtrack and discard the picked 
-element by setting taken[i] equal to false so that it can be picked in a future recursive call. If we reach the condition currentSum is greater 
-than targetSum, then we cannot reach the target by adding more elements to the subset, so there is no need to proceed further; we can just 
-backtrack from here. If we reach the condition, currentSum equals targetSum, that means we made one subset with the target sum. So 
-now we can increment a count variable that counts how many subsets with a sum equal to the target we have made from our array.
+element by setting taken[i] equal to false so that it can be picked in a future recursive call. 
+If we reach the condition currentSum is greater than targetSum, then we cannot reach the target by adding more elements to the subset, so there 
+is no need to proceed further; we can just backtrack from here. 
+If we reach the condition, currentSum equals targetSum, that means we made one subset with the target sum. So now we can increment a count variable 
+that counts how many subsets with a sum equal to the target we have made from our array.
 When count becomes k, that means we have made k equal sum subsets of our array; hence we can return true.
 Finally, when count becomes k - 1, that means we have k equal sum subsets in our array because the totalArraySum 
 is divisible by k and the sum of k - 1 subsets will be (k - 1) * targetSum, hence the last subset-sum must also equal 
