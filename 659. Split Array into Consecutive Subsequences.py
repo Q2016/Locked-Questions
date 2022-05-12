cbcd4c4b8e115541f5457889f6ab9afdb2e94f05
Q2016 <k.sohraby@gmail.com>
2022-05-12 09:00:19 -0700

    Update 659. Split Array into Consecutive Subsequences.py

--- 659. Split Array into Consecutive Subsequences.py
+++ 659. Split Array into Consecutive Subsequences.py
@@ -8,80 +8,90 @@ Input: nums = [1,2,3,3,4,5], Output: true
 Explanation: nums can be split into the following subsequences:
 [1,2,3,3,4,5] --> 1, 2, 3 and 3, 4, 5  
 
-//
-// How to solve?
-//
-// (e.g.) create sequence from (3,4,5,6,7), (5,6,7), (6,7,8,9,10)
-//
-//        -> 3, 4, 5, 5, 6, 6, 6, 7, 7, 7, 8, 9, 10
-//
-//        visually we are solving
-//
-//                    *  *
-//                 *  *  *
-//           *  *  *  *  *  *  *  *
-//           3  4  5  6  7  8  9  10
-//
-// we are creating open ended sequence as scanning through 3 to 10
-//
-// At 3, there is no open sequence so [3) will be created
-//
-// At 4, #(4) == #(open sequence)
-//   so don't close the open sequences, just extends them
-//    [3 4)
-//
-// At 5, #(5) > #(open sequences)  : (2 > 1)
-//   so we extends existing open sequences, plus new sequence from '5'
-//     [3 4 5)
-//         [5)
-//
-// At 6, same situation as at '5',
-//     [3 4 5 6)
-//         [5 6)
-//           [6)
-//
-// At 7, same as at 4 #(7) == #(seq)
-//
-//     [3 4 5 6 7)
-//         [5 6 7)
-//           [6 7)
-//
-// At 8, #(8) < #(seq) so we must close sequences if we can
-//   (if not return false)
-//
-//     [3 4 5 6 7] --> done
-//         [5 6 7] --> done
-//           [6 7 8)
-//
-// At 9 and 10, we are just extending sequences
-//           [6 7 8 9 10)
-//
-// At the end, if we can close all open sequences, return 'true'
-// If not return 'false'
-//
-              
-              
-class Solution:
-    def isPossible(self, nums: List[int]) -> bool:
-        ncount = dict()
-        for num in nums:
-            if num not in ncount:
-                ncount[num] = 0
-            ncount[num] += 1
+
+
+
+
+
+
+
+Solution: DP
+  
+class Solution {
+public:
+    bool isPossible(vector<int> &nums) {
+        int n = nums.size();
+        int start = 0;
+
+        for (int i = 1; i < n; i++) {
+            // Check possibility of a valid segment starting at index start and ending at index i - 1.
+            if (nums[i] - nums[i - 1] > 1) {
+                if (!isSegmentValid(nums, start, i - 1)) {
+                    return false;
+                }
+                // Update the starting index of the next segment.
+                start = i;
+            }
+        }
+        // Check for the last segment
+        return isSegmentValid(nums, start, n - 1);
+    }
+    
+private:
+    bool isSegmentValid(vector<int> &nums, int start, int end) {
+        int noOfUniqueNumbers = nums[end] - nums[start] + 1;
+
+        // Count frequency of each number in the current segment.
+        vector<int> frequency(noOfUniqueNumbers);
+        
+        for (int i = start; i <= end; i++) {
+            frequency[nums[i] - nums[start]]++;
+        }
+        // lengthOneSubsequences[i] holds count of subsequences of length 1 ending with index i
+        vector<int> lengthOneSubsequences(noOfUniqueNumbers);
+
+        // lengthTwoSubsequences[i] holds count of subsequences of length 2 ending with index i
+        vector<int> lengthTwoSubsequences(noOfUniqueNumbers);
+
+        // totalNoOfSubsequences[i] holds count of all subsequences ending with index i
+        vector<int> totalNoOfSubsequences(noOfUniqueNumbers);
+
+        lengthOneSubsequences[0] = totalNoOfSubsequences[0] = frequency[0];
+
+        for (int i = 1; i < noOfUniqueNumbers; i++) {
+
+            // If the frequency[i] is less than total number of subsequences ending with i - 1,
+            // we do not have enough subsequences where we can put i.
+            if (frequency[i] < lengthOneSubsequences[i - 1] + lengthTwoSubsequences[i - 1]) {
+                return false;
+            }
             
-        sequences = []
-        for num in sorted(ncount.keys()):
-            count = ncount[num]
-            s = len(sequences)-1
-            for _ in range(count):
-                if s>= 0 and sequences[s][-1] + 1 == num:
-                    sequences[s].append(num)
-                    s -= 1
-                else:
-                    sequences.append([num])
-                        
-        for sequence in sequences:
-            if len(sequence) < 3:
-                return False
+            //Total number of subsequences of length 2 can be obtained by adding i 
+            //to subsequences of length 1 ending with i - 1.
+            lengthTwoSubsequences[i] = lengthOneSubsequences[i - 1];
             
-        return True              
+            // For the remaining i valued numbers we can either add them to an existing subsequence
+            // or create a new one. We first try to add them to the existing subsequences ending 
+            // with i - 1. If there are not enough of such subsequences, we start a new subsequence.
+            // The existing subsequences ending with i - 1 is denoted by totalNoOfSubsequences[i - 1];
+            lengthOneSubsequences[i] = max(0, frequency[i] - totalNoOfSubsequences[i - 1]);
+            totalNoOfSubsequences[i] = frequency[i];
+        }
+
+        // If there is no remaining subsequence of length one or two, we can return true. 
+        // Otherwise, return false.
+        return lengthOneSubsequences[noOfUniqueNumbers - 1] == 0 && 
+               lengthTwoSubsequences[noOfUniqueNumbers - 1] == 0;
+    }
+};  
+
+
+Complexity Analysis
+
+Time complexity: O(N)
+At one glance, it might look that the isSegmentValid function takes O(N) time. However, if you look at the value of noOfUniqueNumbers, 
+you will realize that the for loop in the isSegmentValid function iterates over each unique element in the nums array only once. 
+For the given array of size N there can be at max N unique elements. Thus the overall time complexity is O(N).
+
+Space complexity: O(N)
+In the isSegmentValid function four arrays of size at max N is used. So the space complexity is O(N * 4) = O(N) in Big O notation.
