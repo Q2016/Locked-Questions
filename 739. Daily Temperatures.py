ac8a2549f13d5e411f80dcf31750578b3eafea67
Q2016 <k.sohraby@gmail.com>
2022-05-13 09:40:45 -0700

    Update 739. Daily Temperatures.py

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -9,22 +9,37 @@ Output: [1,1,4,2,1,1,0,0]
 
     
 Solution:
-lots of different approaches:
-https://leetcode.com/problems/daily-temperatures/solution/
+    
+Monotonic Stack: A monotonic stack is simply a stack where the elements are always in sorted order.    
 
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+        
+        for curr_day, curr_temp in enumerate(temperatures):
+            # Pop until the current day's temperature is not
+            # warmer than the temperature at the top of the stack
+            while stack and temperatures[stack[-1]] < curr_temp:
+                prev_day = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+        
+        return answer
 
-        result = [0] * len(temperatures)
-        stk = []
-        for i in xrange(len(temperatures)):
-            while stk and \
-                  temperatures[stk[-1]] < temperatures[i]:
-                idx = stk.pop()
-                result[idx] = i-idx
-            stk.append(i)
-        return result 
+Time complexity: O(N)
 
+At first glance, it may look like the time complexity of this algorithm should be O(N^2), because there is a nested while loop inside the for loop. 
+However, each element can only be added to the stack once, which means the stack is limited to N pops. Every iteration of the while loop uses 1 pop, 
+which means the while loop will not iterate more than N times in total, across all iterations of the for loop.
+An easier way to think about this is that in the worst case, every element will be pushed and popped once. This gives a time complexity of 
+O(2N) = O(N).
+
+Space complexity: O(N)
+If the input was non-increasing, then no element would ever be popped from the stack, and the stack would grow to a size of N elements at the end.    
+    
+    
     
 Time:  O(n)
 Space: O(n)    
