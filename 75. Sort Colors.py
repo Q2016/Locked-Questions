8df0e6e4bc2e4374d65b3a6c1e55dd34e416568c
Q2016 <k.sohraby@gmail.com>
2022-05-13 11:40:51 -0700

    Update 75. Sort Colors.py

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -8,27 +8,61 @@ Input: nums = [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
 
     
-Solution: O(n) 1 pass in-place solution with explanation
-
-This is a dutch partitioning problem. We are classifying the array into four groups: 
-red, white, unclassified, and blue. Initially we group all elements into unclassified. 
-We iterate from the beginning as long as the white pointer is less than the blue pointer.
-
-If the white pointer is red (nums[white] == 0), we swap with the red pointer and move both 
-white and red pointer forward. If the pointer is white (nums[white] == 1), the element is 
-already in correct place, so we don't have to swap, just move the white pointer forward. 
-If the white pointer is blue, we swap with the latest unclassified element.
-
-def sortColors(self, nums):
-    red, white, blue = 0, 0, len(nums)-1
     
-    while white <= blue:
-        if nums[white] == 0:
-            nums[red], nums[white] = nums[white], nums[red]
-            white += 1
-            red += 1
-        elif nums[white] == 1:
-            white += 1
+    
+    
+    
+    
+    
+    
+    
+Solution: Dutch Partitioning
+
+https://en.wikipedia.org/wiki/Dutch_national_flag_problem    
+    
+    
+Pseudocode
+
+The following pseudocode for three-way partitioning which assumes zero-based array indexing was proposed by Dijkstra himself
+It uses three indices i, j and k, maintaining the invariant that i ≤ j ≤ k.
+
+Entries from 0 up to (but not including) i are values less than mid,
+entries from i up to (but not including) j are values equal to mid,
+entries from j up to (and including) k are values not yet sorted, and
+entries from k + 1 to the end of the array are values greater than mid.
+procedure three-way-partition(A : array of values, mid : value):
+    i ← 0
+    j ← 0
+    k ← size of A - 1
+
+    while j <= k:
+        if A[j] < mid:
+            swap A[i] and A[j]
+            i ← i + 1
+            j ← j + 1
+        else if A[j] > mid:
+            swap A[j] and A[k]
+            k ← k - 1
         else:
-            nums[white], nums[blue] = nums[blue], nums[white]
-            blue -= 1
+            j ← j + 1
+
+
+
+class Solution:
+    def sortColors(self, nums):
+        
+        p1, p2 = 0, len(nums) - 1
+        p = 0
+        while p <= p2:
+            if nums[p] < 1:
+                nums[p], nums[p1] = nums[p1], nums[p]
+                p1 += 1
+                p += 1
+            elif nums[p] > 1:
+                nums[p], nums[p2] = nums[p2], nums[p]
+                p2 -= 1
+            else:
+                p += 1
+
+Time: O(n) 
+Space: in-place                
