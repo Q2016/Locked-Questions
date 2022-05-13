3513ba91356b8081d6fbc2ebbce09eb39d9c1c6d
Q2016 <k.sohraby@gmail.com>
2022-05-12 17:04:32 -0700

    Update 713. Subarray Product Less Than K.py

--- 713. Subarray Product Less Than K.py
+++ 713. Subarray Product Less Than K.py
@@ -9,6 +9,12 @@ Explanation: The 8 subarrays that have product less than 100 are:
 Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.    
 
 
+
+
+
+
+
+
 Solution: Sliding window
 For each right, call opt(right) the smallest left so that the product of the subarray nums[left] * nums[left + 1] * ... * nums[right] 
 is less than k. opt is a monotone increasing function, so we can use a sliding window.
