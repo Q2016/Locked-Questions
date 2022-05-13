6f09e2b129d59f273a498d8fe6d8ca9079471aea
Q2016 <k.sohraby@gmail.com>
2022-05-13 09:24:28 -0700

    Update 737. Sentence Similarity II.py

--- 737. Sentence Similarity II.py
+++ 737. Sentence Similarity II.py
@@ -1,7 +1,7 @@
-https://medium.com/@rebeccahezhang/leetcode-737-sentence-similarity-ii-2ca213f10115
-
+Question:
 Given two sentences words1, words2 (each represented as an array of strings), 
 and a list of similar word pairs pairs, determine if two sentences are similar.
+
 For example, words1 = ["great", "acting", "skills"] and words2 = ["fine", "drama", "talent"] 
 are similar, if the similar word pairs are pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]].
 Note that the similarity relation is transitive. For example, if “great” and “good” are
@@ -12,11 +12,18 @@ Also, a word is always similar with itself. For example, the sentences words1 = 
 words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
 Finally, sentences can only be similar if they have the same number of words. So a sentence 
 like words1 = ["great"] can never be similar to words2 = ["doubleplus","good"].
-Note:
-The length of words1 and words2 will not exceed 1000.
-The length of pairs will not exceed 2000.
-The length of each pairs[i] will be 2.
-The length of each words[i] and pairs[i][j] will be in the range [1, 20].
+
+
+
+
+
+
+
+
+
+Solution: (The solution gives the general idea)
+https://medium.com/@rebeccahezhang/leetcode-737-sentence-similarity-ii-2ca213f10115
+    
 This is more complicated than Sentence Similarity I. To check if two words are similar 
 given the transitivity (for example If “A” = “B”, “B” = “C”, then “A” = “C”), we can use 
 a graph to help us connect all similar words together. Then for each word pairs, we start 
