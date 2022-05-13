e87f86e3d87ad592d56a3b9d189ba03ea2f491fb
Q2016 <k.sohraby@gmail.com>
2022-05-13 09:49:33 -0700

    Update 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -1,14 +1,24 @@
 Question:
-Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+Write an efficient algorithm that searches for a value target in an m x n integer matrix. This matrix has the following properties:
 Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
  
 Example 1:
-Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Input: matrix = [[ 1, 3, 5, 7],
+                 [10,11,16,20],
+                 [23,30,34,60]], target = 3
 Output: true    
 
     
-Solution:
+  
+  
+  
+  
+  
+  
+  
+  
+Solution: BST
 It is basically an advanced version of the binary search
 
 class Solution:
