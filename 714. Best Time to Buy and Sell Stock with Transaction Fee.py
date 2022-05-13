4d46a797aa5d8ee929b292aec0d1d0bf87a1037d
Q2016 <k.sohraby@gmail.com>
2022-05-12 17:30:05 -0700

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.py

--- 714. Best Time to Buy and Sell Stock with Transaction Fee.py
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -14,9 +14,12 @@ Explanation: The maximum profit can be achieved by:
 The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
 
 
+
+
+
 Solution : DP
 At the end of the i-th day, we maintain cash, the maximum profit we could have if we did not have a share of stock, and hold, 
-the maximum profit we could have if we owned a share of stock. To transition from the i-th day to the i+1-th day, we either 
+the maximum profit we could have if we owned a share of stock. To transition from the ith day to the i+1th day, we either 
 sell our stock cash = max(cash, hold + prices[i] - fee) or buy a stock hold = max(hold, cash - prices[i]). At the end, we want 
 to return cash. We can transform cash first without using temporary variables because selling and buying on the same day can't 
 be better than just continuing to hold the stock.
