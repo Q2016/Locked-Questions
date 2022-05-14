e3b293df27480062d56e9e49e1acda78c8d135ff
Q2016 <k.sohraby@gmail.com>
2022-05-13 19:41:36 -0700

    Update 769. Max Chunks To Make Sorted.py

--- 769. Max Chunks To Make Sorted.py
+++ 769. Max Chunks To Make Sorted.py
@@ -13,12 +13,26 @@ For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], wh
     
 Solution:    
     
-class Solution(object):
-    def maxChunksToSorted(self, arr):
+Explanation:
+Many solutions simply describe how to know when we can add a count to the total number of chunks (when max == index), but not why it works.
+We know the final position of the elements in the array: their sorted positions. Since the array consists of elements from [0, N-1], for any 
+value in the array, the corresponding index is where that element must end up in the end. This is how we will decide when we should chunk our elements.
 
-        result, max_i = 0, 0
-        for i, v in enumerate(arr):
-            max_i = max(max_i, v)
-            if max_i == i:
-                result += 1
-        return result
+We want to maximize the number of chunks in our array. But we just said for any value in the array, it must end up in its corresponding 
+index at the end. So the chunk we put that value in must cover at least that index, so that after sorting that chunk, it can be in that index.
+If we encounter an element v1 initially at index i1, where v1 > i1, then for the chunk containing element v1, it must at least contain index v1, 
+so that when we sort that chunk, v1 will move to index v1. Since we are still at i1, we cannot end the chunk for this element until we reach at 
+least v1. We should keep track of this minimum using a variable, say min_index_for_chunk_end.
+If we encounter an element v2 initially at index i2, where v2 < i2, then for the chunk containing element v2, it must contain at least index v2. 
+Since v2 was already encountered before this, we can consider ending the chunk here. However, we might have other elements in the chunk that have 
+a minimum index that is after i2. Luckily we kept track of the minimum index of all previous elements in the chunk using min_index_for_chunk_end. 
+We can only end the chunk here if min_index_for_chunk_end == i2.
+
+    def maxChunksToSorted(self, arr):
+        chunks = 0
+        min_index_for_chunk_end = arr[0]
+        for i in xrange(len(arr)):
+            min_index_for_chunk_end = max(min_index_for_chunk_end, arr[i])
+            if i == min_index_for_chunk_end:
+                chunks += 1
+        return chunks
