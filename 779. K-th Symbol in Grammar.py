6fa7b09ed855d394306ad23136ab2940c4011ad2
Q2016 <k.sohraby@gmail.com>
2022-05-15 13:59:36 -0700

    Update 779. K-th Symbol in Grammar.py

--- 779. K-th Symbol in Grammar.py
+++ 779. K-th Symbol in Grammar.py
@@ -5,13 +5,22 @@ For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 
 Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table of n rows.    
     
     
-Solution:    
-We don't need to actually generate the strings "0110..." (would be TLE error anyway)
-
 row 1: 0
 row 2: 01
 row 3: 0110
-row 4: 01101001
+row 4: 01101001    
+    
+    
+    
+    
+    
+    
+    
+    
+Solution: Recursive
+    
+We don't need to actually generate the strings "0110..." (would be TLE error anyway)
+
 We see that, for any level N, the first half of the string is the same as the string in N-1, the next half is just complement of it. 
 The total number of items in level N is 2^N. The half mark of the string is marked by [2^(N-1)]-th item. So, for any level N:
 
