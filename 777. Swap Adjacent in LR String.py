842c47ef32755fe4a4af36d642b193b4189e8b13
Q2016 <k.sohraby@gmail.com>
2022-05-15 13:43:23 -0700

    Update 777. Swap Adjacent in LR String.py

--- 777. Swap Adjacent in LR String.py
+++ 777. Swap Adjacent in LR String.py
@@ -7,30 +7,48 @@ Example 1:
 Input: start = "RXXLRXRXL", end = "XRLXXRRLX"
 Output: true
 Explanation: We can transform start to end following these steps:
-RXXLRXRXL ->
-XRXLRXRXL ->
-XRLXRXRXL ->
-XRLXXRRXL ->
-XRLXXRRLX    
+RXXLRXRXL ->XRXLRXRXL ->XRLXRXRXL ->XRLXXRRXL ->XRLXXRRLX    
     
     
 Solution:    
 
-class Solution(object):
-    def canTransform(self, start, end):
+Key observations:
+There are three kinds of characters, ‘L’, ‘R’, ‘X’.
+Replacing XL with LX = move L to the left by one
+Replacing RX with XR = move R to the right by one
+If we remove all the X in both strings, the resulting strings should be the same.
+
+Additional observations:
+Since a move always involves X, an L or R cannot move through another L or R.
+Since an L can only move to the right, for each occurrence of L in the start string, its position should be to the same or to the left of 
+its corresponding L in the end string.
+
+Implementation
+We first compare two strings with X removed. This checks relative position between Ls and Rs are correct.
+
+Then we find the indices for each occurence of L and check the condition in the above figure. Then we do the same for R.
+
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
         if len(start) != len(end): return False
         
-        A = [(s, idx) for idx, s in enumerate(start) if s == 'L' or s == 'R']
-        B = [(e, idx) for idx, e in enumerate(end) if e == 'L' or e == 'R']
-        if len(A) != len(B): return False
+        # check L R orders are the same
+        if start.replace('X','') != end.replace('X', ''): return False
         
-        for (s, i), (e, j) in zip(A, B):
-            if s != e: return False
-            if s == 'L':
-                if i < j:
-                    return False
-            if s == 'R':
-                if i > j:
-                    return False
+        n = len(start)
+        Lstart = [i for i in range(n) if start[i] == 'L']
+        Lend = [i for i in range(n) if end[i] == 'L']
+        
+        Rstart = [i for i in range(n) if start[i] == 'R']
+        Rend = [i for i in range(n) if end[i] == 'R']
+		# check L positions are correct
+        for i, j in zip(Lstart, Lend):
+            if i < j:
+                return False
+            
+        # check R positions are correct
+        for i, j in zip(Rstart, Rend):
+            if i > j:
+                return False
             
         return True
