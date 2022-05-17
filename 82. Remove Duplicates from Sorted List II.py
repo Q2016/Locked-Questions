dd6509ccaf968cf2aa95b8dc10d46dbc798448b3
Q2016 <k.sohraby@gmail.com>
2022-05-17 13:19:37 -0700

    Update 82. Remove Duplicates from Sorted List II.py

--- 82. Remove Duplicates from Sorted List II.py
+++ 82. Remove Duplicates from Sorted List II.py
@@ -12,7 +12,27 @@ Solution: Sentinel Head + Predecessor
 Solution from: with images
   https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/solution/
 
+Sentinel Head
 
+Let's start from the most challenging situation: the list head is to be removed.
+
+The standard way to handle this use case is to use the so-called Sentinel Node. Sentinel nodes are widely used for trees and linked 
+lists as pseudo-heads, pseudo-tails, etc. They are purely functional and usually don't hold any data. Their primary purpose is to 
+standardize the situation to avoid edge case handling.
+
+For example, let's use here pseudo-head with zero value to ensure that the situation "delete the list head" could never happen, and all 
+nodes to delete are "inside" the list.
+
+Delete Internal Nodes
+
+The input list is sorted, and we can determine if a node is a duplicate by comparing its value to the node after it in the list. Step by step, 
+we could identify the current sublist of duplicates.
+
+Now it's time to delete it using pointer manipulations. Note that the first node in the duplicates sublist should be removed as well. That 
+means that we have to track the predecessor of duplicates sublist, i.e., the last node before the sublist of duplicates.
+    
+    
+    
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         # sentinel
