69168883e7f09386be845b78a90c8422e0719f84
Q2016 <k.sohraby@gmail.com>
2022-05-17 22:28:20 -0700

    Update 863. All Nodes Distance K in Binary Tree.py

--- 863. All Nodes Distance K in Binary Tree.py
+++ 863. All Nodes Distance K in Binary Tree.py
@@ -3,6 +3,19 @@ Given the root of a binary tree, the value of a target node target, and an integ
 nodes that have a distance k from the target node. You can return the answer in any order.    
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 Solution:
 If we know the parent of every node x, we know all nodes that are distance 1 from x. We can then perform a breadth first search 
 from the target node to find the answer.
@@ -10,7 +23,9 @@ We first do a depth first search where we annotate every node with information a
 After, we do a breadth first search to find all nodes a distance K from the target    
 
 class Solution(object):
+    
     def distanceK(self, root, target, K):
+        
         def dfs(node, par = None):
             if node:
                 node.par = par
@@ -31,3 +46,9 @@ class Solution(object):
                     queue.append((nei, d+1))
 
         return []
+    
+    
+Complexity Analysis
+
+Time Complexity: O(N), where N is the number of nodes in the given tree.
+Space Complexity: O(N).    
