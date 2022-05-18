4be10ad3a81b69193a7962f8c94888ec3db87664
Q2016 <k.sohraby@gmail.com>
2022-05-18 11:39:34 -0700

    Update 890. Find and Replace Pattern.py

--- 890. Find and Replace Pattern.py
+++ 890. Find and Replace Pattern.py
@@ -1,13 +1,21 @@
 Question:
 Given a list of strings words and a string pattern, return a list of words[i] that match pattern. You may return the answer in any order.
-A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.
-Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter, and no two letters map to the same letter.
+A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we 
+get the desired word.
+Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter, and no two letters map to 
+the same letter.
 
 Example 1:
 Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
 Output: ["mee","aqq"]
 Explanation: "mee" matches the pattern because there is a permutation {a -> m, b -> e, ...}. 
 "ccc" does not match the pattern because {a -> c, b -> c, ...} is not a permutation, since a and b map to the same letter.
+
+
+
+
+
+
 
 
 Solution: Two Maps
@@ -20,6 +28,7 @@ is ("aa", "xy"), we will catch the mistake in m1("a")="x"="y").
 Similarly, with (word, pattern) = ("ab", "xx"), we will catch the mistake in m2.
 
 class Solution(object):
+    
     def findAndReplacePattern(self, words, pattern):
         def match(word):
             m1, m2 = {}, {}
