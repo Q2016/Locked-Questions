f191a1a7593ea87a1d6c3c709b848235c5a8fc85
Q2016 <k.sohraby@gmail.com>
2022-05-21 13:02:10 -0700

    Update 958. Check Completeness of a Binary Tree.py

--- 958. Check Completeness of a Binary Tree.py
+++ 958. Check Completeness of a Binary Tree.py
@@ -3,9 +3,21 @@ Given the root of a binary tree, determine if it is a complete binary tree. In a
 is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.    
 
 
-Solution:
+
+
+
+
+
+
+
+
+
+
+Solution:level-order traversal
+    
 The level-order traversal array of a complete binary tree will never have a null node in between non-null nodes. If we encounter a null node, 
 all the following nodes should also be null, otherwise it's not complete.
+
 
 class Solution:
     def isCompleteTree(self, root: TreeNode) -> bool:
@@ -22,3 +34,7 @@ class Solution:
             Q.append(cur_node.right)
             
         return True    
+
+    
+Time O(N) 
+Space O(N)    
