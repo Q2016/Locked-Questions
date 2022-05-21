5b8bdb050ec9776a368732498a98e83bfbe546de
Q2016 <k.sohraby@gmail.com>
2022-05-21 10:37:34 -0700

    Update 951. Flip Equivalent Binary Trees.py

--- 951. Flip Equivalent Binary Trees.py
+++ 951. Flip Equivalent Binary Trees.py
@@ -1,10 +1,23 @@
 Question:
+https://leetcode.com/problems/flip-equivalent-binary-trees/
+    
 For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
 A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
 Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.    
 
 
+
+
+
+
+
+
+
+
+
+
 Solution: Recursion
+    
 If root1 and root2 have the same root value, then we only need to check if their children are equal (up to ordering.)
 There are 3 cases:
 If root1 or root2 is null, then they are equivalent if and only if they are both null.
