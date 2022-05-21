1d786ba00bc78ad2b07197ae16b51c8bcf4733b1
Q2016 <k.sohraby@gmail.com>
2022-05-20 22:05:03 -0700

    Update 946. Validate Stack Sequences.py

--- 946. Validate Stack Sequences.py
+++ 946. Validate Stack Sequences.py
@@ -12,6 +12,12 @@ push(5),
 pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1    
     
     
+    
+    
+    
+    
+    
+    
 Solution: Greedy
 We have to push the items in order, so when do we pop them? If the stack has say, 2 at the top, then if we have to pop that value next, we must do it now. 
 That's because any subsequent push will make the top of the stack different from 2, and we will never be able to pop again.
