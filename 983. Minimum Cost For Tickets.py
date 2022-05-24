4927f96458615721d14f27ec07c0703cfdcd42c8
Q2016 <k.sohraby@gmail.com>
2022-05-23 17:37:05 -0700

    Update 983. Minimum Cost For Tickets.py

--- 983. Minimum Cost For Tickets.py
+++ 983. Minimum Cost For Tickets.py
@@ -10,31 +10,38 @@ Return the minimum number of dollars you need to travel every day in the given l
  
  
  
-Solution: Dynamic Programming (Window Variant)
+Solution: 
+ 
+Approach 1: Dynamic Programming (Day Variant)
 
-As in Approach 1, we only need to buy a travel pass on a day we intend to travel. Now, let dp(i) be the cost to travel from day days[i] to the end 
-of the plan. If say, j1 is the largest index such that days[j1] < days[i] + 1, j7 is the largest index such that days[j7] < days[i] + 7, and j30 
-is the largest index such that days[j30] < days[i] + 30, then we have:
-dp(i)=min(dp(j1)+costs[0],dp(j7)+costs[1],dp(j30)+costs[2])
+Intuition and Algorithm
+
+For each day, if you don't have to travel today, then it's strictly better to wait to buy a pass. If you have to travel today, you have up to 3 choices: 
+you must buy either a 1-day, 7-day, or 30-day pass.
+
+We can express those choices as a recursion and use dynamic programming. Let's say dp(i) is the cost to fulfill your travel plan from day i to the 
+end of the plan. Then, if you have to travel today, your cost is:
+
+dp(i) = min(dp(i+1)+costs[0], dp(i+7)+costs[1], dp(i+30)+costs[2])
 
 from functools import lru_cache
 
 class Solution:
     def mincostTickets(self, days, costs):
-        N = len(days)
+        dayset = set(days)
         durations = [1, 7, 30]
 
         @lru_cache(None)
-        def dp(i): # How much money to do days[i]+
-            if i >= N: return 0
+        def dp(i):
+            if i > 365:
+                return 0
+            elif i in dayset:
+                return min(dp(i + d) + c
+                           for c, d in zip(costs, durations))
+            else:
+                return dp(i + 1)
 
-            ans = float('inf')
-            j = i
-            for c, d in zip(costs, durations):
-                while j < N and days[j] < days[i] + d:
-                    j += 1
-                ans = min(ans, dp(j) + c)
-
-            return ans
-
-        return dp(0)
+        return dp(1)
+       
+       
+       
