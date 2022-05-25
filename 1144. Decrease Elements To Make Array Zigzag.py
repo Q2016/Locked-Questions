f7b9a70c11c80f06326f4077ebc7fdfcf94de9a2
Q2016 <k.sohraby@gmail.com>
2022-05-25 12:29:08 -0700

    Update 1144. Decrease Elements To Make Array Zigzag.py

--- 1144. Decrease Elements To Make Array Zigzag.py
+++ 1144. Decrease Elements To Make Array Zigzag.py
@@ -17,34 +17,135 @@ Explanation: We can decrease 2 to 0 or 3 to 1.
     
 Solution:    
 
+Approach 1: manipulating array    
+    
+    
+class Solution:
+    
+    def MovesToMakeZigZag(A):
+        n=len(A)
+        B=A.copy()
+        
+        for i in range(n):
+            if i+1<n: #Every even-indexed element is greater than adjacent elements, ie. A[0] > A[1] < A[2] > A[3] < A[4] > ...
+                if i%2==0
+                    if A[i]<=A[i+1]:
+                        diff=A[i+1]-A[i]
+                        even+=diff+1
+                        A[i+1]=A[i+1]-(diff+1)
+                else:
+                    if A[i]>=A[i+1]:
+                        diff=A[i]-A[i+1]
+                        even+=diff+1
+                        A[i]=A[i]-(diff+1)
+        for i in range(n):
+            if i+1<n: #every odd-indexed element is greater than adjacent elements, ie. A[0] < A[1] > A[2] < A[3] > A[4] < ...
+                if i%2==0
+                    if B[i]<=B[i+1]:
+                        diff=B[i+1]-B[i]
+                        even+=diff+1
+                        B[i+1]=B[i+1]-(diff+1)
+                else:
+                        if B[i]>=B[i+1]:
+                        diff=B[i]-B[i+1]
+                        even+=diff+1
+                        B[i]=B[i]-(diff+1)           
+    
+        return min(even, odd)
+    
 
 Complexity
 Time O(N) for one pass
-Space O(2) for two options
+Space O(N) for two options
 
 
-There are two possible ways the array can satisfy the zigzag requirement: 
-1: start with a zig (increasing) 
-2. start with zag (decreasing). 
-There is a loop which goes through the whole list of numbers, in each step we keep track of the number of subtractions needed 
-to satisfy the requiremnets for both possible ways mentioned before. We also keep track of the last modified number. 
+Approach 2:
+    
+public class Solution {
+    public int MovesToMakeZigzag(int[] A) {
+        int n = A.Length;
+        int even = 0;
+        int odd = 0;        
+        int first = -1;
+        int second = -1;
+        for(int i=0;i<n;i++)
+        {
+            if(i+1<n)
+            {   
+                if(first==-1 && second ==-1)
+                {
+                    first = A[i];
+                    second = A[i+1];
+                }
+                else
+                {
+                    second = A[i+1];
+                }
+                if(i%2==0)
+                {
+                    if(first<=second)
+                    {
+                        int diff = second-first;
+                        even += diff+1;
+                        second = second - (diff+1);
+                    }
+                }
+                else
+                {
+                    if(first>=second)
+                    {
+                        int diff = first-second;
+                        even += diff+1;
+                        first = first - (diff+1);
+                    }
+                } 
+            }
+            first = second;
+        }
+        //Console.WriteLine(string.Join(" ",A));
+        //odd
+        first = -1;
+        second = -1;
+        for(int i=0;i<n;i++)
+        {
+            if(i+1<n)
+            {
+                if(first==-1 && second ==-1)
+                {
+                    first = A[i];
+                    second = A[i+1];
+                }
+                else
+                {
+                    second = A[i+1];
+                }
+                if(i%2==0)
+                {
+                    if(first>=second)
+                    {
+                        int diff = first-second;
+                        odd += diff+1;
+                        first = first - (diff+1);
+                    }                    
+                }
+                else
+                {
+                    if(first<=second)
+                    {
+                        int diff = second-first;
+                        odd += diff+1;
+                        second = second - (diff+1);
+                    }
+                }
+            }
+            first = second;
+        }
+        //Console.WriteLine(string.Join(" ",B));
+        return Math.Min(even,odd);
+    }
+        
+    
+Complexity
+Time O(N) for one pass
+Space O(1) for two options
 
-class Solution(object):
-    def movesToMakeZigzag(self, nums):
-        
-        zig, zag = 0, 0
-        prev_zig, prev_zag = nums[0], nums[0]
-        
-        for i in range(1, len(nums)):
-            if i % 2 == 0:
-                zig += max(0, prev_zig - nums[i] + 1)
-                prev_zig = nums[i] 
-                zag += max(0, nums[i] - prev_zag + 1)
-                prev_zag = nums[i] - max(0, nums[i] - prev_zag + 1)
-            else:
-                zag += max(0, prev_zag - nums[i] + 1)
-                prev_zag = nums[i]
-                zig += max(0, nums[i] - prev_zig + 1)
-                prev_zig = nums[i] - max(0, nums[i] - prev_zig + 1)
-        
-        return min(zig, zag)
