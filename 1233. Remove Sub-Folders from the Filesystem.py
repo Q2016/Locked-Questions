d43fac0a773537314ccc3adbb0d1cca3133dbd04
Q2016 <k.sohraby@gmail.com>
2022-05-25 17:42:18 -0700

    Update 1233. Remove Sub-Folders from the Filesystem.py

--- 1233. Remove Sub-Folders from the Filesystem.py
+++ 1233. Remove Sub-Folders from the Filesystem.py
@@ -9,8 +9,53 @@ Explanation: Folders "/a/b/" is a subfolder of "/a" and "/c/d/e" is inside of fo
     
     
     
-Solution: Trie
+Solution: 
+ 
+Method 1: sort by length then put only parent into HashSet
 
+Sort folder by length;
+Check if the floder's parent fold in HashSet before adding it into the HashSet.
+Note: the part before any / is a parent.
+ 
+     def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort(key=lambda f: len(f))
+        seen = set()
+        for f in folder:
+            for i in range(2, len(f)):
+                if f[i] == '/' and f[: i] in seen:
+                    break
+            else:
+                seen.add(f)
+        return list(seen)
+ 
+Analysis
+Sort cost n * logn;
+Outer for loop run n times; inner for loop cost i in each iteration due to substring(0, i), that is, 2 + ... + m, which is O(m ^ 2);
+Therefore,
+
+Time: O(n * (logn + m ^ 2)), space: (n * m), where n = folder.length, m = average size of the strings in folder.
+  
+  
+  
+Compare the first two methods
+
+Generally speaking, m > logn,
+
+For method 2:
+O(n * m * logn)
+
+For method 1:
+O(n * (logn + m ^ 2)) = O(n * m ^ 2) > O(n * m * logn) - time complexity of method 1.
+
+Conclusion:
+
+Method 2 is more space efficent and is, generally speaking, faster than method 1;
+Method 1 is faster if folders' average size m < logn.  
+ 
+ 
+ 
+ 
+Method 2: Trie 
 
 Please see and vote for my solutions for
 208. Implement Trie (Prefix Tree)
