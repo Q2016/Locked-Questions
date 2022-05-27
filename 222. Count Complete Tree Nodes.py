55b03ab60f42230e29dd0d7f17dd017f89b902d6
Q2016 <k.sohraby@gmail.com>
2022-05-26 20:00:58 -0700

    Update 222. Count Complete Tree Nodes.py

--- 222. Count Complete Tree Nodes.py
+++ 222. Count Complete Tree Nodes.py
@@ -12,27 +12,55 @@ The O(n) solution is:
 But the problem is asking for less than O(n) so:
 
   
-Solution: i dont understand
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+Solution: 
 
   
+In a complete tree at level l we have 2^l leefs
+Number of total nodes will follow this patern: 
+level   # of nodes 
+    1->1
+    2->3
+    3->7
+    
+then we check the left and right depth, if they are equal the tree is full if not we break it to
+left tree and right tree and recursivly continue
+
+https://www.youtube.com/watch?v=i_r2uKbwHCU
+
 compare the depth between left sub tree and right sub tree.
 A, If it is equal, it means the left sub tree is a full binary tree
 B, It it is not , it means the right sub tree is a full binary tree
 
  class Solution:
-        def countNodes(self, root):
-            if not root:
-                return 0
-            leftDepth = self.getDepth(root.left)
-            rightDepth = self.getDepth(root.right)
-            if leftDepth == rightDepth:
-                return pow(2, leftDepth) + self.countNodes(root.right)
-            else:
-                return pow(2, rightDepth) + self.countNodes(root.left)
-    
-        def getDepth(self, root):
-            if not root:
-                return 0
-            return 1 + self.getDepth(root.left)
+      def countNodes(self, root):
+          if root==None: return 0
+          left, right=root, root
+          h_l, h_r=0, 0
+
+          while left !=None:
+              h_l +=1
+              left=left.left
+
+          while right !=None:
+              h_r +=1
+              right=right.right
+
+          if h_l==h_r: return (1<<h_l)-1
+
+          return 1+ self.countNodes(root.left)+self.countNodes(root.right)
+
         
-  
+time complexity:
+  https://www.youtube.com/watch?v=CvrPf1-flAA
+    
+O(log N *log N)    
