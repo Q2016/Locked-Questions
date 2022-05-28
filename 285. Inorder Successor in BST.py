8a4070b4966c46691b37f98570d213a2c1bd4744
Q2016 <k.sohraby@gmail.com>
2022-05-28 16:28:22 -0700

    Update 285. Inorder Successor in BST.py

--- 285. Inorder Successor in BST.py
+++ 285. Inorder Successor in BST.py
@@ -19,32 +19,34 @@ Input: root = [5,3,6,2,4,null,null,1], p = 6, Output: null
 1
 
 
-Solution: Inorder
+
+
+
+
+
+
+
+Solution: Inorder  (Left, Root, Right)
+
+read: Binary Search Trees_ BST Explained with Examples.pdf  
+  
 The in-order successor of a node is the left-most node in its right sub-tree, if exists. If not, then it is the root of the 
 smallest left sub-tree this node is in.
 
 
     def inorderSuccessor(self, root, p):
 
-        successor = None
-        if p.right is not None:
-            node = p.right
-            successor = node
-            while node.left is not None:
-                node = node.left
-                successor = node
-            return successor
-        else:
-            node = root
-            successor = None
-            while True:
-                if node is None:
-                    return None
-                if node == p:
-                    break
-                elif p.val < node.val:
-                    successor = node
-                    node = node.left
-                else:
-                    node = node.right
-        return successor
+        if root=None: 
+            return None
+        
+        cur=root
+        pre=None
+        
+        while cur !=None :
+            if cur.val > p.val:
+                pre=cur
+                cur=cur.left
+            else:
+                cur=cur.right
+            
+        return pre
