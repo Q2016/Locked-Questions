89a250302d35462a88d51c3de2616a026a395dee
Q2016 <k.sohraby@gmail.com>
2022-05-29 12:55:54 -0700

    Update 36. Valid Sudoku.py

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -4,8 +4,18 @@ Each row must contain the digits 1-9 without repetition. Each column must contai
 Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.    
 
 
+
+
+
+
+
+
+
+
+
+
 Solution:
-Visualization of the index k = i / 3 * 3 + j / 3 :
+Visualization of the index k = mod(i, 3) * 3 + mod(j, 3) for valid Sudoku :
 
 0  0  0 | 1  1  1 | 2  2  2
 0  0  0 | 1  1  1 | 2  2  2
@@ -21,15 +31,18 @@ Visualization of the index k = i / 3 * 3 + j / 3 :
 
     def isValidSudoku(board)
     
+        n=len(board)
+
+        
         used1[9][9] = {0} #check each row
         used2[9][9] = {0} #check each column
         used3[9][9] = {0} #check each sub-boxes
         
-        for i in range(len(board)):
+        for i in range(n):
             for j in range(len(board[i])):
                 if (board[i][j] != '.'):
                     num = int(board[i][j])
-                    k = i / 3 * 3 + j / 3 # This gives the index of the matrix below
+                    k = mod(i, 3) * 3 + mod(j, 3) # This gives the index of the matrix below
                     if (used1[i][num] or used2[j][num] or used3[k][num]):
                         return false
                     used1[i][num] = used2[j][num] = used3[k][num] = 1
