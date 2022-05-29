1f2367985c98700543efc3c66a63234352dab966
Q2016 <k.sohraby@gmail.com>
2022-05-29 12:28:07 -0700

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -3,12 +3,19 @@ There is an integer array nums sorted in ascending order (with distinct values).
 nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is 
 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at 
 pivot index 3 and become [4,5,6,7,0,1,2]. Given the array nums after the possible rotation and an integer target, return the index 
-of target if it is in nums, or -1 if it is not in nums. You must write an algorithm with O(log n) runtime complexity.
+of target if it is in nums, or -1 if it is not in nums. 
+You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4    
 
+    
+    
+    
+    
+    
+    
 
 Solution: BST
 Let's say nums looks like this: [12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
@@ -26,6 +33,7 @@ And then we can simply do ordinary binary search.
             return -1;
         lo = 0, 
         hi = len(nums)-1;
+        
         while (lo < hi):
             mid = lo + (hi - lo)/2;
             #target and mid are on the same side
