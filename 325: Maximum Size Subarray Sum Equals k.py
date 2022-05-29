e2ded5db3e3cf349da66df6bbcbb4f09d8dff904
Q2016 <k.sohraby@gmail.com>
2022-05-29 12:13:01 -0700

    Update 325: Maximum Size Subarray Sum Equals k.py

--- 325: Maximum Size Subarray Sum Equals k.py
+++ 325: Maximum Size Subarray Sum Equals k.py
@@ -1,11 +1,22 @@
 Question:
-Given an array nums and target value k, find the maximum length of a subarray that sums to k. If there isn’t one, return 0 instead.
+Given an array nums and target value k, find the maximum length of a subarray that sums to k. 
+If there isn’t one, return 0 instead.
 
 Example:
 Input: nums = [1, -1, 5, -2, 3], k = 3
 Output: 4
 
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 Solution:
 Record the sum at each index location i. If sum – k has been found before at index j, 
 that means the sum of nums[j: i] is equal to k (because the sum of nums[:i] is the sum and the sum of 
@@ -14,19 +25,23 @@ the difference between the current position and the sum – k position is the lo
   
   
     def maxSubArrayLen(self, nums, k):
-
-        sums = {}
-        cur_sum, max_len = 0, 0
-        for i in xrange(len(nums)):
-            cur_sum += nums[i]
-            if cur_sum == k:
-                max_len = i + 1
-            elif cur_sum - k in sums:
-                max_len = max(max_len, i - sums[cur_sum - k])
-            if cur_sum not in sums:
-                sums[cur_sum] = i  # Only keep the smallest index.
-        return max_len
+        
+        index_dict={0:-1}
+        
+        running_sum = 0
+        ans=0 #maximum length
+        
+        for i, num in enumerate(nums):
+            running_sum += num
+            
+            if running_sum - k in index_dict:
+                ans = max(ans, i - index_dict[running_sum - k])
+           
+            if running_sum not in index_dict:
+                index_dict[running_sum] = i  
+        
+        return ans
 
       
-# Time:  O(n)
-# Space: O(n)      
+Time:  O(n)
+Space: O(n)      
