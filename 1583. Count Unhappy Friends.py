f179cd70194fd99b77088ed19fb079d422b257b1
Q2016 <k.sohraby@gmail.com>
2022-06-20 19:47:35 -0700

    Create 1583. Count Unhappy Friends.py

---
+++ 1583. Count Unhappy Friends.py
@@ -0,0 +1,60 @@
+Question:
+You are given a list of preferences for n friends, where n is always even.
+For each person i, preferences[i] contains a list of friends sorted in the order of preference. In other words, a friend earlier in the list 
+is more preferred than a friend later in the list. Friends in each list are denoted by integers from 0 to n-1.
+
+All the friends are divided into pairs. The pairings are given in a list pairs, where pairs[i] = [xi, yi] denotes xi is paired with yi and yi 
+is paired with xi.
+
+However, this pairing may cause some of the friends to be unhappy. A friend x is unhappy if x is paired with y and there exists a friend u who 
+is paired with v but:
+
+x prefers u over y, and
+u prefers x over v.
+Return the number of unhappy friends.
+
+Example 1:
+Input: n = 4, preferences = [[1, 2, 3], [3, 2, 0], [3, 1, 0], [1, 2, 0]], pairs = [[0, 1], [2, 3]]
+Output: 2
+Explanation:
+Friend 1 is unhappy because:
+- 1 is paired with 0 but prefers 3 over 0, and
+- 3 prefers 1 over 2.
+Friend 3 is unhappy because:
+- 3 is paired with 2 but prefers 1 over 2, and
+- 1 prefers 3 over 0.
+Friends 0 and 2 are happy.  
+
+
+
+
+
+
+
+Solution:
+  
+Create dictionary using each friend as keys and a list of people they are closer to than the person they are paired with as values. 
+This can be done using index.
+
+Then use nested for loop to find when people are on each other's list.
+
+class Solution:
+    def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
+        
+        dd = {}
+        
+        for i,x in pairs:
+            dd[i] = preferences[i][:preferences[i].index(x)]
+            dd[x] = preferences[x][:preferences[x].index(i)]
+        
+        ans = 0
+            
+        for i in dd:
+            for x in dd[i]:
+                if i in dd[x]:
+                    ans += 1
+                    break
+        
+        return ans
+
+Time complexity: O(N^2)      
