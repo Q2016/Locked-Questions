1070aab8f6b04cbaec272b40f093fb0c5cedead2
Q2016 <k.sohraby@gmail.com>
2022-06-24 18:09:01 -0700

    Update 1011. Capacity To Ship Packages Within D Days.py

--- 1011. Capacity To Ship Packages Within D Days.py
+++ 1011. Capacity To Ship Packages Within D Days.py
@@ -1,8 +1,8 @@
 Question:
-A conveyor belt has packages that must be shipped from one port to another within days days.
+A conveyor belt has packages that must be shipped from one port to another within days.
 The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt. 
 We may not load more weight than the maximum weight capacity of the ship.
-Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within.
 
 Example 1:
 Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
