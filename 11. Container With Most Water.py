761d39cf44deeb412fd36891b14142043af6a471
Q2016 <k.sohraby@gmail.com>
2022-06-26 09:31:32 -0700

    Update 11. Container With Most Water.py

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -3,6 +3,10 @@ There are n vertical lines drawn such that the two endpoints of the ith line are
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 Return the maximum amount of water a container can store.    
 
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of 
+water (blue section) the container can contain is 49.
 
 
 
@@ -12,6 +16,9 @@ Return the maximum amount of water a container can store.
 
 
 
+
+If we could sort and keep the index of each hight, we can do it, would be NlogN
+Oh crap, actually two point works because the left and right pointers start from maximum distances already!
 
 Solution: Two pointers & Greedy
 
