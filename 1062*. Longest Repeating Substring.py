51ac7e91ae2e4cd49880682d39fea6c5b6b54112
Q2016 <k.sohraby@gmail.com>
2022-06-25 21:09:52 -0700

    Update and rename 1062. Longest Repeating Substring.py to 1062*. Longest Repeating Substring.py

--- 1062*. Longest Repeating Substring.py
+++ 1062*. Longest Repeating Substring.py
@@ -1,26 +1,24 @@
 Question:
 Given a string S, find out the length of the longest repeating substring(s). Return 0 if no repeating substring exists.
 
-
 Example 1:
-
-Input: "abcd"
-Output: 0
-Explanation: There is no repeating substring.
-Example 2:
-
 Input: "abbaba"
 Output: 2
 Explanation: The longest repeating substrings are "ab" and "ba", each of which occurs twice.
 Example 3:
 
-Input: "aabcaabdaab"
-Output: 3
-Explanation: The longest repeating substring is "aab", which occurs 3 times.
 
   
   
+  
+  
+  
+  
+  
 Solution: BS (makes sense, you use BS to pick string)
+  
+https://www.youtube.com/watch?v=Tf_mD59DLf0  
+  
   
 function longestRepeatingSubstring(s) {
   l = 0;
