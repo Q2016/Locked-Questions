4d0c2958e7c65fdcca2a33211a3ab7a02c1921d6
Q2016 <k.sohraby@gmail.com>
2022-06-27 11:26:00 -0700

    Update 1197. Minimum Knight Moves.py

--- 1197. Minimum Knight Moves.py
+++ 1197. Minimum Knight Moves.py
@@ -4,6 +4,23 @@ A knight has 8 possible moves it can make, as illustrated below. Each move is tw
 cardinal direction, then one square in an orthogonal direction.
 Return the minimum number of steps needed to move the knight to the square [x, y]. 
 
+Example 1:
+Input: x = 2, y = 1
+Output: 1
+Explanation: [0, 0] → [2, 1]
+Example 2:
+Input: x = 5, y = 5
+Output: 4
+Explanation: [0, 0] → [2, 1] → [4, 2] → [3, 4] → [5, 5]
+
+
+
+
+
+
+  
+
+
 
 
 Solution: BFS?
