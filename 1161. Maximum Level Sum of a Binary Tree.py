2b5c04226453bdbf439f21b85b3740a0fd44c5e3
Q2016 <k.sohraby@gmail.com>
2022-06-27 08:59:48 -0700

    Update 1161. Maximum Level Sum of a Binary Tree.py

--- 1161. Maximum Level Sum of a Binary Tree.py
+++ 1161. Maximum Level Sum of a Binary Tree.py
@@ -2,6 +2,20 @@ Question:
 Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
 Return the smallest level x such that the sum of all the values of nodes at level x is maximal    
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Solution: BFS
 
 Use BFS to find the sum of each level, then locate the level with largest sum.    
