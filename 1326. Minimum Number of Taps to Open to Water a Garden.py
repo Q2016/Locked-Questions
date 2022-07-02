76a74a8fc766d2def9fbf91e461e09ab94d0660b
Q2016 <k.sohraby@gmail.com>
2022-07-01 21:20:07 -0700

    Update 1326. Minimum Number of Taps to Open to Water a Garden.py

--- 1326. Minimum Number of Taps to Open to Water a Garden.py
+++ 1326. Minimum Number of Taps to Open to Water a Garden.py
@@ -5,6 +5,26 @@ Given an integer n and an integer array ranges of length n + 1 where ranges[i] (
 [i - ranges[i], i + ranges[i]] if it was open.
 Return the minimum number of taps that should be open to water the whole garden, If the garden cannot be watered return -1.
 
+Input: n = 5, ranges = [3,4,1,1,0,0]
+Output: 1
+Explanation: The tap at point 0 can cover the interval [-3,3]
+The tap at point 1 can cover the interval [-3,5]
+The tap at point 2 can cover the interval [1,3]
+The tap at point 3 can cover the interval [2,4]
+The tap at point 4 can cover the interval [4,4]
+The tap at point 5 can cover the interval [5,5]
+Opening Only the second tap will water the whole garden [0,5]
+
+
+
+
+
+
+
+
+
+
+
 
 
 Solution:
