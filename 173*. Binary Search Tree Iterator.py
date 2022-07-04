842cdd1886cc7609aaab8591330712c5cab39d91
Q2016 <k.sohraby@gmail.com>
2022-07-04 08:01:21 -0700

    Update and rename 173. Binary Search Tree Iterator.py to 173*. Binary Search Tree Iterator.py

--- 173*. Binary Search Tree Iterator.py
+++ 173*. Binary Search Tree Iterator.py
@@ -23,28 +23,37 @@ bSTIterator.hasNext(); // return True
 bSTIterator.next();    // return 20
 bSTIterator.hasNext(); // return False
 
+
+
+
+
+
+
+
 Solution:
 Use Stack to store directed left children from root. When next() be called, I just pop one element and process its right child as new root.
 The code is pretty straightforward. So this can satisfy O(h) memory, hasNext() in O(1) time, But next() is O(h) time.
 I can't find a solution that can satisfy both next() in O(1) time, space in O(h).    
 
+
+https://www.youtube.com/watch?v=RXy5RzGF5wo
+
 class BSTIterator:
-    # @param root, a binary search tree's root node
+   
     def __init__(self, root):
-        self.stack = list()
-        self.pushAll(root)
-
-    # @return a boolean, whether we have a next smallest number
-    def hasNext(self):
-        return self.stack
-
-    # @return an integer, the next smallest number
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root=root.left
+    
     def next(self):
-        tmpNode = self.stack.pop()
-        self.pushAll(tmpNode.right)
-        return tmpNode.val
-        
-    def pushAll(self, node):
-        while node is not None:
-            self.stack.append(node)
-            node = node.left
+        res=self.stack.pop()
+        cur=res.right
+        while cur:
+            self.stack.append(cur)
+            cur=cur.left
+        return res.val
+            
+    def hasNext(self):
+        return self.stack !=[]
+
