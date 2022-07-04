b2fe4d176b0bc20a2b92d9978e3675187e39c02d
Q2016 <k.sohraby@gmail.com>
2022-07-04 06:55:54 -0700

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -8,8 +8,18 @@ Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]    
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-Solution: Two pointers
+Solution: 3 pointers
 # https://www.youtube.com/watch?v=jzZsG8n2R9A
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
