7a5ced7743c928abf4b4266f43bfc4c31ff4fd2c
Q2016 <k.sohraby@gmail.com>
2022-07-08 10:48:11 -0700

    Update 98. Validate Binary Search Tree.py

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -16,15 +16,19 @@ Both the left and right subtrees must also be binary search trees.
 
 
 Solution: (suppose to be similar to 333.)
-Use recursion. Pass down two parameters: lessThan (which means that all nodes in the 
-the current subtree must be smaller than this value) and largerThan (all must be larger 
-than it). Compare root of the current subtree with these two values. Then, recursively 
-check the left and right subtree of the current one. Take care of the values passed down.
+https://www.youtube.com/watch?v=s6ATEkipzow
 
-class Solution(object):
-    def isValidBST(self, root, lessThan = float('inf'), largerThan = float('-inf')):
-        if not root:
-            return True
-        if root.val <= largerThan or root.val >= lessThan:
-            return False
-        return self.isValidBST(root.left, min(lessThan, root.val), largerThan) and self.isValidBST(root.right, lessThan, max(root.val, largerThan))
+class Solution:
+     def isValidBST(self, root):
+          
+          def valid(node,left,right):
+               if not node:
+                    return True
+               if not (node.val<right and node.val>left):
+                    return False
+               
+               return (valid(node.left,left,node.val) and valid(node.right, node.val, right))
+          
+          return valid(root, float("-inf"), float("inf"))
+
+ Time complexity is O(n) 
