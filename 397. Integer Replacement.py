35c3b4462d9680b6807c44975e722dbbfe056885
Q2016 <k.sohraby@gmail.com>
2022-07-11 16:16:52 -0700

    Update 397. Integer Replacement.py

--- 397. Integer Replacement.py
+++ 397. Integer Replacement.py
@@ -11,10 +11,60 @@ Input:8, Output: 3, Explanation: 8 -> 4 -> 2 -> 1
                   
                   
                   
-                  
-                  
+              
+              
+              
+          
+          
+          
+          
+              
+Time complexity of DP version was in the interview              
       
-Solution:  (We do direct approach similar to 1342.)    
+Solution: BFS   
+https://www.youtube.com/watch?v=5ksn2Myjlig
+  
+def integerReplacement(self, n):
+    queue=deque([n])
+    seen=set()
+    seen.add(n)
+    ans=0
+    while queue:
+        for _ in range(len(queue)):
+            cur=queue.poplef()
+
+            if cur==1:
+              return and
+
+            # odd number
+            if cur %2:
+              if cur+1 not in seen:
+                queue.append(cur+1)
+                seen.add(cur+1)
+
+              if cur-1 not in seen:
+                queue.append(cur-1)
+                seen.add(cur-1)
+
+            else:
+              if cur//2 not in seen:
+                queue.append(cur//2)
+                seen.add(cur//2)
+
+        ans+=1
+   return ans
+  
+Time O(log n)  
+  
+  
+  
+Solution 2: DP  
+  
+The point about the DP is that, we can only solve it top-down. Time complexity of DP is also O(log n) because of the n//2 operations.
+  
+  
+(We do direct approach similar to 1342.)    
+
 Denote f(n) the minimum number of jumps from n to 1. By definition, we have the recurrence 
 f(1) = 0, f(2n) = 1 + f(n), f(2n + 1) = min(f(2n) + 1, f(2n + 2) + 1). First notice that this sequence is well defined 
 because f(2n + 2) = f(n + 1) + 1, so f(2n + 1) = min(f(2n) + 1, f(n + 1) + 2). Every element is defined by some element before it.
@@ -30,5 +80,5 @@ because f(2n + 2) = f(n + 1) + 1, so f(2n + 1) = min(f(2n) + 1, f(n + 1) + 2). E
             memo[n] = 1 + min(self.recRep(n+1, memo), self.recRep(n-1, memo))
             return memo[n]
         else:
-            memo[n] = 1 + self.recRep(n/2, memo)
+            memo[n] = 1 + self.recRep(n//2, memo)
             return memo[n]
