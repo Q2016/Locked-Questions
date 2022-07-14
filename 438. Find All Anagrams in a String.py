cdd7a2cb07b4e0db44ada9c89f082b5aa9902347
Q2016 <k.sohraby@gmail.com>
2022-07-13 18:15:19 -0700

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -17,6 +17,37 @@ The substring with start index = 6 is "bac", which is an anagram of "abc".
 
 Solution: Two pointers
     
+    https://www.youtube.com/watch?v=G8xtZy0fDKg
+  
+class Solution:
+    def findAnagrams(self, s, p):
+        if len(p)>len(q): return []
+        
+        pCount, sCount={}, {}
+        for i in range(len(p)):
+            pCount[p[i]]=1+pCount.get(p[i],0)
+            sCount[s[i]]=1+sCount.get(s[i],0)
+            
+        res=[0] if sCount==pCount else []
+        l=0
+        for r in range(len(p), len(s)):
+            sCount[s[r]]=1+sCount.get(s[r],0)
+            sCoount[s[l]] -=1
+            
+            if sCount[s[l]]==0: # not trivial
+                sCount.pop(s[l])
+                
+            l+=1
+            if sCount==pCount:
+                res.append(l)
+                
+        return res 
+            
+
+
+
+
+
     def findAnagrams(self, s, p):
 
         result = []
