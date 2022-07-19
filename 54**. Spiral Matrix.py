862ac37b8a260b97135f602237c8e90e689a6363
Q2016 <k.sohraby@gmail.com>
2022-07-19 07:49:26 -0700

    Update and rename 54. Spiral Matrix.py to 54**. Spiral Matrix.py

--- 54**. Spiral Matrix.py
+++ 54**. Spiral Matrix.py
@@ -18,6 +18,50 @@ Output: [1,2,3,6,9,8,7,4,5]
   
   
 Solution:  
+  
+  https://www.youtube.com/watch?v=BJnMZNwUk1M
+  
+class Solution:
+  def spiralOrder(self, matrix):
+    res=[]
+    left, right=0, len(matrix[0])
+    top, bottom=0, len(matrix)
+    
+    while left<right and top<bottom:
+      # get every i in the top row
+      for i in range(left, right):
+        res.append(matrix[top][i])
+      top +=1
+      
+      # get every i in the right col
+      for i in range(top, bottom):
+        res.append(matrix[i][right-1])
+      right -=1
+      
+      if not (left< right and top<bottom):
+        break
+        
+      # get every i in the bottom row
+      for i in range(right-1, left-1,-1):
+        res.append(matrix[bottom-1][i])
+        
+      bottom -=1
+      
+      # get every i in the left col
+      for i in range(bottom-1, top-1,-1):
+        res.append(matrix[i][left])
+      left+=1
+      
+    return res  
+   
+       
+  
+  
+  
+  
+  
+  
+  
 This is a very simple and easy to understand solution. I traverse right and increment rowBegin, then traverse 
 down and decrement colEnd, then I traverse left and decrement rowEnd, and finally I traverse up and increment colBegin.
 The only tricky part is that when I traverse left or up I have to check whether the row or col still exists to prevent duplicates. 
