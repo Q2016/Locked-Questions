95450d6c733088160e002ffc62473ded56b8e823
Q2016 <k.sohraby@gmail.com>
2022-07-19 12:13:31 -0700

    Update and rename 323. Number of Connected Components in an Undirected Graph.py to 323*. Number of Connected Components in an Undirected Graph.py

--- 323*. Number of Connected Components in an Undirected Graph.py
+++ 323*. Number of Connected Components in an Undirected Graph.py
@@ -23,13 +23,55 @@ is the same as [1, 0] and thus will not appear together in edges.
 
 
 
-Solution: BFS (261. Repeated)
-     
+Solution: Union Find (261. Repeated)
+   https://www.youtube.com/watch?v=8f1XPm4WOUc  
+    
+class Solution:
+     def countComponents(self, n, edges):
+          par=[i for i in range(n)]
+          rank=[1]*n
+          
+          def find(n1): # find root parent
+               res=n1
+               
+               while res !=par[res]:
+                    par[res] = par[par[res]] # parent to be grand parent
+                    res=par[res]
+               return res
+          
+          def union(n1, n2):
+               p1, p2= find(n1), find(n2)
+               
+               if p1==p2:
+                    return 0
+               if rank[p2]> rank[p1]:
+                    par[p1]=p2
+                    rank[p2]+=rank[p1]
+               else:
+                    par[p2]=p1
+                    rank[p1]+=rank[p2]
+               return 1
+          
+          res=n
+          for n1, n2 in edges:
+               res-=union(n1, n2)
+          return res
+                    
+               
+                    
+
+
+
+
+
+
+
+
+
+BFS:
 Start from index 0 to n. For each index, use BFS to find all it’s related numbers and append them 
 to the visited set, if this index has no more related number then count + 1 and start from next 
 index, note that if the index is in visited set. we skip to next index.
-
-
 
 def countComponents(self, n: int, edges: List[List[int]]) -> int:
         dist = collections.defaultdict(list)
