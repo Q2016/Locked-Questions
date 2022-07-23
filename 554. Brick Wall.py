a329da2ff5f2a63d503fdbfa388e550f4d18a416
Q2016 <k.sohraby@gmail.com>
2022-07-23 16:45:14 -0700

    Update 554. Brick Wall.py

--- 554. Brick Wall.py
+++ 554. Brick Wall.py
@@ -22,38 +22,24 @@ Input:      Output: 2
 
 
 Solution: Hashmap
+    https://www.youtube.com/watch?v=Kkmv2h48ekw
+    
     
 For the picture of the problem, look at the leetcode.    
 Number of Bricks Crossed by Line = Number of Rows in Wall - Frequency of Most Occuring Edge
     
-    
-    
-class Solution {
-public:
-    int leastBricks(vector<vector<int>>& wall) 
-    {
-        unordered_map<int, int> edge_frequency;     //HashMap to store the number of common edges among the rows
-
-        int max_frequency = 0;         //Variable to store the frequency of most occuring edge
+class Solution:
+    def leastBricks(self, wall):
+        countGap={0:0}
         
-        for(int row=0; row<wall.size(); row++)        //Iterating through each row
-        {
-            int edge_postion = 0;       //Variable to store different edge postion
-            
-            for(int brick_no=0; brick_no< wall[row].size() -1; brick_no++)    //Iterating through each brick inside a row
-            { 
-                int current_brick_length = wall[row][brick_no];  //Length of the current brick
+        for r in wall:
+            total=0
+            for b in r[:-1]:
+                total +=b
+                countGap[total]=1+countGap.get(total,0)
                 
-                edge_postion = edge_postion + current_brick_length ;  //Next Edge Position = Previous Edge Position + Current Brick's Length
-                
-                edge_frequency[edge_postion]++;  //Incrementing the Frequency of just calculated Edge Postion
-                
-                max_frequency = max(edge_frequency[edge_postion],max_frequency);  //Comparing the "Frequency of just calculated Edge Postion" with "Max Frequency seen till now" & storing whichever is greater.
-            }
-        }
-        return wall.size() - max_frequency; // returning (Number of Bricks Crossed by Line) i.e. (Number of Rows in Wall - Frequency of Most Occuring Edge) 
-    }
-};
+        return len(wall)-countGap
+    
 
     
     
