b719a5b75d6a4ae25c7a36cdb12d8f37b08323d9
Q2016 <k.sohraby@gmail.com>
2022-07-24 19:49:29 -0700

    Update and rename 6. Zigzag Conversion.py to 6***. Zigzag Conversion.py

--- 6***. Zigzag Conversion.py
+++ 6***. Zigzag Conversion.py
@@ -13,13 +13,6 @@ Output: "PAHNAPLSIIGYIR"
     
 Solution:
     
-    def convert(self, s, numRows):
-        if numRows == 1:
-            return s
-        step, zigzag = 2 * numRows - 2, ""
-        for i in xrange(numRows):
-            for j in xrange(i, len(s), step):
-                zigzag += s[j]
-                if 0 < i < numRows - 1 and j + step - 2 * i < len(s):
-                    zigzag += s[j + step - 2 * i]
-        return zigzag
+come back again 
+
+https://www.youtube.com/watch?v=Q2Tw6gcVEwc
